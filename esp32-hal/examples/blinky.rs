6d94b61268e01083a805c40fcca85010e74f1a85
Björn Quentin <bjoernQ@users.noreply.github.com>
2022-01-06 16:57:55 +0100

    Shared GPIO Implementation (#3)
    * make esp32c3 examples run from flash / flashable
    * use gpio3 for blinky
    * direct boot in Cargo.toml
    * have a shared gpio impl
    * use PACs from their original locations again

--- esp32-hal/examples/blinky.rs
+++ esp32-hal/examples/blinky.rs
@@ -1,26 +1,21 @@
 #![no_std]
 #![no_main]
 
-use esp32c3_hal::{gpio::IO, pac, prelude::*, RtcCntl, Timer};
+use esp32_hal::{gpio::IO, pac, prelude::*, Timer};
 use nb::block;
 use panic_halt as _;
-use riscv_rt::entry;
+use xtensa_lx_rt::entry;
 
 #[entry]
 fn main() -> ! {
     let peripherals = pac::Peripherals::take().unwrap();
 
     let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
-    let rtc_cntl = RtcCntl::new(peripherals.RTC_CNTL);
     let mut timer0 = Timer::new(peripherals.TIMG0);
-    let mut timer1 = Timer::new(peripherals.TIMG1);
 
-    rtc_cntl.set_super_wdt_enable(false);
-    rtc_cntl.set_wdt_enable(false);
     timer0.disable();
-    timer1.disable();
 
-    let mut led = io.pins.gpio5.into_push_pull_output();
+    let mut led = io.pins.gpio15.into_push_pull_output();
     led.set_high().unwrap();
 
     timer0.start(10_000_000u64);
--- esp32c3-hal/esp32c3-link.x
+++ esp32c3-hal/esp32c3-link.x
@@ -1,2 +1,14 @@
 INCLUDE memory.x
-INCLUDE link.x
+
+SECTIONS
+{
+  .header : AT(0)
+  {
+    LONG(0xaedb041d)
+    LONG(0xaedb041d)
+  } > IROM
+}
+
+_stext = ORIGIN(IROM) + 8;
+
+INCLUDE riscv-link.x
