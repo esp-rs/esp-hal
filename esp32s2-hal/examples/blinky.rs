d5938de6c049cb110339ee238a6f5831a545a6e0
bjoernQ <bjoern.quentin@mobile-j.de>
2022-02-18 16:16:14 +0100

    GPIO for ESP32S2

---
+++ esp32s2-hal/examples/blinky.rs
@@ -0,0 +1,31 @@
+#![no_std]
+#![no_main]
+
+use esp32s2_hal::{gpio::IO, pac::Peripherals, prelude::*, Delay, Timer};
+use panic_halt as _;
+use xtensa_lx_rt::entry;
+
+#[entry]
+fn main() -> ! {
+    let peripherals = Peripherals::take().unwrap();
+
+    // Disable the TIMG watchdog timer.
+    let mut timer0 = Timer::new(peripherals.TIMG0);
+
+    timer0.disable();
+
+    // Set GPIO4 as an output, and set its state high initially.
+    let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
+    let mut led = io.pins.gpio4.into_push_pull_output();
+
+    led.set_high().unwrap();
+
+    // Initialize the Delay peripheral, and use it to toggle the LED state in a
+    // loop.
+    let mut delay = Delay::new();
+
+    loop {
+        led.toggle().unwrap();
+        delay.delay_ms(500u32);
+    }
+}
