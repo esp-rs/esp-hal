4cc78c4b6b498e6105286f4523ed1397d8a38520
bjoernQ <bjoern.quentin@mobile-j.de>
2022-02-24 15:48:15 +0100

    Move GPIO type definitions to esp-hal-common

--- esp-hal-common/src/gpio.rs
+++ esp-hal-common/src/gpio.rs
@@ -10,6 +10,12 @@ use core::marker::PhantomData;
 
 pub use paste::paste;
 
+#[cfg_attr(feature = "esp32", path = "gpio/esp32.rs")]
+#[cfg_attr(feature = "esp32c3", path = "gpio/esp32c3.rs")]
+#[cfg_attr(feature = "esp32s2", path = "gpio/esp32s2.rs")]
+#[cfg_attr(feature = "esp32s3", path = "gpio/esp32s3.rs")]
+pub mod types;
+
 #[derive(Copy, Clone)]
 pub enum Event {
     RisingEdge  = 1,
