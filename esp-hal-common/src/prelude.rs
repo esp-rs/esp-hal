2417cd0b33848c191c6ca04958bc4f70b6f65f37
Jesse Braham <jesse@beta7.io>
2022-06-14 07:47:55 -0700

    Update the prelude, adding a public module for the `embedded-hal` alpha traits

--- esp-hal-common/src/prelude.rs
+++ esp-hal-common/src/prelude.rs
@@ -4,15 +4,56 @@
 //! drivers implemented in this crate.
 
 pub use embedded_hal::{
-    blocking::delay::{DelayMs as _, DelayUs as _},
     digital::v2::{
-        InputPin as _,
-        OutputPin as _,
-        StatefulOutputPin as _,
-        ToggleableOutputPin as _,
+        InputPin as _embedded_hal_digital_v2_InputPin,
+        OutputPin as _embedded_hal_digital_v2_OutputPin,
+        StatefulOutputPin as _embedded_hal_digital_v2_StatefulOutputPin,
+        ToggleableOutputPin as _embedded_hal_digital_v2_ToggleableOutputPin,
     },
     prelude::*,
 };
-pub use fugit::{ExtU32 as _, ExtU64 as _, RateExtU32 as _, RateExtU64 as _};
+pub use fugit::{
+    ExtU32 as _fugit_ExtU32,
+    ExtU64 as _fugit_ExtU64,
+    RateExtU32 as _fugit_RateExtU32,
+    RateExtU64 as _fugit_RateExtU64,
+};
+pub use nb;
 
 pub use crate::system::SystemExt;
+
+/// All traits required for using the 1.0.0-alpha.x release of embedded-hal
+pub mod eh1 {
+    pub use embedded_hal_1::{
+        delay::blocking::DelayUs as _embedded_hal_delay_blocking_DelayUs,
+        digital::blocking::{
+            InputPin as _embedded_hal_digital_blocking_InputPin,
+            OutputPin as _embedded_hal_digital_blocking_OutputPin,
+            StatefulOutputPin as _embedded_hal_digital_blocking_StatefulOutputPin,
+            ToggleableOutputPin as _embedded_hal_digital_blocking_ToggleableOutputPin,
+        },
+        i2c::blocking::I2c as _embedded_hal_i2c_blocking_I2c,
+        serial::nb::{
+            Read as _embedded_hal_serial_nb_Read,
+            Write as _embedded_hal_serial_nb_Write,
+        },
+        spi::{
+            blocking::{
+                SpiBus as _embedded_hal_spi_blocking_SpiBus,
+                SpiBusFlush as _embedded_hal_spi_blocking_SpiBusFlush,
+                SpiBusRead as _embedded_hal_spi_blocking_SpiBusRead,
+                SpiBusWrite as _embedded_hal_spi_blocking_SpiBusWrite,
+            },
+            nb::FullDuplex as _embedded_hal_spi_nb_FullDuplex,
+        },
+    };
+    pub use fugit::{
+        ExtU32 as _fugit_ExtU32,
+        ExtU64 as _fugit_ExtU64,
+        RateExtU32 as _fugit_RateExtU32,
+        RateExtU64 as _fugit_RateExtU64,
+    };
+    pub use nb;
+
+    pub use crate::system::SystemExt;
+}
