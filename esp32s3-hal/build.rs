0e7fd0caa9ed1b334f6a1c1e451dcbd01b56ba25
bjoernQ <bjoern.quentin@mobile-j.de>
2022-07-18 14:23:55 +0200

    Add direct-boot support for ESP32-S3

--- esp32s3-hal/build.rs
+++ esp32s3-hal/build.rs
@@ -1,5 +1,6 @@
 use std::{env, fs::File, io::Write, path::PathBuf};
 
+#[cfg(not(feature = "direct-boot"))]
 fn main() {
     // Put the linker script somewhere the linker can find it
     let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
@@ -21,6 +22,42 @@ fn main() {
     File::create(out.join("esp32s3.x"))
         .unwrap()
         .write_all(include_bytes!("ld/esp32s3.x"))
+        .unwrap();
+
+    File::create(out.join("linkall.x"))
+        .unwrap()
+        .write_all(include_bytes!("ld/linkall.x"))
+        .unwrap();
+
+    println!("cargo:rustc-link-search={}", out.display());
+
+    // Only re-run the build script when memory.x is changed,
+    // instead of when any part of the source code changes.
+    println!("cargo:rerun-if-changed=memory.x");
+}
+
+#[cfg(feature = "direct-boot")]
+fn main() {
+    // Put the linker script somewhere the linker can find it
+    let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
+    File::create(out.join("memory.x"))
+        .unwrap()
+        .write_all(include_bytes!("ld/db-memory.x"))
+        .unwrap();
+
+    File::create(out.join("alias.x"))
+        .unwrap()
+        .write_all(include_bytes!("ld/rom.x"))
+        .unwrap();
+
+    File::create(out.join("hal-defaults.x"))
+        .unwrap()
+        .write_all(include_bytes!("ld/hal-defaults.x"))
+        .unwrap();
+
+    File::create(out.join("esp32s3.x"))
+        .unwrap()
+        .write_all(include_bytes!("ld/db-esp32s3.x"))
         .unwrap();
 
     File::create(out.join("linkall.x"))
