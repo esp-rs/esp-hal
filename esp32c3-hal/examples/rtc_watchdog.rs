83e217417a8e853d02360231a3716b2cfbb15c80
Gustavo Henrique Nihei <gustavo.nihei@espressif.com>
2022-08-03 15:32:35 -0300

    esp32c3: Add example for the RTC Watchdog Timer driver
    Signed-off-by: Gustavo Henrique Nihei <gustavo.nihei@espressif.com>

---
+++ esp32c3-hal/examples/rtc_watchdog.rs
@@ -0,0 +1,69 @@
+//! This demos the RTC Watchdog Timer (RWDT).
+//! The RWDT is initially configured to trigger an interrupt after a given timeout.
+//! Then, upon expiration, the RWDT is restarted and then reconfigured to reset both the main
+//! system and the RTC.
+
+#![no_std]
+#![no_main]
+
+use core::cell::RefCell;
+
+use bare_metal::Mutex;
+
+use esp32c3_hal::{
+    clock::ClockControl,
+    interrupt,
+    pac::{self, Peripherals},
+    prelude::*,
+    Rtc,
+};
+use esp_hal_common::Rwdt;
+use panic_halt as _;
+use riscv_rt::entry;
+
+static mut RWDT: Mutex<RefCell<Option<Rwdt>>> = Mutex::new(RefCell::new(None));
+
+#[entry]
+fn main() -> ! {
+    let peripherals = Peripherals::take().unwrap();
+    let system = peripherals.SYSTEM.split();
+    let _clocks = ClockControl::boot_defaults(system.clock_control).freeze();
+
+    let mut rtc = Rtc::new(peripherals.RTC_CNTL);
+
+    // Disable watchdog timers
+    rtc.swd.disable();
+    rtc.rwdt.disable();
+
+    rtc.rwdt.start(2000u64.millis());
+    rtc.rwdt.listen();
+
+    interrupt::enable(pac::Interrupt::RTC_CORE, interrupt::Priority::Priority1).unwrap();
+
+    riscv::interrupt::free(|_cs| unsafe {
+        RWDT.get_mut().replace(Some(rtc.rwdt));
+    });
+
+    unsafe {
+        riscv::interrupt::enable();
+    }
+
+    loop {}
+}
+
+#[interrupt]
+fn RTC_CORE() {
+    riscv::interrupt::free(|cs| unsafe {
+        esp_println::println!("RWDT Interrupt");
+
+        let mut rwdt = RWDT.borrow(*cs).borrow_mut();
+        let rwdt = rwdt.as_mut().unwrap();
+
+        rwdt.clear_interrupt();
+
+        esp_println::println!("Restarting in 5 seconds...");
+
+        rwdt.start(5000u64.millis());
+        rwdt.unlisten();
+    });
+}
