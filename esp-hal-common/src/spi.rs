605e196c745047b45ec45b171e47e6e8dd0d9a3c
Andreas Hartmann <hartan@7x.de>
2022-08-08 12:44:26 +0200

    common/spi: Implement `SpiDevice` for xtensa arch
    and move the code into a submodule that is fenced with conditional
    compilation directives.

--- esp-hal-common/src/spi.rs
+++ esp-hal-common/src/spi.rs
@@ -1,7 +1,8 @@
 //! # Serial Peripheral Interface
 //!
-//! There are multiple ways to use SPI, depending on your needs. Regardles of which way you choose,
-//! you must first create an SPI instance with [`Spi::new`].
+//! There are multiple ways to use SPI, depending on your needs. Regardles of
+//! which way you choose, you must first create an SPI instance with
+//! [`Spi::new`].
 //!
 //! ```rust
 //! let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
@@ -25,23 +26,26 @@
 //!
 //! ## Exclusive access to the SPI bus
 //!
-//! If all you want to do is to communicate to a single device, and you initiate transactions
-//! yourself, there are a number of ways to achieve this:
+//! If all you want to do is to communicate to a single device, and you initiate
+//! transactions yourself, there are a number of ways to achieve this:
 //!
-//! - Use the [`FullDuplex`](embedded_hal::spi::FullDuplex) trait to read/write single bytes at a
-//!   time,
-//! - Use the [`SpiBus`](embedded_hal_1::spi::blocking::SpiBus) trait (requires the "eh1" feature)
-//!   and its associated functions to initiate transactions with simultaneous reads and writes, or
+//! - Use the [`FullDuplex`](embedded_hal::spi::FullDuplex) trait to read/write
+//!   single bytes at a time,
+//! - Use the [`SpiBus`](embedded_hal_1::spi::blocking::SpiBus) trait (requires
+//!   the "eh1" feature) and its associated functions to initiate transactions
+//!   with simultaneous reads and writes, or
 //! - Use the [`SpiBusWrite`](embedded_hal_1::spi::blocking::SpiBusWrite) and
-//!   [`SpiBusRead`](embedded_hal_1::spi::blocking::SpiBusRead) traits (requires the "eh1" feature)
-//!   and their associated functions to read or write mutiple bytes at a time.
+//!   [`SpiBusRead`](embedded_hal_1::spi::blocking::SpiBusRead) traits (requires
+//!   the "eh1" feature) and their associated functions to read or write mutiple
+//!   bytes at a time.
 //!
 //!
 //! ## Shared SPI access
 //!
-//! If you have multiple devices on the same SPI bus that each have their own CS line, you may want
-//! to have a look at the [`SpiBusController`] and [`SpiBusDevice`] implemented here. These give
-//! exclusive access to the underlying SPI bus by means of a Mutex. This ensures that device
+//! If you have multiple devices on the same SPI bus that each have their own CS
+//! line, you may want to have a look at the [`SpiBusController`] and
+//! [`SpiBusDevice`] implemented here. These give exclusive access to the
+//! underlying SPI bus by means of a Mutex. This ensures that device
 //! transactions do not interfere with each other.
 
 use core::convert::Infallible;
@@ -360,100 +364,112 @@ mod ehal1 {
         }
     }
 
-    use xtensa_lx::mutex::Mutex;
-    use xtensa_lx::mutex::SpinLockMutex;
-    use core::cell::RefCell;
-    use embedded_hal_1::spi;
-    use embedded_hal_1::spi::ErrorType;
-    use embedded_hal_1::spi::blocking::SpiDevice;
-    use embedded_hal_1::digital::blocking::OutputPin;
+    #[cfg(any(feature = "esp32", feature = "esp32s2", feature = "esp32s3"))]
+    pub use xtensa_spi_device::*;
 
-    /// SPI bus controller.
-    ///
-    /// Has exclusive access to an SPI bus, which is managed via a `Mutex`. Used as basis for the
-    /// [`SpiBusDevice`] implementation. Note that the wrapped [`RefCell`] is used solely to
-    /// achieve interior mutability.
-    pub struct SpiBusController<B: SpiBus + ErrorType> {
-        lock: SpinLockMutex<RefCell<B>>,
-    }
+    #[cfg(any(feature = "esp32", feature = "esp32s2", feature = "esp32s3"))]
+    mod xtensa_spi_device {
+        use super::*;
+        use core::cell::RefCell;
 
-    impl<B: SpiBus + ErrorType> SpiBusController<B> {
-        /// Create a new controller from an SPI bus instance.
+        use embedded_hal_1::{
+            digital::blocking::OutputPin,
+            spi::{self, blocking::SpiDevice, ErrorType},
+        };
+        use xtensa_lx::mutex::{Mutex, SpinLockMutex};
+
+        /// SPI bus controller.
         ///
-        /// Takes ownership of the SPI bus in the process. Afterwards, the SPI bus can only be
-        /// accessed via instances of [`SpiBusDevice`].
-        pub fn from_spi(bus: B) -> Self {
-            SpiBusController {
-                lock: SpinLockMutex::new(RefCell::new(bus)),
+        /// Has exclusive access to an SPI bus, which is managed via a `Mutex`. Used as basis for
+        /// the [`SpiBusDevice`] implementation. Note that the wrapped [`RefCell`] is used solely
+        /// to achieve interior mutability.
+        pub struct SpiBusController<B: SpiBus + ErrorType> {
+            lock: SpinLockMutex<RefCell<B>>,
+        }
+
+        impl<B: SpiBus + ErrorType> SpiBusController<B> {
+            /// Create a new controller from an SPI bus instance.
+            ///
+            /// Takes ownership of the SPI bus in the process. Afterwards, the SPI bus can only be
+            /// accessed via instances of [`SpiBusDevice`].
+            pub fn from_spi(bus: B) -> Self {
+                SpiBusController {
+                    lock: SpinLockMutex::new(RefCell::new(bus)),
+                }
             }
         }
-    }
 
-    impl<B> ErrorType for SpiBusController<B>
-    where
-        B: SpiBus + ErrorType,
-    {
-        type Error = spi::ErrorKind;
-    }
+        impl<B> ErrorType for SpiBusController<B>
+        where
+            B: SpiBus + ErrorType,
+        {
+            type Error = spi::ErrorKind;
+        }
 
-    /// An SPI device on a shared SPI bus.
-    ///
-    /// Provides device specific access on a shared SPI bus. Enables attaching multiple SPI devices
-    /// to the same bus, each with its own CS line, and performing safe transfers on them.
-    pub struct SpiBusDevice<'a, B, CS>
-    where
-        B: SpiBus + ErrorType,
-        CS: OutputPin,
-    {
-        bus: &'a SpiBusController<B>,
-        cs: CS,
-    }
+        /// An SPI device on a shared SPI bus.
+        ///
+        /// Provides device specific access on a shared SPI bus. Enables attaching multiple SPI
+        /// devices to the same bus, each with its own CS line, and performing safe transfers on
+        /// them.
+        pub struct SpiBusDevice<'a, B, CS>
+        where
+            B: SpiBus + ErrorType,
+            CS: OutputPin,
+        {
+            bus: &'a SpiBusController<B>,
+            cs: CS,
+        }
 
-    impl<'a, B, CS> SpiBusDevice<'a, B, CS>
-    where
-        B: SpiBus + ErrorType,
-        CS: OutputPin,
-    {
-        pub fn new(bus: &'a SpiBusController<B>, cs: CS) -> Self {
-            SpiBusDevice {
-                bus,
-                cs,
+        impl<'a, B, CS> SpiBusDevice<'a, B, CS>
+        where
+            B: SpiBus + ErrorType,
+            CS: OutputPin,
+        {
+            pub fn new(bus: &'a SpiBusController<B>, cs: CS) -> Self {
+                SpiBusDevice { bus, cs }
             }
         }
-    }
 
-    impl<'a, B, CS> ErrorType for SpiBusDevice<'a, B, CS>
-    where
-        B: SpiBus + ErrorType,
-        CS: OutputPin,
-    {
-        type Error = spi::ErrorKind;
-    }
+        impl<'a, B, CS> ErrorType for SpiBusDevice<'a, B, CS>
+        where
+            B: SpiBus + ErrorType,
+            CS: OutputPin,
+        {
+            type Error = spi::ErrorKind;
+        }
 
-    impl<B, CS> SpiDevice for SpiBusDevice<'_, B, CS>
-    where
-        B: SpiBus + ErrorType,
-        CS: OutputPin,
-    {
-        type Bus = B;
+        impl<B, CS> SpiDevice for SpiBusDevice<'_, B, CS>
+        where
+            B: SpiBus + ErrorType,
+            CS: OutputPin,
+        {
+            type Bus = B;
 
-        fn transaction<R>(&mut self, f: impl FnOnce(&mut Self::Bus) -> Result<R, <Self::Bus as ErrorType>::Error>) -> Result<R, Self::Error> {
-            (&self.bus.lock).lock(|bus| {
-                let mut bus = bus.borrow_mut();
-                self.cs.set_low().map_err(|_| spi::ErrorKind::ChipSelectFault)?;
+            fn transaction<R>(
+                &mut self,
+                f: impl FnOnce(&mut Self::Bus) -> Result<R, <Self::Bus as ErrorType>::Error>,
+            ) -> Result<R, Self::Error> {
+                (&self.bus.lock).lock(|bus| {
+                    let mut bus = bus.borrow_mut();
+                    self.cs
+                        .set_low()
+                        .map_err(|_| spi::ErrorKind::ChipSelectFault)?;
 
-                // We postpone handling these errors until AFTER we raised CS again, so the bus is
-                // free (Or we die trying if CS errors).
-                let f_res = f(&mut bus);
-                let flush_res = bus.flush();
+                    // We postpone handling these errors until AFTER we raised CS again, so the bus
+                    // is free (Or we die trying if CS errors).
+                    let f_res = f(&mut bus);
+                    let flush_res = bus.flush();
 
-                self.cs.set_high().map_err(|_| spi::ErrorKind::ChipSelectFault)?;
+                    self.cs
+                        .set_high()
+                        .map_err(|_| spi::ErrorKind::ChipSelectFault)?;
 
-                let f_res = f_res.map_err(|_| spi::ErrorKind::Other)?;
-                flush_res.map_err(|_| spi::ErrorKind::Other)?;
+                    let f_res = f_res.map_err(|_| spi::ErrorKind::Other)?;
+                    flush_res.map_err(|_| spi::ErrorKind::Other)?;
 
-                Ok(f_res)
-            })
+                    Ok(f_res)
+                })
+            }
         }
     }
 }
