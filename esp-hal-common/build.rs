c028353a99a7be8dacf411eee73d4a3646cce1ee
Jesse Braham <jesse@beta7.io>
2022-09-13 14:14:18 -0700

    Update the build script to inject a configuration value for the enabled chip

--- esp-hal-common/build.rs
+++ esp-hal-common/build.rs
@@ -1,12 +1,24 @@
 fn main() {
-    let chip_features = [
-        cfg!(feature = "esp32"),
-        cfg!(feature = "esp32c3"),
-        cfg!(feature = "esp32s2"),
-        cfg!(feature = "esp32s3"),
-    ];
+    let esp32 = cfg!(feature = "esp32");
+    let esp32c3 = cfg!(feature = "esp32c3");
+    let esp32s2 = cfg!(feature = "esp32s2");
+    let esp32s3 = cfg!(feature = "esp32s3");
 
-    if chip_features.iter().filter(|&&f| f).count() != 1 {
-        panic!("Exactly one chip must be enabled via its cargo feature");
+    // Ensure that exactly one chip has been specified
+    let chip_features = [esp32, esp32c3, esp32s2, esp32s3];
+    match chip_features.iter().filter(|&&f| f).count() {
+        1 => {}
+        n => panic!("Exactly 1 chip must be enabled via its Cargo feature, {n} provided"),
+    }
+
+    // Inject a configuration symbol for the enabled chip
+    if esp32 {
+        println!("cargo:rustc-cfg=esp32");
+    } else if esp32c3 {
+        println!("cargo:rustc-cfg=esp32c3");
+    } else if esp32s2 {
+        println!("cargo:rustc-cfg=esp32s2");
+    } else if esp32s3 {
+        println!("cargo:rustc-cfg=esp32s3");
     }
 }
