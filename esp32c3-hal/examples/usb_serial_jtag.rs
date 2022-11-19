bc39cdae86e721b9ffd10002a3e738ff136c97dd
Josh Weberruss <joshua.weberruss@gmail.com>
2022-11-19 16:01:12 +1100

    Add Rx and interrupts to UsbSerialJtag
    Add non-blocking Write

--- esp32c3-hal/examples/usb_serial_jtag.rs
+++ esp32c3-hal/examples/usb_serial_jtag.rs
@@ -6,19 +6,25 @@
 #![no_std]
 #![no_main]
 
-use core::fmt::Write;
+use core::{cell::RefCell, fmt::Write};
 
+use critical_section::Mutex;
 use esp32c3_hal::{
     clock::ClockControl,
-    pac::Peripherals,
+    interrupt,
+    pac::{self, Peripherals, USB_DEVICE},
     prelude::*,
     timer::TimerGroup,
-    Delay,
+    Cpu,
     Rtc,
     UsbSerialJtag,
 };
 use esp_backtrace as _;
+use nb::block;
 use riscv_rt::entry;
+
+static USB_SERIAL: Mutex<RefCell<Option<UsbSerialJtag<USB_DEVICE>>>> =
+    Mutex::new(RefCell::new(None));
 
 #[entry]
 fn main() -> ! {
@@ -26,9 +32,9 @@ fn main() -> ! {
     let system = peripherals.SYSTEM.split();
     let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
 
-    let mut delay = Delay::new(&clocks);
     let mut rtc = Rtc::new(peripherals.RTC_CNTL);
     let timer_group0 = TimerGroup::new(peripherals.TIMG0, &clocks);
+    let mut timer0 = timer_group0.timer0;
     let mut wdt0 = timer_group0.wdt;
     let timer_group1 = TimerGroup::new(peripherals.TIMG1, &clocks);
     let mut wdt1 = timer_group1.wdt;
@@ -39,8 +45,52 @@ fn main() -> ! {
     wdt0.disable();
     wdt1.disable();
 
-    loop {
-        writeln!(UsbSerialJtag, "Hello world!").ok();
-        delay.delay_ms(500u32);
+    let mut usb_serial = UsbSerialJtag::new(peripherals.USB_DEVICE);
+
+    usb_serial.listen_rx_packet_recv_interrupt();
+
+    timer0.start(1u64.secs());
+
+    critical_section::with(|cs| USB_SERIAL.borrow_ref_mut(cs).replace(usb_serial));
+
+    interrupt::enable(
+        pac::Interrupt::USB_SERIAL_JTAG,
+        interrupt::Priority::Priority1,
+    )
+    .unwrap();
+
+    interrupt::set_kind(
+        Cpu::ProCpu,
+        interrupt::CpuInterrupt::Interrupt1,
+        interrupt::InterruptKind::Edge,
+    );
+
+    unsafe {
+        riscv::interrupt::enable();
     }
+
+    loop {
+        critical_section::with(|cs| {
+            writeln!(
+                USB_SERIAL.borrow_ref_mut(cs).as_mut().unwrap(),
+                "Hello world!"
+            )
+            .ok();
+        });
+
+        block!(timer0.wait()).unwrap();
+    }
+}
+
+#[interrupt]
+fn USB_SERIAL_JTAG() {
+    critical_section::with(|cs| {
+        let mut usb_serial = USB_SERIAL.borrow_ref_mut(cs);
+        let usb_serial = usb_serial.as_mut().unwrap();
+        writeln!(usb_serial, "USB serial interrupt").unwrap();
+        while let nb::Result::Ok(c) = usb_serial.read_byte() {
+            writeln!(usb_serial, "Read byte: {:02x}", c).unwrap();
+        }
+        usb_serial.reset_rx_packet_recv_interrupt();
+    });
 }
--- esp32s3-hal/examples/usb_serial_jtag.rs
+++ esp32s3-hal/examples/usb_serial_jtag.rs
@@ -1,23 +1,30 @@
 //! This shows how to output text via USB Serial/JTAG.
 //! You need to connect via the Serial/JTAG interface to see any output.
 //! Most dev-kits use a USB-UART-bridge - in that case you won't see any output.
+//! PLEASE NOTE: USB Serial input on ESP32-S3 is not currently working
+//! See https://github.com/esp-rs/esp-hal/issues/269 for details
 
 #![no_std]
 #![no_main]
 
-use core::fmt::Write;
+use core::{cell::RefCell, fmt::Write};
 
+use critical_section::Mutex;
 use esp32s3_hal::{
     clock::ClockControl,
-    pac::Peripherals,
+    interrupt,
+    pac::{self, Peripherals, USB_DEVICE},
     prelude::*,
     timer::TimerGroup,
-    Delay,
     Rtc,
     UsbSerialJtag,
 };
 use esp_backtrace as _;
+use nb::block;
 use xtensa_lx_rt::entry;
+
+static USB_SERIAL: Mutex<RefCell<Option<UsbSerialJtag<USB_DEVICE>>>> =
+    Mutex::new(RefCell::new(None));
 
 #[entry]
 fn main() -> ! {
@@ -25,17 +32,47 @@ fn main() -> ! {
     let system = peripherals.SYSTEM.split();
     let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
 
-    let mut delay = Delay::new(&clocks);
     let mut rtc = Rtc::new(peripherals.RTC_CNTL);
     let timer_group0 = TimerGroup::new(peripherals.TIMG0, &clocks);
+    let mut timer0 = timer_group0.timer0;
     let mut wdt = timer_group0.wdt;
 
     // Disable MWDT and RWDT (Watchdog) flash boot protection
     wdt.disable();
     rtc.rwdt.disable();
 
+    let mut usb_serial = UsbSerialJtag::new(peripherals.USB_DEVICE);
+
+    usb_serial.listen_rx_packet_recv_interrupt();
+
+    timer0.start(1u64.secs());
+
+    critical_section::with(|cs| USB_SERIAL.borrow_ref_mut(cs).replace(usb_serial));
+
+    interrupt::enable(pac::Interrupt::USB_DEVICE, interrupt::Priority::Priority1).unwrap();
+
     loop {
-        writeln!(UsbSerialJtag, "Hello world!").ok();
-        delay.delay_ms(500u32);
+        critical_section::with(|cs| {
+            writeln!(
+                USB_SERIAL.borrow_ref_mut(cs).as_mut().unwrap(),
+                "Hello world!"
+            )
+            .ok();
+        });
+
+        block!(timer0.wait()).unwrap();
     }
+}
+
+#[interrupt]
+fn USB_DEVICE() {
+    critical_section::with(|cs| {
+        let mut usb_serial = USB_SERIAL.borrow_ref_mut(cs);
+        let usb_serial = usb_serial.as_mut().unwrap();
+        writeln!(usb_serial, "USB serial interrupt").unwrap();
+        while let nb::Result::Ok(c) = usb_serial.read_byte() {
+            writeln!(usb_serial, "Read byte: {:02x}", c).unwrap();
+        }
+        usb_serial.reset_rx_packet_recv_interrupt();
+    });
 }
