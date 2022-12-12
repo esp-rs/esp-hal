7d0a2e0ad977176e93073d11df3ef860f76b7b53
Scott Mabin <scott@mabez.dev>
2022-12-12 14:56:24 +0100

    syncronise hello world example

--- esp32c3-hal/examples/hello_world.rs
+++ esp32c3-hal/examples/hello_world.rs
@@ -25,18 +25,12 @@ fn main() -> ! {
     let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
 
     let mut rtc = Rtc::new(peripherals.RTC_CNTL);
-    let mut uart = peripherals.UART0;
+    let mut uart0 = Uart::new(peripherals.UART0);
     let timer_group0 = TimerGroup::new(peripherals.TIMG0, &clocks);
     let mut timer0 = timer_group0.timer0;
     let mut wdt0 = timer_group0.wdt;
     let timer_group1 = TimerGroup::new(peripherals.TIMG1, &clocks);
     let mut wdt1 = timer_group1.wdt;
-    
-    let mut uart0 = Uart::new(&mut uart);
-    writeln!(uart0, "Test 1").unwrap();
-    drop(uart0); // this ends the mutable borrow of uart, its now available in the peripheral struct again
-    let mut uart0 = Uart::new(uart); // construct via move this time
-    writeln!(uart0, "Test 2").unwrap();
 
     // Disable watchdog timers
     rtc.swd.disable();
