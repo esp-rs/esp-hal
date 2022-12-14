86ed5aa0d984f5648689dd344b73c8cb25d425ac
Jesse Braham <jesse@beta7.io>
2022-12-14 09:22:15 +0100

    Add SHA to list of peripherals to be created

--- esp-hal-common/src/peripherals/esp32.rs
+++ esp-hal-common/src/peripherals/esp32.rs
@@ -55,6 +55,7 @@ mod peripherals {
         I2C0,
         I2C1,
         RNG,
+        SHA,
         SPI0,
         SPI1,
         SPI2,
--- esp-hal-common/src/peripherals/esp32c2.rs
+++ esp-hal-common/src/peripherals/esp32c2.rs
@@ -36,6 +36,7 @@ mod peripherals {
     crate::create_peripherals! {
         I2C0,
         RNG,
+        SHA,
         SPI0,
         SPI1,
         SPI2,
