9ca1579ee8044b0cbdd5f30108d79c548e9aca77
Jesse Braham <jesse@beta7.io>
2023-01-04 10:21:44 -0800

    Update `twai` example for the C3 to match other chips

--- esp32c3-hal/examples/twai.rs
+++ esp32c3-hal/examples/twai.rs
@@ -1,15 +1,30 @@
+//! This example sends a CAN message to another ESP and receives it back.
+//!
+//! Wiring:
+//! This example works without CAN Transceivers by:
+//! * setting the tx pins to open drain
+//! * connecting all rx and tx pins together
+//! * adding a pull-up to the signal pins
+//!
+//! ESP1/GND --- ESP2/GND
+//! ESP1/IO2 --- ESP1/IO3 --- ESP2/IO2 --- ESP2/IO3 --- 4.8kOhm --- ESP1/5V
+//!
+//! `IS_FIRST_SENDER` below must be set to false on one of the ESP's
+
 #![no_std]
 #![no_main]
+
+const IS_FIRST_SENDER: bool = true;
 
 // Run this example with the eh1 feature enabled to use embedded-can instead of
 // embedded-hal-0.2.7. embedded-can was split off from embedded-hal before it's
 // upgrade to 1.0.0. cargo run --example twai --features eh1 --release
 #[cfg(feature = "eh1")]
-use embedded_can::{nb::Can, Frame, Id};
+use embedded_can::{nb::Can, Frame, StandardId};
 // Run this example without the eh1 flag to use the embedded-hal 0.2.7 CAN traits.
 // cargo run --example twai --release
 #[cfg(not(feature = "eh1"))]
-use embedded_hal::can::{Can, Frame, Id};
+use embedded_hal::can::{Can, Frame, StandardId};
 use esp32c3_hal::{
     clock::ClockControl,
     gpio::IO,
@@ -44,9 +59,8 @@ fn main() -> ! {
 
     let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
 
-    // Use GPIO pins 2 and 3 to connect to the respective pins on the CAN
-    // transceiver.
-    let can_tx_pin = io.pins.gpio2;
+    // Set the tx pin as open drain. Skip this if using transceivers.
+    let can_tx_pin = io.pins.gpio2.into_open_drain_output();
     let can_rx_pin = io.pins.gpio3;
 
     // The speed of the CAN bus.
@@ -67,10 +81,10 @@ fn main() -> ! {
     // undesired messages. Note that due to how the hardware filters messages,
     // standard ids and extended ids may both match a filter. Frame ids should
     // be explicitly checked in the application instead of fully relying on
-    // these partial acceptance filters to exactly match. A filter that matches
-    // standard ids of an even value.
+    // these partial acceptance filters to exactly match.
+    // A filter that matches StandardId::ZERO.
     const FILTER: twai::filter::SingleStandardFilter =
-        twai::filter::SingleStandardFilter::new(b"xxxxxxxxxx0", b"x", [b"xxxxxxxx", b"xxxxxxxx"]);
+        twai::filter::SingleStandardFilter::new(b"00000000000", b"x", [b"xxxxxxxx", b"xxxxxxxx"]);
     can_config.set_filter(FILTER);
 
     // Start the peripheral. This locks the configuration settings of the peripheral
@@ -78,31 +92,23 @@ fn main() -> ! {
     // received.
     let mut can = can_config.start();
 
-    loop {
-        // Wait for a frame to be received.
-        let frame = block!(can.receive()).unwrap();
-
-        println!("Received a frame:");
-
-        // Print different messages based on the frame id type.
-        match frame.id() {
-            Id::Standard(id) => {
-                println!("\tStandard Id: {:?}", id);
-            }
-            Id::Extended(id) => {
-                println!("\tExtended Id: {:?}", id);
-            }
-        }
-
-        // Print out the frame data or the requested data length code for a remote
-        // transmission request frame.
-        if frame.is_data_frame() {
-            println!("\tData: {:?}", frame.data());
-        } else {
-            println!("\tRemote Frame. Data Length Code: {}", frame.dlc());
-        }
-
-        // Transmit the frame back.
-        let _result = block!(can.transmit(&frame)).unwrap();
+    if IS_FIRST_SENDER {
+        // Send a frame to the other ESP
+        let frame = Frame::new(StandardId::ZERO, &[1, 2, 3]).unwrap();
+        block!(can.transmit(&frame)).unwrap();
+        println!("Sent a frame");
     }
+
+    // Wait for a frame to be received.
+    let frame = block!(can.receive()).unwrap();
+
+    println!("Received a frame: {frame:?}");
+
+    if !IS_FIRST_SENDER {
+        // Transmit the frame back to the other ESP
+        block!(can.transmit(&frame)).unwrap();
+        println!("Sent a frame");
+    }
+
+    loop {}
 }
