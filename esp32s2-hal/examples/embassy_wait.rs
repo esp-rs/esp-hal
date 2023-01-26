283b8b74cd7e94197a84029278967f06b3dabd6d
Scott Mabin <scott@mabez.dev>
2023-01-26 13:04:22 +0000

    Add esp32s2 wait example

---
+++ esp32s2-hal/examples/embassy_wait.rs
@@ -0,0 +1,68 @@
+#![no_std]
+#![no_main]
+#![feature(type_alias_impl_trait)]
+
+use embassy_executor::Executor;
+use embassy_time::{Duration, Timer};
+
+use esp32s2_hal::{
+    clock::ClockControl,
+    prelude::*,
+    timer::TimerGroup,
+    Rtc, embassy, peripherals::Peripherals, IO,
+};
+use esp_backtrace as _;
+use esp_hal_common::{PullDown, Input, Gpio0};
+use static_cell::StaticCell;
+
+use embedded_hal_async::digital::Wait;
+
+#[embassy_executor::task]
+async fn ping(mut pin: Gpio0<Input<PullDown>>) {
+    loop {
+        esp_println::println!("Waiting...");
+        pin.wait_for_rising_edge().await.unwrap();
+        esp_println::println!("Ping!");
+        Timer::after(Duration::from_millis(100)).await;
+    }
+}
+
+static EXECUTOR: StaticCell<Executor> = StaticCell::new();
+
+#[xtensa_lx_rt::entry]
+fn main() -> ! {
+    esp_println::println!("Init!");
+    let peripherals = Peripherals::take();
+    let system = peripherals.SYSTEM.split();
+    let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
+
+    let mut rtc = Rtc::new(peripherals.RTC_CNTL);
+    let timer_group0 = TimerGroup::new(peripherals.TIMG0, &clocks);
+    let mut wdt0 = timer_group0.wdt;
+    let timer_group1 = TimerGroup::new(peripherals.TIMG1, &clocks);
+    let mut wdt1 = timer_group1.wdt;
+
+    // Disable watchdog timers
+    rtc.rwdt.disable();
+    wdt0.disable();
+    wdt1.disable();
+
+    #[cfg(feature = "embassy-time-timg0")]
+    embassy::init(&clocks, timer_group0.timer0);
+
+    let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
+    // GPIO 0 as input
+    let input = io.pins.gpio0.into_pull_down_input();
+
+    // Async requires the GPIO interrupt to wake futures
+    esp32s2_hal::interrupt::enable(
+        esp32s2_hal::peripherals::Interrupt::GPIO,
+        esp32s2_hal::interrupt::Priority::Priority1,
+    )
+    .unwrap();
+
+    let executor = EXECUTOR.init(Executor::new());
+    executor.run(|spawner| {
+        spawner.spawn(ping(input)).ok();
+    });
+}
