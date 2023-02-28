39b4db0cf96b2c5ab9c39e5db720e6da9a6c6bd2
Jesse Braham <jesse@beta7.io>
2023-02-28 07:24:10 -0800

    Add examples for the `RNG` peripheral for all supported devices

---
+++ esp32c2-hal/examples/rng.rs
@@ -0,0 +1,44 @@
+//! Demonstrates the use of the hardware Random Number Generator (RNG)
+
+#![no_std]
+#![no_main]
+
+use esp32c2_hal::{
+    clock::ClockControl,
+    peripherals::Peripherals,
+    prelude::*,
+    timer::TimerGroup,
+    Rng,
+    Rtc,
+};
+use esp_backtrace as _;
+use esp_println::println;
+
+#[entry]
+fn main() -> ! {
+    let peripherals = Peripherals::take();
+    let system = peripherals.SYSTEM.split();
+    let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
+
+    let mut rtc = Rtc::new(peripherals.RTC_CNTL);
+    let timer_group0 = TimerGroup::new(peripherals.TIMG0, &clocks);
+    let mut wdt0 = timer_group0.wdt;
+
+    // Disable watchdog timers:
+    rtc.swd.disable();
+    rtc.rwdt.disable();
+    wdt0.disable();
+
+    // Instantiate the Random Number Generator peripheral:
+    let mut rng = Rng::new(peripherals.RNG);
+
+    // Generate a random word (u32):
+    println!("Random u32:   {}", rng.random());
+
+    // Fill a buffer with random bytes:
+    let mut buf = [0u8; 16];
+    rng.read(&mut buf).unwrap();
+    println!("Random bytes: {:?}", buf);
+
+    loop {}
+}
