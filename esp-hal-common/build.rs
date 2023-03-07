02cec63e77f2e5461b38c998ebe4cadf5e2bf020
Jesse Braham <jesse@beta7.io>
2023-03-07 09:52:04 -0800

    Add a couple cfg symbols, cleanup/organize some exports/modules

--- esp-hal-common/build.rs
+++ esp-hal-common/build.rs
@@ -34,23 +34,25 @@ fn main() {
     //   - the core count ('single_core' or 'multi_core')
     //
     // Additionally, the following symbols MAY be defined if present:
+    //   - 'aes'
     //   - 'dac'
     //   - 'gdma'
     //   - 'i2c1'
     //   - 'i2s'
     //   - 'mcpwm'
+    //   - 'pcnt'
     //   - 'pdma'
+    //   - 'plic'
+    //   - 'radio'
     //   - 'rmt'
     //   - 'spi3'
     //   - 'systimer'
     //   - 'timg0'
     //   - 'timg1'
+    //   - 'twai'
     //   - 'uart2'
     //   - 'usb_otg'
     //   - 'usb_serial_jtag'
-    //   - 'aes'
-    //   - 'plic'
-    //   - 'radio'
     //
     // New symbols can be added as needed, but please be sure to update both this
     // comment and the required vectors below.
@@ -58,19 +60,20 @@ fn main() {
         vec![
             "esp32",
             "xtensa",
-            "mcpwm",
             "multi_core",
+            "aes",
             "dac",
             "i2c1",
             "i2s",
+            "mcpwm",
+            "pcnt",
             "pdma",
+            "radio",
             "rmt",
             "spi3",
             "timg0",
             "timg1",
             "uart2",
-            "aes",
-            "radio",
         ]
     } else if esp32c2 {
         vec![
@@ -78,80 +81,86 @@ fn main() {
             "riscv",
             "single_core",
             "gdma",
+            "radio",
             "systimer",
             "timg0",
-            "radio",
         ]
     } else if esp32c3 {
         vec![
             "esp32c3",
             "riscv",
             "single_core",
+            "aes",
             "gdma",
             "i2s",
+            "radio",
             "rmt",
             "spi3",
             "systimer",
             "timg0",
             "timg1",
+            "twai",
             "usb_serial_jtag",
-            "aes",
-            "radio",
         ]
     } else if esp32c6 {
         vec![
             "esp32c6",
             "riscv",
             "single_core",
+            "aes",
             "gdma",
             "i2s",
             "mcpwm",
+            "pcnt",
+            "plic",
+            "radio",
             "rmt",
             "systimer",
             "timg0",
             "timg1",
+            "twai",
             "usb_serial_jtag",
-            "plic",
-            "aes",
-            "radio",
         ]
     } else if esp32s2 {
         vec![
             "esp32s2",
             "xtensa",
             "single_core",
+            "aes",
             "dac",
             "i2c1",
             "i2s",
+            "pcnt",
             "pdma",
+            "radio",
             "rmt",
             "spi3",
             "systimer",
             "timg0",
             "timg1",
             "usb_otg",
-            "aes",
-            "radio",
         ]
     } else if esp32s3 {
         vec![
             "esp32s3",
             "xtensa",
             "multi_core",
+            "aes",
             "gdma",
             "i2c1",
             "i2s",
             "mcpwm",
+            "pcnt",
+            "radio",
             "rmt",
             "spi3",
             "systimer",
             "timg0",
             "timg1",
+            "twai",
             "uart2",
             "usb_otg",
             "usb_serial_jtag",
-            "aes",
-            "radio",
         ]
     } else {
         unreachable!(); // We've already confirmed exactly one chip was selected
---
+++ esp-hal-common/src/interrupt/mod.rs
@@ -0,0 +1,9 @@
+#[cfg(riscv)]
+pub use riscv::*;
+#[cfg(xtensa)]
+pub use xtensa::*;
+
+#[cfg(riscv)]
+mod riscv;
+#[cfg(xtensa)]
+mod xtensa;
