bfc30896743582d3d0906f61c841d81052a77bc2
onsdagens <pawdzi-7@student.ltu.se>
2023-03-08 16:25:57 +0100

    Software interrupt example for esp32c3

---
+++ esp32c3-hal/examples/software_interrupts.rs
@@ -0,0 +1,106 @@
+//! GPIO interrupt
+//!
+//! This prints "Interrupt" when the boot button is pressed.
+//! It also blinks an LED like the blinky example.
+
+#![no_std]
+#![no_main]
+
+use core::cell::RefCell;
+
+use critical_section::Mutex;
+use esp32c3_hal::{
+    clock::ClockControl,
+    interrupt::{self},
+    peripherals::{self, Peripherals},
+    prelude::*,
+    riscv,
+    timer::TimerGroup,
+    Delay, Rtc,
+    system::{SoftwareInterrupt,SoftwareInterruptControl}
+};
+use esp_backtrace as _;
+
+static SWINT: Mutex<RefCell<Option<SoftwareInterruptControl>>> = Mutex::new(RefCell::new(None));
+
+#[entry]
+fn main() -> ! {
+    let peripherals = Peripherals::take();
+    let system =peripherals.SYSTEM.split();
+    let clockctrl = system.clock_control;
+    let sw_int = system.software_interrupt_control;
+    let clocks = ClockControl::boot_defaults(clockctrl).freeze();
+
+    // Disable the watchdog timers. For the ESP32-C3, this includes the Super WDT,
+    // the RTC WDT, and the TIMG WDTs.
+    let mut rtc = Rtc::new(peripherals.RTC_CNTL);
+    let timer_group0 = TimerGroup::new(peripherals.TIMG0, &clocks);
+    let mut wdt0 = timer_group0.wdt;
+    let timer_group1 = TimerGroup::new(peripherals.TIMG1, &clocks);
+    let mut wdt1 = timer_group1.wdt;
+
+    rtc.swd.disable();
+    rtc.rwdt.disable();
+    wdt0.disable();
+    wdt1.disable();
+
+    critical_section::with(|cs| SWINT.borrow_ref_mut(cs).replace(sw_int));
+
+    interrupt::enable(peripherals::Interrupt::FROM_CPU_INTR0, interrupt::Priority::Priority3).unwrap();
+    interrupt::enable(peripherals::Interrupt::FROM_CPU_INTR1, interrupt::Priority::Priority3).unwrap();
+    interrupt::enable(peripherals::Interrupt::FROM_CPU_INTR2, interrupt::Priority::Priority3).unwrap();
+    interrupt::enable(peripherals::Interrupt::FROM_CPU_INTR3, interrupt::Priority::Priority3).unwrap();
+    unsafe {riscv::interrupt::enable()}
+    let mut delay = Delay::new(&clocks);
+    let mut counter = 0;
+    loop {
+        delay.delay_ms(500u32);
+        match counter {           
+            0=>critical_section::with(|cs| {
+                    SWINT.borrow_ref_mut(cs).as_mut().unwrap().raise(SoftwareInterrupt::SoftwareInterrupt0);
+                    }),
+            1=>critical_section::with(|cs| {
+                    SWINT.borrow_ref_mut(cs).as_mut().unwrap().raise(SoftwareInterrupt::SoftwareInterrupt1);
+                    }),
+            2=>critical_section::with(|cs| {
+                    SWINT.borrow_ref_mut(cs).as_mut().unwrap().raise(SoftwareInterrupt::SoftwareInterrupt2);
+                    }),
+            3=>{critical_section::with(|cs| {
+                    SWINT.borrow_ref_mut(cs).as_mut().unwrap().raise(SoftwareInterrupt::SoftwareInterrupt3);
+                    });
+                counter = -1},
+            _=> {}
+        }
+        counter +=1;
+        
+    }
+}
+
+#[interrupt]
+fn FROM_CPU_INTR0() {
+    esp_println::println!("SW interrupt0");
+    critical_section::with(|cs| {
+        SWINT.borrow_ref_mut(cs).as_mut().unwrap().reset(SoftwareInterrupt::SoftwareInterrupt0);
+    });
+}
+#[interrupt]
+fn FROM_CPU_INTR1() {
+    esp_println::println!("SW interrupt1");
+    critical_section::with(|cs| {
+        SWINT.borrow_ref_mut(cs).as_mut().unwrap().reset(SoftwareInterrupt::SoftwareInterrupt1);
+    });
+}
+#[interrupt]
+fn FROM_CPU_INTR2() {
+    esp_println::println!("SW interrupt2");
+    critical_section::with(|cs| {
+        SWINT.borrow_ref_mut(cs).as_mut().unwrap().reset(SoftwareInterrupt::SoftwareInterrupt2);
+    });
+}
+#[interrupt]
+fn FROM_CPU_INTR3() {
+    esp_println::println!("SW interrupt3");
+    critical_section::with(|cs| {
+        SWINT.borrow_ref_mut(cs).as_mut().unwrap().reset(SoftwareInterrupt::SoftwareInterrupt3);
+    });
+}
