ddf0c0cb8c8d1b9d94c465e7398589bcdfaf423d
onsdagens <pawdzi-7@student.ltu.se>
2023-03-08 17:37:33 +0100

    Software interrupt example for esp32c3

--- esp32c3-hal/examples/software_interrupts.rs
+++ esp32c3-hal/examples/software_interrupts.rs
@@ -15,9 +15,10 @@ use esp32c3_hal::{
     peripherals::{self, Peripherals},
     prelude::*,
     riscv,
+    system::{SoftwareInterrupt, SoftwareInterruptControl},
     timer::TimerGroup,
-    Delay, Rtc,
-    system::{SoftwareInterrupt,SoftwareInterruptControl}
+    Delay,
+    Rtc,
 };
 use esp_backtrace as _;
 
@@ -26,7 +27,7 @@ static SWINT: Mutex<RefCell<Option<SoftwareInterruptControl>>> = Mutex::new(RefC
 #[entry]
 fn main() -> ! {
     let peripherals = Peripherals::take();
-    let system =peripherals.SYSTEM.split();
+    let system = peripherals.SYSTEM.split();
     let clockctrl = system.clock_control;
     let sw_int = system.software_interrupt_control;
     let clocks = ClockControl::boot_defaults(clockctrl).freeze();
@@ -46,33 +47,67 @@ fn main() -> ! {
 
     critical_section::with(|cs| SWINT.borrow_ref_mut(cs).replace(sw_int));
 
-    interrupt::enable(peripherals::Interrupt::FROM_CPU_INTR0, interrupt::Priority::Priority3).unwrap();
-    interrupt::enable(peripherals::Interrupt::FROM_CPU_INTR1, interrupt::Priority::Priority3).unwrap();
-    interrupt::enable(peripherals::Interrupt::FROM_CPU_INTR2, interrupt::Priority::Priority3).unwrap();
-    interrupt::enable(peripherals::Interrupt::FROM_CPU_INTR3, interrupt::Priority::Priority3).unwrap();
-    unsafe {riscv::interrupt::enable()}
+    interrupt::enable(
+        peripherals::Interrupt::FROM_CPU_INTR0,
+        interrupt::Priority::Priority3,
+    )
+    .unwrap();
+    interrupt::enable(
+        peripherals::Interrupt::FROM_CPU_INTR1,
+        interrupt::Priority::Priority3,
+    )
+    .unwrap();
+    interrupt::enable(
+        peripherals::Interrupt::FROM_CPU_INTR2,
+        interrupt::Priority::Priority3,
+    )
+    .unwrap();
+    interrupt::enable(
+        peripherals::Interrupt::FROM_CPU_INTR3,
+        interrupt::Priority::Priority3,
+    )
+    .unwrap();
+    unsafe { riscv::interrupt::enable() }
     let mut delay = Delay::new(&clocks);
     let mut counter = 0;
     loop {
         delay.delay_ms(500u32);
-        match counter {           
-            0=>critical_section::with(|cs| {
-                    SWINT.borrow_ref_mut(cs).as_mut().unwrap().raise(SoftwareInterrupt::SoftwareInterrupt0);
-                    }),
-            1=>critical_section::with(|cs| {
-                    SWINT.borrow_ref_mut(cs).as_mut().unwrap().raise(SoftwareInterrupt::SoftwareInterrupt1);
-                    }),
-            2=>critical_section::with(|cs| {
-                    SWINT.borrow_ref_mut(cs).as_mut().unwrap().raise(SoftwareInterrupt::SoftwareInterrupt2);
-                    }),
-            3=>{critical_section::with(|cs| {
-                    SWINT.borrow_ref_mut(cs).as_mut().unwrap().raise(SoftwareInterrupt::SoftwareInterrupt3);
-                    });
-                counter = -1},
-            _=> {}
+        match counter {
+            0 => critical_section::with(|cs| {
+                SWINT
+                    .borrow_ref_mut(cs)
+                    .as_mut()
+                    .unwrap()
+                    .raise(SoftwareInterrupt::SoftwareInterrupt0);
+            }),
+            1 => critical_section::with(|cs| {
+                SWINT
+                    .borrow_ref_mut(cs)
+                    .as_mut()
+                    .unwrap()
+                    .raise(SoftwareInterrupt::SoftwareInterrupt1);
+            }),
+            2 => critical_section::with(|cs| {
+                SWINT
+                    .borrow_ref_mut(cs)
+                    .as_mut()
+                    .unwrap()
+                    .raise(SoftwareInterrupt::SoftwareInterrupt2);
+            }),
+            3 => {
+                critical_section::with(|cs| {
+                    SWINT
+                        .borrow_ref_mut(cs)
+                        .as_mut()
+                        .unwrap()
+                        .raise(SoftwareInterrupt::SoftwareInterrupt3);
+                });
+                counter = 0;
+                continue;
+            }
+            _ => {}
         }
-        counter +=1;
-        
+        counter += 1;
     }
 }
 
@@ -80,27 +115,43 @@ fn main() -> ! {
 fn FROM_CPU_INTR0() {
     esp_println::println!("SW interrupt0");
     critical_section::with(|cs| {
-        SWINT.borrow_ref_mut(cs).as_mut().unwrap().reset(SoftwareInterrupt::SoftwareInterrupt0);
+        SWINT
+            .borrow_ref_mut(cs)
+            .as_mut()
+            .unwrap()
+            .reset(SoftwareInterrupt::SoftwareInterrupt0);
     });
 }
 #[interrupt]
 fn FROM_CPU_INTR1() {
     esp_println::println!("SW interrupt1");
     critical_section::with(|cs| {
-        SWINT.borrow_ref_mut(cs).as_mut().unwrap().reset(SoftwareInterrupt::SoftwareInterrupt1);
+        SWINT
+            .borrow_ref_mut(cs)
+            .as_mut()
+            .unwrap()
+            .reset(SoftwareInterrupt::SoftwareInterrupt1);
     });
 }
 #[interrupt]
 fn FROM_CPU_INTR2() {
     esp_println::println!("SW interrupt2");
     critical_section::with(|cs| {
-        SWINT.borrow_ref_mut(cs).as_mut().unwrap().reset(SoftwareInterrupt::SoftwareInterrupt2);
+        SWINT
+            .borrow_ref_mut(cs)
+            .as_mut()
+            .unwrap()
+            .reset(SoftwareInterrupt::SoftwareInterrupt2);
     });
 }
 #[interrupt]
 fn FROM_CPU_INTR3() {
     esp_println::println!("SW interrupt3");
     critical_section::with(|cs| {
-        SWINT.borrow_ref_mut(cs).as_mut().unwrap().reset(SoftwareInterrupt::SoftwareInterrupt3);
+        SWINT
+            .borrow_ref_mut(cs)
+            .as_mut()
+            .unwrap()
+            .reset(SoftwareInterrupt::SoftwareInterrupt3);
     });
 }
