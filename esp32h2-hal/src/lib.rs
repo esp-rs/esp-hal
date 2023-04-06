e985f249059d853023c0158a84548956d4d23e89
playfulFence <konnor1980@yandex.ru>
2023-04-06 20:00:20 +0200

    comment yet unimplemented includes

--- esp32h2-hal/src/lib.rs
+++ esp32h2-hal/src/lib.rs
@@ -6,9 +6,9 @@ pub use embedded_hal as ehal;
 pub use esp_hal_common::embassy;
 pub use esp_hal_common::*;
 
-pub use self::gpio::IO;
+// pub use self::gpio::IO;
 
-/// Common module for analog functions
-pub mod analog {
-    pub use esp_hal_common::analog::{AvailableAnalog, SarAdcExt};
-}
+// /// Common module for analog functions
+// pub mod analog {
+//     pub use esp_hal_common::analog::{AvailableAnalog, SarAdcExt};
+// }
