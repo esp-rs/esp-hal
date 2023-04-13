f27a0049fc999cda61ce80c00713e2501f2ee109
playfulFence <konnor1980@yandex.ru>
2023-04-13 16:44:56 +0200

    Cleaning code(1): Removed unnecessary moves, registers, addresses, functions

--- esp-hal-common/src/clock/clocks_ll/esp32h2.rs
+++ esp-hal-common/src/clock/clocks_ll/esp32h2.rs
@@ -11,25 +11,9 @@ const I2C_BBPLL_OC_REF_DIV : u8 = 2;
 const I2C_BBPLL_OC_REF_DIV_MSB : u8 = 3;
 const I2C_BBPLL_OC_REF_DIV_LSB : u8 = 0;
 
-
-const I2C_BBPLL_OC_DCHGP_LSB: u32 = 4; // CHECKME
-const I2C_BBPLL_OC_DCHGP_MSB: u32 = 6; // CHECKME
-
 const I2C_BBPLL_OC_DIV: u8 = 3;
 const I2C_BBPLL_OC_DIV_MSB: u8 = 5;
 const I2C_BBPLL_OC_DIV_LSB: u8 = 0;
-
-const I2C_BBPLL_OC_DR1: u8 = 5;
-const I2C_BBPLL_OC_DR1_MSB: u8 = 2;
-const I2C_BBPLL_OC_DR1_LSB: u8 = 0;
-
-const I2C_BBPLL_OC_DR3: u8 = 5;
-const I2C_BBPLL_OC_DR3_MSB: u8 = 6;
-const I2C_BBPLL_OC_DR3_LSB: u8 = 4;
-
-const I2C_BBPLL_OC_DCUR: u8 = 6;
-const I2C_BBPLL_OC_DCUR_MSB: u8 = 2; // potentially deleted
-const I2C_BBPLL_OC_DCUR_LSB: u8 = 0; // potentially deleted
 
 const I2C_BBPLL_OC_DHREF_SEL: u8 = 5;
 const I2C_BBPLL_OC_DHREF_SEL_MSB: u8 = 5;
@@ -39,10 +23,6 @@ const I2C_BBPLL_OC_DLREF_SEL: u8 = 5;
 const I2C_BBPLL_OC_DLREF_SEL_MSB: u8 = 7;
 const I2C_BBPLL_OC_DLREF_SEL_LSB: u8 = 6;
 
-const I2C_BBPLL_OC_VCO_DBIAS: u8 = 9;
-const I2C_BBPLL_OC_VCO_DBIAS_MSB: u8 = 1;
-const I2C_BBPLL_OC_VCO_DBIAS_LSB: u8 = 0;
-
 const I2C_MST_ANA_CONF0_REG: u32 = 0x6000E044;
 const I2C_MST_BBPLL_STOP_FORCE_HIGH: u32 = 1 << 2;
 const I2C_MST_BBPLL_STOP_FORCE_LOW: u32 = 1 << 3;
@@ -50,27 +30,27 @@ const I2C_MST_BBPLL_CAL_DONE: u32 = 1 << 24;
 
 const MODEM_LPCON_CLK_CONF_FORCE_ON_REG: u32 = DR_REG_MODEM_LPCON_BASE + 0xc;
 const MODEM_LPCON_CLK_I2C_MST_FO: u32 = 1 << 2;
-const MODEM_LPCON_I2C_CLK_CONF_REG: u32 = DR_REG_MODEM_LPCON_BASE + 0x8;
-const MODEM_LPCON_CLK_I2C_SEL_96M: u32 = 1 << 0;
+
+// May be needed for 96MHz
+// const MODEM_LPCON_I2C_CLK_CONF_REG: u32 = DR_REG_MODEM_LPCON_BASE + 0x8;
+// const MODEM_LPCON_CLK_I2C_SEL_96M: u32 = 1 << 0;
 
 const DR_REG_MODEM_LPCON_BASE: u32 = 0x600AD000;
 const MODEM_LPCON_CLK_CONF_REG: u32 = DR_REG_MODEM_LPCON_BASE + 0x8;
 const MODEM_LPCON_CLK_I2C_MST_EN: u32 = 1 << 2;
 
 const DR_REG_I2C_ANA_MST_BASE: u32 = 0x600AD800;
-//const LP_I2C_ANA_MST_DATE_REG: u32 = DR_REG_LP_I2C_ANA_MST_BASE + 0x3fc;  // CHECKME
 const I2C_MST_DATE_REG: u32 = DR_REG_I2C_ANA_MST_BASE + 0x34;
 const I2C_MST_ANA_CONF2_REG: u32 = DR_REG_I2C_ANA_MST_BASE + 0x20;
 const I2C_MST_ANA_CONF2:u32 = 1 << 18;
 const I2C_MST_CLK_EN: u32 = 1 << 28;    
 
+const REGI2C_BBPLL: u8 = 0x66;
 const REGI2C_BIAS: u8 = 0x6a;
 const REGI2C_PMU_REG: u8 = 0x6d;
 const REGI2C_ULP_CAL: u8 = 0x61;
 const REGI2C_SAR_I2C: u8 = 0x69;
 
-
-const LP_I2C_ANA_MST_DEVICE_EN_REG: u32 = DR_REG_I2C_ANA_MST_BASE + 0x14; // CHECKME
 const REGI2C_BBPLL_DEVICE_EN: u32 = 1 << 9; // (1 << 5) << 4;
 const REGI2C_BIAS_DEVICE_EN: u32 =  1 << 8; // (1 << 4) << 4;
 const REGI2C_PMU_DEVICE_EN: u32 = 1 << 12; // (1 << 8) << 4;
@@ -78,7 +58,6 @@ const REGI2C_ULP_CAL_DEVICE_EN: u32 = 1 << 10 ; // (1 << 6) << 4;
 const REGI2C_SAR_I2C_DEVICE_EN: u32 = 1 << 11; // (1 << 7) << 4;
 
 
-const REGI2C_RTC_BUSY: u32 = 1 << 25;
 const REGI2C_RTC_SLAVE_ID_V: u8 = 0xFF;
 const REGI2C_RTC_SLAVE_ID_S: u8 = 0;
 const REGI2C_RTC_ADDR_V: u8 = 0xFF;
@@ -97,14 +76,10 @@ const I2C_ANA_MST_I2C0_BUSY: u32 = 1 << 25;
 
 pub(crate) fn esp32h2_rtc_bbpll_configure(_xtal_freq: XtalClock, _pll_freq: PllClock) {
     unsafe {
+
         (MODEM_LPCON_CLK_CONF_FORCE_ON_REG as *mut u32).write_volatile(
             (MODEM_LPCON_CLK_CONF_FORCE_ON_REG as *mut u32).read_volatile() | MODEM_LPCON_CLK_I2C_MST_FO,
         );
-
-        // Set I2C clock to 96MHz
-        // (MODEM_LPCON_I2C_CLK_CONF_REG as *mut u32).write_volatile(
-        //     (MODEM_LPCON_I2C_CLK_CONF_REG as *mut u32).read_volatile() | MODEM_LPCON_CLK_I2C_SEL_96M,
-        // );
 
         let i2c_mst_ana_conf0_reg_ptr = I2C_MST_ANA_CONF0_REG as *mut u32;
 
@@ -279,7 +254,7 @@ fn clk_ll_bus_update()
         pcr.bus_clk_update
             .modify(|_, w| w.bus_clock_update().bit(true));
 
-        while (pcr.bus_clk_update.read().bus_clock_update().bit_is_set()) {}
+        while pcr.bus_clk_update.read().bus_clock_update().bit_is_set() {}
     }
 }
 
@@ -303,7 +278,7 @@ fn regi2c_enable_block(block: u8) {
         REGI2C_BIAS => {
             reg_set_bit(I2C_MST_ANA_CONF2_REG, REGI2C_BIAS_DEVICE_EN);
         }
-        REGI2C_DIG_REG => {
+        REGI2C_PMU_REG => {
             reg_set_bit(I2C_MST_ANA_CONF2_REG, REGI2C_PMU_DEVICE_EN);
         }
         REGI2C_ULP_CAL => {
@@ -324,7 +299,7 @@ fn regi2c_disable_block(block: u8) {
         REGI2C_BIAS => {
             reg_clr_bit(I2C_MST_ANA_CONF2_REG, REGI2C_BIAS_DEVICE_EN);
         }
-        REGI2C_DIG_REG => {
+        REGI2C_PMU_REG => {
             reg_clr_bit(I2C_MST_ANA_CONF2_REG, REGI2C_PMU_DEVICE_EN);
         }
         REGI2C_ULP_CAL => {
@@ -362,25 +337,6 @@ fn reg_get_bit(reg: u32, b: u32) -> u32 {
 
 fn reg_get_field(reg: u32, s: u32, v: u32) -> u32 {
     unsafe { ((reg as *mut u32).read_volatile() >> s) & v }
-}
-
-// fn reg_set_field(reg : u32, s: u32, v: u32) {
-//     unsafe {
-//         (reg as *mut u32).write_volatile(((reg as *mut u32).read_volatile() & !(v << s)) | (v & (v << s)));
-//     }
-// }
-
-pub(crate) fn regi2c_write(block: u8, _host_id: u8, reg_add: u8, data: u8) {
-    regi2c_enable_block(block);
-
-    let temp: u32 = ((block as u32 & REGI2C_RTC_SLAVE_ID_V as u32) << REGI2C_RTC_SLAVE_ID_S as u32)
-                    | ((reg_add as u32 & REGI2C_RTC_ADDR_V as u32) << REGI2C_RTC_ADDR_S as u32)
-                    | ((0x1 & REGI2C_RTC_WR_CNTL_V as u32) << REGI2C_RTC_WR_CNTL_S as u32) // 0: READ I2C register; 1: Write I2C register;
-                    | (((data as u32) & REGI2C_RTC_DATA_V as u32) << REGI2C_RTC_DATA_S as u32);
-    reg_write(I2C_MST_I2C0_CTRL_REG, temp);
-    while reg_get_bit(I2C_MST_I2C0_CTRL_REG, REGI2C_RTC_BUSY) != 0 {}
-
-    regi2c_disable_block(block);
 }
 
 pub(crate) fn regi2c_write_mask(block: u8, _host_id: u8, reg_add: u8, msb: u8, lsb: u8, data: u8) {
