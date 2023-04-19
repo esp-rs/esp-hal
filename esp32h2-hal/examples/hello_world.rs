2755bb7d36b8da619067ad3aa65b1c8ca25bad70
playfulFence <konnor1980@yandex.ru>
2023-04-19 17:46:36 +0200

    Adjust for rustfmt

--- esp32h2-hal/examples/hello_world.rs
+++ esp32h2-hal/examples/hello_world.rs
@@ -1,9 +1,14 @@
 #![no_std]
 #![no_main]
 
+use esp32h2_hal::{
+    clock::{ClockControl, CpuClock},
+    entry,
+    peripherals::Peripherals,
+    prelude::*,
+};
 use esp_backtrace as _;
 use esp_println::println;
-use esp32h2_hal::{clock::{ClockControl, CpuClock}, peripherals::Peripherals, entry, prelude::*};
 
 #[entry]
 fn main() -> ! {
