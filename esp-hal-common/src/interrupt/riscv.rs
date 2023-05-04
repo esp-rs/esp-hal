3b17f4289f8f42abcc71fa52db6f69139e909fc0
onsdagens <112828711+onsdagens@users.noreply.github.com>
2023-05-04 02:35:46 +0200

    Rolling back an irrelevant change

--- esp-hal-common/src/interrupt/riscv.rs
+++ esp-hal-common/src/interrupt/riscv.rs
@@ -417,7 +417,7 @@ pub unsafe extern "C" fn start_trap_rust_hal(trap_frame: *mut TrapFrame) {
 /// This function is called from an trap handler.
 #[doc(hidden)]
 unsafe fn handle_exception(pc: usize, trap_frame: *mut TrapFrame) {
-    let insn = *(pc as *const _);
+    let insn: usize = *(pc as *const _);
     let needs_atomic_emulation = (insn & 0b1111111) == 0b0101111;
     if !needs_atomic_emulation {
         extern "C" {
