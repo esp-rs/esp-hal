25951c04ea411c25aab6dbfcfef2d48cd3247733
Scott Mabin <scott@mabez.dev>
2023-05-16 13:42:42 +0100

    Simplify GPIO interrupt status

--- esp-hal-common/src/soc/esp32h2/gpio.rs
+++ esp-hal-common/src/soc/esp32h2/gpio.rs
@@ -7,6 +7,8 @@ use crate::{
         GpioPin,
         InputOutputAnalogPinType,
         InputOutputPinType,
+        InteruptStatusRegisterAccess,
+        InteruptStatusRegisterAccessBank0,
         Unknown,
     },
     peripherals::GPIO,
@@ -217,7 +219,6 @@ pub enum OutputSignal {
 
 // FIXME: add alternate function numbers/signals where necessary
 crate::gpio::gpio! {
-    Single,
     (0, 0, InputOutput)
     (1, 0, InputOutputAnalog)
     (2, 0, InputOutputAnalog)
@@ -254,6 +255,16 @@ crate::gpio::analog! {
     3
     4
     5
+}
+
+impl InteruptStatusRegisterAccess for InteruptStatusRegisterAccessBank0 {
+    fn pro_cpu_interrupt_status_read() -> u32 {
+        unsafe { &*GPIO::PTR }.pcpu_int.read().bits()
+    }
+
+    fn pro_cpu_nmi_status_read() -> u32 {
+        unsafe { &*GPIO::PTR }.pcpu_nmi_int.read().bits()
+    }
 }
 
 // TODO USB pins
