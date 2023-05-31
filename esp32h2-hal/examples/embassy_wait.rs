e096bca561e53646c602df752c16b91c8f4706db
Jesse Braham <jessebraham@users.noreply.github.com>
2023-05-31 07:17:39 -0700

    Add `embassy_serial` and `embassy_wait` examples for ESP32-H2 (#569)
    * Add `embassy_{serial,wait}` examples for ESP32-H2
    
    * Update the CHANGELOG

---
+++ esp32h2-hal/examples/embassy_wait.rs
@@ -0,0 +1,88 @@
+//! embassy wait
+//!
+//! This is an example of asynchronously `Wait`ing for a pin state to change.
+
+#![no_std]
+#![no_main]
+#![feature(type_alias_impl_trait)]
+
+use embassy_executor::Executor;
+use embassy_time::{Duration, Timer};
+use embedded_hal_async::digital::Wait;
+use esp32h2_hal::{
+    clock::ClockControl,
+    embassy,
+    gpio::{Gpio9, Input, PullDown},
+    peripherals::Peripherals,
+    prelude::*,
+    timer::TimerGroup,
+    Rtc,
+    IO,
+};
+use esp_backtrace as _;
+use static_cell::StaticCell;
+
+#[embassy_executor::task]
+async fn ping(mut pin: Gpio9<Input<PullDown>>) {
+    loop {
+        esp_println::println!("Waiting...");
+        pin.wait_for_rising_edge().await.unwrap();
+        esp_println::println!("Ping!");
+        Timer::after(Duration::from_millis(100)).await;
+    }
+}
+
+static EXECUTOR: StaticCell<Executor> = StaticCell::new();
+
+#[entry]
+fn main() -> ! {
+    esp_println::println!("Init!");
+    let peripherals = Peripherals::take();
+    let mut system = peripherals.PCR.split();
+    let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
+
+    let mut rtc = Rtc::new(peripherals.LP_CLKRST);
+    let timer_group0 = TimerGroup::new(
+        peripherals.TIMG0,
+        &clocks,
+        &mut system.peripheral_clock_control,
+    );
+    let mut wdt0 = timer_group0.wdt;
+    let timer_group1 = TimerGroup::new(
+        peripherals.TIMG1,
+        &clocks,
+        &mut system.peripheral_clock_control,
+    );
+    let mut wdt1 = timer_group1.wdt;
+
+    // Disable watchdog timers
+    rtc.swd.disable();
+    rtc.rwdt.disable();
+    wdt0.disable();
+    wdt1.disable();
+
+    #[cfg(feature = "embassy-time-systick")]
+    embassy::init(
+        &clocks,
+        esp32h2_hal::systimer::SystemTimer::new(peripherals.SYSTIMER),
+    );
+
+    #[cfg(feature = "embassy-time-timg0")]
+    embassy::init(&clocks, timer_group0.timer0);
+
+    let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
+    // GPIO 9 as input
+    let input = io.pins.gpio9.into_pull_down_input();
+
+    // Async requires the GPIO interrupt to wake futures
+    esp32h2_hal::interrupt::enable(
+        esp32h2_hal::peripherals::Interrupt::GPIO,
+        esp32h2_hal::interrupt::Priority::Priority1,
+    )
+    .unwrap();
+
+    let executor = EXECUTOR.init(Executor::new());
+    executor.run(|spawner| {
+        spawner.spawn(ping(input)).ok();
+    });
+}
