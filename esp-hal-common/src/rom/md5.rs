3732a66f948c311bb74a43c7dff1040310af3e7a
Jordan Halase <jordan@halase.me>
2023-06-22 21:08:24 -0500

    Implement traits and add comments to MD5 module

--- esp-hal-common/src/rom/md5.rs
+++ esp-hal-common/src/rom/md5.rs
@@ -1,15 +1,20 @@
-//! MD5 Message-Digest Algorithm 
-
-use core::mem::MaybeUninit;
+//! MD5 Message-Digest Algorithm
 
 #[allow(unused)]
-use core::ffi::{c_int, c_void};
+use core::ffi::{c_int, c_uchar, c_void};
+use core::{
+    convert::From,
+    fmt,
+    mem::MaybeUninit,
+    ops::{Deref, DerefMut},
+};
 
 // If there is not exactly one of the MD5 variations defined in the device
 // toml file then `InternalContext` will be either undefined or multiple
 // defined and this module will fail to compile letting you know to fix it
 
 #[cfg(rom_md5_bsd)]
+#[derive(Clone)]
 #[repr(C)]
 struct InternalContext {
     buf: [u32; 4],
@@ -25,30 +30,33 @@ extern "C" {
 }
 
 #[cfg(rom_md5_mbedtls)]
+#[derive(Clone)]
 #[repr(C)]
 struct InternalContext {
     total: [u32; 2],
     state: [u32; 4],
-    buffer: [core::ffi::c_uchar; 64],
+    buffer: [c_uchar; 64],
 }
 
 #[cfg(rom_md5_mbedtls)]
 extern "C" {
     fn esp_rom_mbedtls_md5_starts_ret(context: *mut InternalContext) -> c_int;
-    fn esp_rom_mbedtls_md5_update_ret(context: *mut InternalContext, buf: *const c_void, len: u32);
-    fn esp_rom_mbedtls_md5_finish_ret(context: *mut InternalContext, digest: *mut u8);
+    fn esp_rom_mbedtls_md5_update_ret(
+        context: *mut InternalContext,
+        buf: *const c_void,
+        len: u32,
+    ) -> c_int;
+    fn esp_rom_mbedtls_md5_finish_ret(context: *mut InternalContext, digest: *mut u8) -> c_int;
 }
 
+#[derive(Clone)]
 pub struct Context(InternalContext);
 
-pub struct Digest(pub [u8; 16]);
-
 impl Context {
-
+    /// Create a new MD5 context
     #[inline]
     pub fn new() -> Self {
         let mut ctx = MaybeUninit::<InternalContext>::uninit();
-
         unsafe {
             #[cfg(rom_md5_bsd)]
             esp_rom_md5_init(ctx.as_mut_ptr());
@@ -60,30 +68,109 @@ impl Context {
         }
     }
 
+    /// Feed data to the hasher
     #[inline]
     pub fn consume<T: AsRef<[u8]>>(&mut self, data: T) {
         let data = data.as_ref();
         unsafe {
             #[cfg(rom_md5_bsd)]
-            esp_rom_md5_update(&mut self.0 as *mut _, data.as_ptr() as *const c_void, data.len() as u32);
+            esp_rom_md5_update(
+                &mut self.0 as *mut _,
+                data.as_ptr() as *const c_void,
+                data.len() as u32,
+            );
 
             #[cfg(rom_md5_mbedtls)]
-            let _ = esp_rom_mbedtls_md5_update_ret(&mut self.0 as *mut _, data.as_ptr() as *const c_void, data.len() as u32);
+            let _ = esp_rom_mbedtls_md5_update_ret(
+                &mut self.0 as *mut _,
+                data.as_ptr() as *const c_void,
+                data.len() as u32,
+            );
         }
     }
 
+    /// Finalize and return a digest
     #[inline]
     pub fn compute(mut self) -> Digest {
         let mut digest = MaybeUninit::<[u8; 16]>::uninit();
-
         unsafe {
             #[cfg(rom_md5_bsd)]
             esp_rom_md5_final(digest.as_mut_ptr() as *mut _, &mut self.0 as *mut _);
 
             #[cfg(rom_md5_mbedtls)]
-            let _ = esp_rom_mbedtls_md5_finish_ret(&mut self.0 as *mut _, digest.as_mut_ptr() as *mut _);
+            let _ = esp_rom_mbedtls_md5_finish_ret(
+                &mut self.0 as *mut _,
+                digest.as_mut_ptr() as *mut _,
+            );
 
             Digest(digest.assume_init())
         }
+    }
+}
+
+/// Compute a full digest from a single buffer
+#[inline]
+pub fn compute<T: AsRef<[u8]>>(data: T) -> Digest {
+    let mut ctx = Context::new();
+    ctx.consume(data);
+    ctx.compute()
+}
+
+#[repr(transparent)]
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
+pub struct Digest(pub [u8; 16]);
+
+impl fmt::LowerHex for Digest {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for &byte in &self.0 {
+            write!(f, "{:02x}", byte)?;
+        }
+        Ok(())
+    }
+}
+
+impl fmt::UpperHex for Digest {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for &byte in &self.0 {
+            write!(f, "{:02X}", byte)?;
+        }
+        Ok(())
+    }
+}
+
+impl fmt::Display for Digest {
+    #[inline]
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        <Digest as fmt::LowerHex>::fmt(self, f)
+    }
+}
+
+impl fmt::Debug for Digest {
+    #[inline]
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        <Digest as fmt::LowerHex>::fmt(self, f)
+    }
+}
+
+impl From<Digest> for [u8; 16] {
+    #[inline]
+    fn from(digest: Digest) -> Self {
+        digest.0
+    }
+}
+
+impl Deref for Digest {
+    type Target = [u8; 16];
+
+    #[inline]
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for Digest {
+    #[inline]
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
     }
 }
