39146f6ed919fe24ae1dbf5d360a154463abb981
bjoernQ <bjoern.quentin@mobile-j.de>
2023-07-07 11:06:39 +0200

    Add a new RMT driver

--- esp-hal-common/src/soc/esp32c3/mod.rs
+++ esp-hal-common/src/soc/esp32c3/mod.rs
@@ -10,4 +10,9 @@ pub(crate) mod registers {
 pub(crate) mod constants {
     pub const I2S_SCLK: u32 = 160_000_000;
     pub const I2S_DEFAULT_CLK_SRC: u8 = 2;
+
+    pub const RMT_RAM_START: usize = 0x60016400;
+    pub const RMT_CHANNEL_RAM_SIZE: usize = 48;
+    pub const RMT_CLOCK_SRC: u8 = 1;
+    pub const RMT_CLOCK_SRC_FREQ: fugit::HertzU32 = fugit::HertzU32::MHz(80);
 }
