81fa80d0074b05891a9a4b60c680e972f85fcfc5
Dániel Buga <bugadani@gmail.com>
2023-07-21 13:15:59 +0200

    Add note about xtensa

--- esp-hal-common/src/gpio.rs
+++ esp-hal-common/src/gpio.rs
@@ -1859,6 +1859,8 @@ mod asynch {
 
         let mut intr_bits = intrs;
         while intr_bits != 0 {
+            // TODO: we should probably call `leading_zeros` on Xtensa
+            // when that lowers to NSAU.
             let pin_nr = intr_bits.trailing_zeros();
             set_int_enable(pin_nr as u8, 0, 0, false);
             PIN_WAKERS[pin_nr as usize].wake(); // wake task
