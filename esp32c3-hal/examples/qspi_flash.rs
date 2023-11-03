a6db22d16206d8176a8db31681a1df8810019fa9
sanxus <121212349+sanxus@users.noreply.github.com>
2023-11-04 01:34:25 +0800

    fix (maybe?) all broken examples caused by the change (esp32c3-hal)

--- esp32c3-hal/examples/qspi_flash.rs
+++ esp32c3-hal/examples/qspi_flash.rs
@@ -56,15 +56,17 @@ fn main() -> ! {
 
     let mut spi = Spi::new_half_duplex(
         peripherals.SPI2,
+        100u32.kHz(),
+        SpiMode::Mode0,
+        &clocks,
+    )
+    .with_pins(
         Some(sclk),
         Some(mosi),
         Some(miso),
         Some(sio2),
         Some(sio3),
         Some(cs),
-        100u32.kHz(),
-        SpiMode::Mode0,
-        &clocks,
     )
     .with_dma(dma_channel.configure(
         false,
