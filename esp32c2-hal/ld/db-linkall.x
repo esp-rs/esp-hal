fb31f868f103b54e05412e721f39a82f18866c26
Scott Mabin <scott@mabez.dev>
2023-11-07 14:34:46 +0000

    Remove direct boot support from esp-hal (#903)
    * Remove direct boot support from esp-hal
    
    * changelog

--- esp32c2-hal/ld/db-linkall.x
+++
@@ -1,3 +0,0 @@
-INCLUDE "esp32c2-link.x"
-INCLUDE "hal-defaults.x"
-INCLUDE "rom-functions.x"
--- esp32h2-hal/build.rs
+++ esp32h2-hal/build.rs
@@ -1,6 +1,5 @@
 use std::{env, error::Error, fs, path::PathBuf};
 
-#[cfg(not(feature = "direct-boot"))]
 fn main() -> Result<(), Box<dyn Error>> {
     // Put the linker script somewhere the linker can find it
     let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
@@ -9,29 +8,6 @@ fn main() -> Result<(), Box<dyn Error>> {
     fs::copy("ld/bl-esp32h2-memory.x", out.join("memory.x"))?;
     fs::copy("ld/bl-riscv-link.x", out.join("bl-riscv-link.x"))?;
     fs::copy("ld/bl-linkall.x", out.join("linkall.x"))?;
-
-    fs::copy("ld/rom-functions.x", out.join("rom-functions.x"))?;
-
-    // Only re-run the build script when memory.x is changed,
-    // instead of when any part of the source code changes.
-    println!("cargo:rerun-if-changed=ld/memory.x");
-
-    #[cfg(feature = "defmt")]
-    println!("cargo:rustc-link-arg=-Tdefmt.x");
-
-    Ok(())
-}
-
-#[cfg(feature = "direct-boot")]
-fn main() -> Result<(), Box<dyn Error>> {
-    // Put the linker script somewhere the linker can find it
-    let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
-    println!("cargo:rustc-link-search={}", out.display());
-
-    fs::copy("ld/db-esp32h2-memory.x", out.join("memory.x"))?;
-    fs::copy("ld/db-esp32h2-link.x", out.join("esp32h2-link.x"))?;
-    fs::copy("ld/db-riscv-link.x", out.join("riscv-link.x"))?;
-    fs::copy("ld/db-linkall.x", out.join("linkall.x"))?;
 
     fs::copy("ld/rom-functions.x", out.join("rom-functions.x"))?;
 
