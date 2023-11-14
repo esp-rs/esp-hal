9f3b53814b17149d2d2caed5bed39dbda3d624c9
bjoernQ <bjoern.quentin@mobile-j.de>
2023-11-14 16:11:11 +0100

    Remove WDT disabling from remaining examples

--- esp32c3-hal/examples/spi_slave_dma.rs
+++ esp32c3-hal/examples/spi_slave_dma.rs
@@ -37,9 +37,7 @@ use esp32c3_hal::{
         slave::{prelude::*, Spi},
         SpiMode,
     },
-    timer::TimerGroup,
     Delay,
-    Rtc,
 };
 use esp_backtrace as _;
 use esp_println::println;
@@ -49,19 +47,6 @@ fn main() -> ! {
     let peripherals = Peripherals::take();
     let system = peripherals.SYSTEM.split();
     let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
-
-    // Disable the watchdog timers. For the ESP32-C3, this includes the Super WDT,
-    // the RTC WDT, and the TIMG WDTs.
-    let mut rtc = Rtc::new(peripherals.RTC_CNTL);
-    let timer_group0 = TimerGroup::new(peripherals.TIMG0, &clocks);
-    let mut wdt0 = timer_group0.wdt;
-    let timer_group1 = TimerGroup::new(peripherals.TIMG1, &clocks);
-    let mut wdt1 = timer_group1.wdt;
-
-    rtc.swd.disable();
-    rtc.rwdt.disable();
-    wdt0.disable();
-    wdt1.disable();
 
     let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
     let slave_sclk = io.pins.gpio6;
