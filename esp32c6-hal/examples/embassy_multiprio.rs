89e86834261eca55ba3ac4d7294d86edaedcf773
Dániel Buga <bugadani@gmail.com>
2023-11-17 17:28:00 +0100

    Unify multiprio examples

--- esp32c6-hal/examples/embassy_multiprio.rs
+++ esp32c6-hal/examples/embassy_multiprio.rs
@@ -1,15 +1,26 @@
 //! This example shows how to use the interrupt executors to prioritize some
-//! tasks over others. The low priority task will not be able to run its async
-//! task while the blocking task is running, but the high priority task will be
-//! able to blink the LED regardless.
+//! tasks over others.
+//!
+//! The example creates three tasks:
+//!  - A low priority task that is not actually async, but simulates some
+//!    blocking work. This task will run for 5 seconds, then sleep for 5
+//!    seconds.
+//!  - A low priority task that is actually async, but will not be able to run
+//!    while the blocking task is running.
+//!  - A high priority task that prints something every second. The example
+//!    demonstrates that this task will continue to run even while the low
+//!    priority blocking task is running.
 
 #![no_std]
 #![no_main]
 #![feature(type_alias_impl_trait)]
 
 use embassy_executor::Spawner;
-use embassy_time::{Duration, Instant, Ticker};
-use esp32c6_hal::{
+use embassy_time::{Duration, Instant, Ticker, Timer};
+use esp32c6_hal as hal;
+use esp_backtrace as _;
+use esp_println::println;
+use hal::{
     clock::ClockControl,
     embassy::{
         self,
@@ -19,8 +30,6 @@ use esp32c6_hal::{
     peripherals::Peripherals,
     prelude::*,
 };
-use esp_backtrace as _;
-use esp_println::println;
 
 static INT_EXECUTOR_0: InterruptExecutor<FromCpu1> = InterruptExecutor::new();
 
@@ -49,6 +58,18 @@ async fn low_prio_blocking() {
         let start = Instant::now();
         while start.elapsed() < Duration::from_secs(5) {}
         println!("Calculation finished");
+        Timer::after(Duration::from_secs(5)).await;
+    }
+}
+
+/// A well-behaved, but starved async task.
+#[embassy_executor::task]
+async fn low_prio_async() {
+    println!("Starting low-priority task that will not be able to run while the blocking task is running");
+    let mut ticker = Ticker::every(Duration::from_secs(1));
+    loop {
+        println!("Low priority ticks");
+        ticker.next().await;
     }
 }
 
@@ -62,18 +83,19 @@ async fn main(low_prio_spawner: Spawner) {
     #[cfg(feature = "embassy-time-systick")]
     embassy::init(
         &clocks,
-        esp32c6_hal::systimer::SystemTimer::new(peripherals.SYSTIMER),
+        hal::systimer::SystemTimer::new(peripherals.SYSTIMER),
     );
 
     #[cfg(feature = "embassy-time-timg0")]
     {
-        let timer_group0 = esp32c6_hal::timer::TimerGroup::new(peripherals.TIMG0, &clocks);
+        let timer_group0 = hal::timer::TimerGroup::new(peripherals.TIMG0, &clocks);
         embassy::init(&clocks, timer_group0.timer0);
     }
 
     let spawner = INT_EXECUTOR_0.start(Priority::Priority2);
     spawner.must_spawn(high_prio());
 
-    println!("spawning");
+    println!("Spawning low-priority tasks");
+    low_prio_spawner.must_spawn(low_prio_async());
     low_prio_spawner.must_spawn(low_prio_blocking());
 }
