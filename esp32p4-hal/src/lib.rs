e1ccb66a8261f23c884748efa42a6c7c4ed6db6e
Jesse Braham <jesse@beta7.io>
2024-01-19 08:30:44 -0800

    Update package-level documentation for each chip-specific HAL package

--- esp32p4-hal/src/lib.rs
+++ esp32p4-hal/src/lib.rs
@@ -1,11 +1,9 @@
-//! `no_std` HAL for the ESP32-P4 from Espressif.
+//! A bare-metal (`no_std`) Hardware Abstraction Layer for the ESP32-P4 from
+//! Espressif.
 //!
-//! Implements a number of the traits defined by the various packages in the
-//! [embedded-hal] repository.
-//!
-//! [embedded-hal]: https://github.com/rust-embedded/embedded-hal
-
-#![no_std]
+//! ## Feature Flags
+#![doc = document_features::document_features!()]
 #![doc(html_logo_url = "https://avatars.githubusercontent.com/u/46717278")]
+#![no_std]
 
 pub use esp_hal_common::*;
