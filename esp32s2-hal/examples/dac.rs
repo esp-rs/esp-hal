5cc6e95d0b738707f0536f74331f540eba9c8660
Jesse Braham <jesse@beta7.io>
2024-01-19 07:05:54 -0800

    Refactor the `analog::dac` module

--- esp32s2-hal/examples/dac.rs
+++ esp32s2-hal/examples/dac.rs
@@ -7,7 +7,7 @@
 
 use esp32s2_hal::{
     clock::ClockControl,
-    dac,
+    dac::{DAC1, DAC2},
     gpio::IO,
     peripherals::Peripherals,
     prelude::*,
@@ -26,9 +26,8 @@ fn main() -> ! {
     let pin18 = io.pins.gpio18.into_analog();
 
     // Create DAC instances
-    let analog = peripherals.SENS.split();
-    let mut dac1 = dac::DAC1::dac(analog.dac1, pin17).unwrap();
-    let mut dac2 = dac::DAC2::dac(analog.dac2, pin18).unwrap();
+    let mut dac1 = DAC1::new(peripherals.DAC1, pin17);
+    let mut dac2 = DAC2::new(peripherals.DAC2, pin18);
 
     let mut delay = Delay::new(&clocks);
 
