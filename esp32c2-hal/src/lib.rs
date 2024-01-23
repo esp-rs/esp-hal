5391e686685013c7fe5c556f7ff656ce30a9fc0e
Jesse Braham <jessebraham@users.noreply.github.com>
2024-01-23 15:49:06 +0000

    Use the `document-features` crate instead of manually writing package feature docs (#1107)
    * Add `document-features` dependency to all HAL packages
    
    * Update package-level documentation for each chip-specific HAL package
    
    * Document package features for all chip-specific HALs

--- esp32c2-hal/src/lib.rs
+++ esp32c2-hal/src/lib.rs
@@ -1,57 +1,9 @@
-//! `no_std` HAL for the ESP32-C2/ESP8684 from Espressif.
+//! A bare-metal (`no_std`) Hardware Abstraction Layer for the ESP32-C2 from
+//! Espressif.
 //!
-//! Implements a number of the traits defined by the various packages in the
-//! [embedded-hal] repository.
-//!
-//! [embedded-hal]: https://github.com/rust-embedded/embedded-hal
-//!
-//! ### Cargo Features
-//!
-//! - `async` - Enable support for asynchronous operation, with interfaces
-//!   provided by [embedded-hal-async] and [embedded-io-async]
-//! - `debug` - Enable debug features in the HAL (used for development)
-//! - `defmt` - Enable [`defmt::Format`] on certain types
-//! - `direct-vectoring` - Enable direct vector table hooking support
-//! - `eh1` - Implement the traits defined in the `1.0.0-xxx` pre-releases of
-//!   [embedded-hal], [embedded-hal-nb], and [embedded-io]
-//! - `embassy` - Enable support for [embassy], a modern asynchronous embedded
-//!   framework. One of `embassy-time-*` features must also be enabled when
-//!   using this feature.
-//! - `embassy-time-systick` - Enable the [embassy] time driver using the
-//!   `SYSTIMER` peripheral. The `SYSTIMER` peripheral has three alarms
-//!   available for use
-//! - `embassy-time-timg0` - Enable the [embassy] time driver using the `TIMG0`
-//!   peripheral. The `TIMG0` peripheral has a single alarm available for use
-//! - `embassy-integrated-timers` - Uses hardware timers as alarms for the
-//!   executors. Using this feature limits the number of executors to the number
-//!   of hardware alarms provided by the time driver
-//! - `embassy-generic-queue-N` (where `N` can be `8`, `16`, `32`, `64` or
-//!   `128`) - Use a generic timer queue of size `N` for the executors' timer
-//!   queues. Using this feature can expand the number of executors you can use
-//!   to `N`
-//! - `interrupt-preemption` - Enable priority-based interrupt preemption
-//! - `log` - enable log output using the `log` crate
-//! - `rt` - Runtime support
-//! - `ufmt` - Implement the [`ufmt_write::uWrite`] trait for the UART and USB
-//!   Serial JTAG drivers
-//! - `vectored` - Enable interrupt vectoring
-//! - `xtal-26mhz` - The target device uses a 26MHz crystal
-//! - `xtal-40mhz` - The target device uses a 40MHz crystal
-//!
-//! #### Default Features
-//!
-//! The `rt`, `vectored`, `xtal-40mhz` and `embassy-integrated-timers` features
-//! are enabled by default.
-//!
-//! [embedded-hal-async]: https://github.com/rust-embedded/embedded-hal/tree/master/embedded-hal-async
-//! [embedded-io-async]: https://github.com/rust-embedded/embedded-hal/tree/master/embedded-io-async
-//! [embedded-hal]: https://github.com/rust-embedded/embedded-hal/tree/master/embedded-hal
-//! [embedded-hal-nb]: https://github.com/rust-embedded/embedded-hal/tree/master/embedded-hal-nb
-//! [embedded-io]: https://github.com/rust-embedded/embedded-hal/tree/master/embedded-io
-//! [embassy]: https://github.com/embassy-rs/embassy
-//! [`ufmt_write::uWrite`]: https://docs.rs/ufmt-write/latest/ufmt_write/trait.uWrite.html
-//! [`defmt::Format`]: https://docs.rs/defmt/0.3.5/defmt/trait.Format.html
-#![no_std]
+//! ## Feature Flags
+#![doc = document_features::document_features!()]
 #![doc(html_logo_url = "https://avatars.githubusercontent.com/u/46717278")]
+#![no_std]
 
 pub use esp_hal_common::*;
--- esp32s3-hal/src/lib.rs
+++ esp32s3-hal/src/lib.rs
@@ -1,64 +1,9 @@
-//! `no_std` HAL for the ESP32-S3 from Espressif.
+//! A bare-metal (`no_std`) Hardware Abstraction Layer for the ESP32-S3 from
+//! Espressif.
 //!
-//! Implements a number of the traits defined by the various packages in the
-//! [embedded-hal] repository.
-//!
-//! [embedded-hal]: https://github.com/rust-embedded/embedded-hal
-//!
-//! ### Cargo Features
-//!
-//! - `async` - Enable support for asynchronous operation, with interfaces
-//!   provided by [embedded-hal-async] and [embedded-io-async]
-//! - `debug` - Enable debug features in the HAL (used for development)
-//! - `defmt` - Enable [`defmt::Format`] on certain types
-//! - `eh1` - Implement the traits defined in the `1.0.0-xxx` pre-releases of
-//!   [embedded-hal], [embedded-hal-nb], and [embedded-io]
-//! - `embassy` - Enable support for [embassy], a modern asynchronous embedded
-//!   framework. One of `embassy-time-*` features must also be enabled when
-//!   using this feature.
-//! - `embassy-executor-interrupt` - Use the multicore-aware interrupt-mode
-//!   embassy executor
-//! - `embassy-executor-thread` - Use the multicore-aware thread-mode embassy
-//!   executor
-//! - `embassy-time-systick` - Enable the [embassy] time driver using the
-//!   `SYSTIMER` peripheral. The `SYSTIMER` peripheral has three alarms
-//!   available for use
-//! - `embassy-time-timg0` - Enable the [embassy] time driver using the `TIMG0`
-//!   peripheral. The `TIMG0` peripheral has two alarms available for use
-//! - `embassy-integrated-timers` - Uses hardware timers as alarms for the
-//!   executors. Using this feature limits the number of executors to the number
-//!   of hardware alarms provided by the time driver
-//! - `embassy-generic-queue-N` (where `N` can be `8`, `16`, `32`, `64` or
-//!   `128`) - Use a generic timer queue of size `N` for the executors' timer
-//!   queues. Using this feature can expand the number of executors you can use
-//!   to `N`
-//! - `log` - enable log output using the `log` crate
-//! - `opsram-2m` - Use externally connected Octal PSRAM (2MB)
-//! - `opsram-4m` - Use externally connected Octal PSRAM (4MB)
-//! - `opsram-8m` - Use externally connected Octal PSRAM (8MB)
-//! - `opsram-16m`- Use externally connected Octal PSRAM (16MB)
-//! - `psram-2m` - Use externally connected PSRAM (2MB)
-//! - `psram-4m` - Use externally connected PSRAM (4MB)
-//! - `psram-8m` - Use externally connected PSRAM (8MB)
-//! - `psram-80mhz` - Use faster 80Mhz PSRAM frequencty instead of default 40Mhz
-//! - `rt` - Runtime support
-//! - `ufmt` - Implement the [`ufmt_write::uWrite`] trait for the UART driver
-//! - `vectored` - Enable interrupt vectoring
-//!
-//! #### Default Features
-//!
-//! The `rt`, `vectored` and `embassy-integrated-timers` features are enabled by
-//! default.
-//!
-//! [embedded-hal-async]: https://github.com/rust-embedded/embedded-hal/tree/master/embedded-hal-async
-//! [embedded-io-async]: https://github.com/rust-embedded/embedded-hal/tree/master/embedded-io-async
-//! [embedded-hal]: https://github.com/rust-embedded/embedded-hal/tree/master/embedded-hal
-//! [embedded-hal-nb]: https://github.com/rust-embedded/embedded-hal/tree/master/embedded-hal-nb
-//! [embedded-io]: https://github.com/rust-embedded/embedded-hal/tree/master/embedded-io
-//! [embassy]: https://github.com/embassy-rs/embassy
-//! [`ufmt_write::uWrite`]: https://docs.rs/ufmt-write/latest/ufmt_write/trait.uWrite.html
-//! [`defmt::Format`]: https://docs.rs/defmt/0.3.5/defmt/trait.Format.html
-#![no_std]
+//! ## Feature Flags
+#![doc = document_features::document_features!()]
 #![doc(html_logo_url = "https://avatars.githubusercontent.com/u/46717278")]
+#![no_std]
 
 pub use esp_hal_common::*;
