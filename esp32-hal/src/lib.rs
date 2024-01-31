a7a337805b19a035b798e0eea34dd572cefd3a5c
Jesse Braham <jesse@beta7.io>
2024-01-31 08:16:29 -0800

    Remove the chip-specific HAL packages

--- esp32-hal/src/lib.rs
+++
@@ -1,9 +0,0 @@
-//! A bare-metal (`no_std`) Hardware Abstraction Layer for the ESP32 from
-//! Espressif.
-//!
-//! ## Feature Flags
-#![doc = document_features::document_features!()]
-#![doc(html_logo_url = "https://avatars.githubusercontent.com/u/46717278")]
-#![no_std]
-
-pub use esp_hal::*;
