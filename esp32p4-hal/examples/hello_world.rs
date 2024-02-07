a08b38d231134222f01fe00282bec0f53d4ce700
Kirill Mikhailov <62840029+playfulFence@users.noreply.github.com>
2024-02-07 20:35:25 +0100

    Adding clock support for `ESP32-P4` (#1145)
    * Begin adding clock support for ESP32-P4
    
    * WIP
    
    * WIP: more functionality added, minor example update
    
    * WIP state (testing)
    
    * Format
    
    * Finalizing + populating reset_reason enum
    
    * Update esp-pacs dependency
    
    ---------
    
    Co-authored-by: Jesse Braham <jesse@beta7.io>

--- esp32p4-hal/examples/hello_world.rs
+++ esp32p4-hal/examples/hello_world.rs
@@ -1,11 +1,20 @@
 #![no_std]
 #![no_main]
 
-use esp32p4_hal::prelude::*;
+use esp32p4_hal::{
+    clock::{ClockControl, CpuClock},
+    peripherals::Peripherals,
+    prelude::*,
+    system::SystemExt,
+};
 use esp_backtrace as _;
 
 #[entry]
 fn main() -> ! {
+    let peripherals = Peripherals::take();
+    let system = peripherals.SYSTEM.split();
+    let clocks = ClockControl::configure(system.clock_control, CpuClock::Clock90MHz).freeze();
+
     esp_println::println!("Hello, world!");
     loop {}
 }
