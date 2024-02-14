fed0831c0ed3bcfac0adb4abae860c2e0aea25c8
Jesse Braham <jesse@beta7.io>
2024-02-14 09:57:47 -0800

    Implement builder pattern for cargo command-line args, refactoring

--- xtask/src/lib.rs
+++ xtask/src/lib.rs
@@ -2,14 +2,17 @@ use std::{
     collections::VecDeque,
     fs,
     path::{Path, PathBuf},
-    process::{Command, Stdio},
 };
 
-use anyhow::{anyhow, bail, Result};
+use anyhow::{bail, Result};
 use clap::ValueEnum;
 use strum::{Display, EnumIter, IntoEnumIterator};
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Display, ValueEnum)]
+use self::cargo::CargoArgsBuilder;
+
+mod cargo;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Display, EnumIter, ValueEnum)]
 #[strum(serialize_all = "kebab-case")]
 pub enum Package {
     EspHal,
@@ -124,20 +127,22 @@ pub fn build_documentation(
 
     log::info!("Building '{package_name}' documentation targeting '{chip}'");
 
-    // Build up an array of command-line arguments to pass to `cargo doc`:
-    let mut args = vec![
-        "doc".into(),
-        "-Zbuild-std=core".into(), // Required for Xtensa, for some reason
-        format!("--target={target}"),
-        format!("--features={}", chip.to_string()),
-    ];
+    // Build up an array of command-line arguments to pass to `cargo`:
+    let mut builder = CargoArgsBuilder::default()
+        .subcommand("doc")
+        .arg("-Zbuild-std=core") // Required for Xtensa, for some reason
+        .target(target)
+        .features(&[chip.to_string()]);
+
     if open {
-        args.push("--open".into());
+        builder = builder.arg("--open");
     }
+
+    let args = builder.build();
     log::debug!("{args:#?}");
 
     // Execute `cargo doc` from the package root:
-    cargo(&args, &package_path)?;
+    cargo::run(&args, &package_path)?;
 
     Ok(())
 }
@@ -217,40 +222,26 @@ pub fn build_example(
         format!("--example={}", example.name())
     };
 
+    let mut features = example.features().to_vec();
+    features.push(chip.to_string());
+
+    let mut builder = CargoArgsBuilder::default()
+        .subcommand("build")
+        .arg("-Zbuild-std=alloc,core")
+        .arg("--release")
+        .target(target)
+        .features(&features)
+        .arg(bin);
+
     // If targeting an Xtensa device, we must use the '+esp' toolchain modifier:
-    let mut args = vec![];
     if target.starts_with("xtensa") {
-        args.push("+esp".into());
+        builder = builder.toolchain("esp");
     }
 
-    args.extend(vec![
-        "build".into(),
-        "-Zbuild-std=alloc,core".into(),
-        "--release".into(),
-        format!("--target={target}"),
-        format!("--features={},{}", chip, example.features().join(",")),
-        bin,
-    ]);
+    let args = builder.build();
     log::debug!("{args:#?}");
 
-    cargo(&args, package_path)?;
+    cargo::run(&args, package_path)?;
 
     Ok(())
-}
-
-fn cargo(args: &[String], cwd: &Path) -> Result<()> {
-    let status = Command::new("cargo")
-        .args(args)
-        .current_dir(cwd)
-        .stdout(Stdio::piped())
-        .stderr(Stdio::inherit())
-        .status()?;
-
-    // Make sure that we return an appropriate exit code here, as Github Actions
-    // requires this in order to function correctly:
-    if status.success() {
-        Ok(())
-    } else {
-        Err(anyhow!("Failed to execute cargo subcommand"))
-    }
 }
