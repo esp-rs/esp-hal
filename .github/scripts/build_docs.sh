4b0607eea77d5ab0e6a701d775acb81f3a36d761
Jesse Braham <jesse@beta7.io>
2024-03-06 09:57:41 -0800

    Move script into `.github/scripts`, add the package version to documentation path

---
+++ .github/scripts/build_docs.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# Extract the package version from `esp-hal` (using arcane methods):
+PKG_VERSION=$(
+  cargo metadata --format-version=1 --no-deps --manifest-path=esp-hal/Cargo.toml \
+  | jq -r '.packages[] | select(.name=="esp-hal") | .version'
+)
+
+# Build the documentation for each supported cheap, namespacing by
+# package version and chip:
+CHIPS=("esp32" "esp32c2" "esp32c3" "esp32c6" "esp32h2" "esp32p4" "esp32s2" "esp32s3")
+
+for CHIP in "${CHIPS[@]}"; do
+  cargo xtask build-documentation \
+    --output-path="docs/$PKG_VERSION"/"$CHIP"/ \
+    esp-hal \
+    "$CHIP"
+done
+
+# Copy any additional resources (such as the index and our logo)
+# to the location of the built documentation as well:
+cp resources/esp-rs.svg docs/
+cp resources/index.html docs/
