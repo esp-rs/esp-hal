4a3e0921ff332f62e4e6ce12799690fc7a2e1a2e
Alexandre Levy <a13xlevy@pm.me>
2024-03-15 01:32:45 +0000

    Assign configured ETM channel to a variable to prevent drop

--- examples/src/bin/etm_timer.rs
+++ examples/src/bin/etm_timer.rs
@@ -1,17 +1,18 @@
 //! This shows how to use the general purpose timers ETM tasks and events
 //! Notice you need to import the traits esp_hal::timer::etm::{TimerEtmEvents, TimerEtmTasks}
-//!
-//% CHIPS: esp32h2 esp32c6
+
+//% CHIPS: esp32c6 esp32h2
 
 #![no_std]
 #![no_main]
 
 use core::cell::RefCell;
 
-use critical_section::{CriticalSection, Mutex};
+use critical_section::Mutex;
 use esp_backtrace as _;
 use esp_hal::{
     clock::ClockControl,
+    delay::Delay,
     etm::Etm,
     interrupt::{self, Priority},
     peripherals::{Interrupt, Peripherals, TIMG0},
@@ -32,6 +33,8 @@ fn main() -> ! {
     let system = peripherals.SYSTEM.split();
     let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
 
+    let mut delay = Delay::new(&clocks);
+
     let timg0 = TimerGroup::new(peripherals.TIMG0, &clocks);
     let mut timer0 = timg0.timer0;
 
@@ -43,26 +46,33 @@ fn main() -> ! {
 
     let channel0 = etm.channel0;
 
-    channel0.setup(&event, &task);
-
-    timer0.set_counter_decrementing(false);
-    timer0.reset_counter();
-    timer0.set_counter_active(true);
+    let _configured_channel = channel0.setup(&event, &task);
 
     // Setup alarm at 100ms
     // 80 / 2 (default divider) timer clock cycles == 1 us
     timer0.load_alarm_value(100 * 1_000 * 40);
+    timer0.set_alarm_active(true);
 
     // Enable interrupt that will be triggered by the alarm
     interrupt::enable(Interrupt::TG0_T0_LEVEL, Priority::Priority1).unwrap();
 
-    timer0.set_alarm_active(true);
+    timer0.listen();
+    timer0.set_counter_active(true);
 
     critical_section::with(|cs| {
         TIMER0.borrow_ref_mut(cs).replace(timer0);
     });
 
-    loop {}
+    loop {
+        delay.delay_ms(500u32);
+
+        critical_section::with(|cs| {
+            let mut timer0 = TIMER0.borrow_ref_mut(cs);
+            let timer0 = timer0.as_mut().unwrap();
+            // Counter value should be the same than in interrupt because we configured ETM to stop the timer on alarm
+            esp_println::println!("counter in main: {}", timer0.now());
+        });
+    }
 }
 
 #[interrupt]
@@ -73,7 +83,7 @@ fn TG0_T0_LEVEL() {
 
         timer0.clear_interrupt();
 
-        // This should display close to 4_000_000
-        esp_println::println!("Counter: {}", timer0.now());
+        // Counter value should be a very small number
+        esp_println::println!("counter in interrupt: {}", timer0.now());
     });
 }
