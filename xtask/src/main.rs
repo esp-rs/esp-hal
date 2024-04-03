969f9a15f682188a3cbb75a02c44fb0d3c0d2f95
Scott Mabin <scott@mabez.dev>
2024-04-03 18:07:46 +0100

    Build HIL in CI prior to running them (#1388)
    * xtask: allow building hil tests
    
    * check hil tests build in CI

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -212,11 +212,10 @@ fn build_examples(workspace: &Path, mut args: BuildExamplesArgs) -> Result<()> {
     // Absolute path of the package's root:
     let package_path = xtask::windows_safe_path(&workspace.join(args.package.to_string()));
 
-    // Absolute path to the directory containing the examples:
-    let example_path = if args.package == Package::Examples {
-        package_path.join("src").join("bin")
-    } else {
-        package_path.join("examples")
+    let example_path = match args.package {
+        Package::Examples => package_path.join("src").join("bin"),
+        Package::HilTest => package_path.join("tests"),
+        _ => package_path.join("examples"),
     };
 
     // Determine the appropriate build target for the given package and chip:
