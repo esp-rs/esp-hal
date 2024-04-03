b0e4eb4e29f946d9b2e21e442bb7c13332728fa9
Jesse Braham <jesse@beta7.io>
2024-04-03 08:12:30 -0700

    Provide native APIs for SPI functionality currently handled by `embedded-hal` traits

--- examples/src/bin/spi_loopback.rs
+++ examples/src/bin/spi_loopback.rs
@@ -14,12 +14,10 @@
 //! data.
 
 //% CHIPS: esp32 esp32c2 esp32c3 esp32c6 esp32h2 esp32s2 esp32s3
-//% FEATURES: embedded-hal-02
 
 #![no_std]
 #![no_main]
 
-use embedded_hal_02::blocking::spi::Transfer;
 use esp_backtrace as _;
 use esp_hal::{
     clock::ClockControl,
