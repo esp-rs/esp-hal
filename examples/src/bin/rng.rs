83e46bd402936ab3d8febb61c81e325fc25c2270
Kirill Mikhailov <konnor1980@yandex.ru>
2024-04-08 18:09:37 +0200

    More progress

--- examples/src/bin/rng.rs
+++ examples/src/bin/rng.rs
@@ -6,21 +6,38 @@
 #![no_main]
 
 use esp_backtrace as _;
-use esp_hal::{peripherals::Peripherals, prelude::*, rng::Trng};
+use esp_hal::{prelude::*, rng::Trng,
+    analog::adc::{AdcConfig, Attenuation, ADC},
+    clock::ClockControl,
+    delay::Delay,
+    gpio::IO,
+    peripherals::{Peripherals, ADC1}};
+
 use esp_println::println;
 
 #[entry]
 fn main() -> ! {
     let peripherals = Peripherals::take();
-    let mut trng = Trng::new(peripherals.RNG, peripherals.ADC1);
+    let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
 
-    // Generate a random word (u32):
-    println!("Random u32:   {}", trng.random());
-
+    let analog_pin = io.pins.gpio3.into_analog();
+    let mut adc1_config = AdcConfig::new();
+    let mut adc1_pin = adc1_config.enable_pin(analog_pin, Attenuation::Attenuation11dB);
+    let mut adc1 = ADC::<ADC1>::new(peripherals.ADC1, adc1_config);
+    let pin_value: u16 = nb::block!(adc1.read_oneshot(&mut adc1_pin)).unwrap();
+    
+    let mut trng = Trng::new(peripherals.RNG, &mut adc1);
+    
+    let (mut rng, adc1) = trng.downgrade();
+    
     // Fill a buffer with random bytes:
     let mut buf = [0u8; 16];
-    trng.read(&mut buf);
+    // trng.read(&mut buf);
     println!("Random bytes: {:?}", buf);
 
-    loop {}
+    loop {
+        println!("Random u32:   {}", rng.random());
+        let pin_value: u16 = nb::block!(adc1.read_oneshot(&mut adc1_pin)).unwrap();
+        println!("ADC reading = {}", pin_value);
+    }
 }
