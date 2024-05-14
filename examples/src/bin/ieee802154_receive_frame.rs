8d001aed3342d7d3eba13b4b5c63015b02e47fc1
Jesse Braham <jesse@beta7.io>
2024-05-14 06:31:27 -0700

    Add examples for `esp-ieee802154` package

---
+++ examples/src/bin/ieee802154_receive_frame.rs
@@ -0,0 +1,35 @@
+//% CHIPS: esp32c6 esp32h2
+
+#![no_std]
+#![no_main]
+
+use esp_backtrace as _;
+use esp_hal::{peripherals::Peripherals, prelude::*};
+use esp_ieee802154::*;
+use esp_println::println;
+
+#[entry]
+fn main() -> ! {
+    let mut peripherals = Peripherals::take();
+    let mut ieee802154 = Ieee802154::new(peripherals.IEEE802154, &mut peripherals.RADIO_CLK);
+
+    ieee802154.set_config(Config {
+        channel: 15,
+        promiscuous: false,
+        rx_when_idle: true,
+        auto_ack_rx: true,
+        auto_ack_tx: true,
+        pan_id: Some(0x4242),
+        short_addr: Some(0x2323),
+        ..Config::default()
+    });
+
+    println!("Start receiving:");
+    ieee802154.start_receive();
+
+    loop {
+        if let Some(frame) = ieee802154.get_received() {
+            println!("Received {:?}\n", &frame);
+        }
+    }
+}
