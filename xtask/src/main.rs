1cada8da3db88d11a3bcf2608a598b9c1db4db71
bjoernQ <bjoern.quentin@mobile-j.de>
2024-05-27 14:18:43 +0200

    Include needed feature to lint esp-storage

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -490,6 +490,15 @@ fn lint_packages(workspace: &Path, _args: LintPackagesArgs) -> Result<()> {
                 ],
             )?,
 
+            Package::EspStorage => lint_package(
+                &path,
+                &[
+                    "-Zbuild-std=core",
+                    "--target=riscv32imc-unknown-none-elf",
+                    "--features=esp32c6",
+                ],
+            )?,
+
             // We will *not* check the following packages with `clippy`; this
             // may or may not change in the future:
             Package::Examples | Package::HilTest => {}
