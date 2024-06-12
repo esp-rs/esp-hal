243e04f4bcad7b8a540cc349185ae3d7c27eeef9
bjoernQ <bjoern.quentin@mobile-j.de>
2024-06-12 15:42:01 +0200

    Fix `mk_static` macro

--- examples/src/bin/esp_wifi_embassy_access_point.rs
+++ examples/src/bin/esp_wifi_embassy_access_point.rs
@@ -51,7 +51,7 @@ use esp_wifi::{
 };
 
 macro_rules! mk_static {
-    ($t:path,$val:expr) => {{
+    ($t:ty,$val:expr) => {{
         static STATIC_CELL: static_cell::StaticCell<$t> = static_cell::StaticCell::new();
         #[deny(unused_attributes)]
         let x = STATIC_CELL.uninit().write(($val));
