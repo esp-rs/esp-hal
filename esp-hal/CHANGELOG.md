8cb921f73b0005a78b657359235ed9e8709823e2
Björn Quentin <bjoernQ@users.noreply.github.com>
2024-06-28 13:14:29 +0200

    Add GPIO as wake-up source (#1724)
    * Add GPIO as wake-up source
    
    * CHANGELOG.md
    
    * Clippy
    
    * Doc fix

--- esp-hal/CHANGELOG.md
+++ esp-hal/CHANGELOG.md
@@ -17,6 +17,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - Add DmaTransactionTxOwned, DmaTransactionRxOwned, DmaTransactionTxRxOwned, functions to do owning transfers added to SPI half-duplex (#1672)
 - uart: Implement `embedded_io::ReadReady` for `Uart` and `UartRx` (#1702)
 - ESP32-C6: Support lp-core as wake-up source (#1723)
+- Add support for GPIO wake-up source (#1724)
 
 ### Fixed
 
--- esp-hal/src/rtc_cntl/sleep/mod.rs
+++ esp-hal/src/rtc_cntl/sleep/mod.rs
@@ -153,6 +153,34 @@ impl Default for WakeFromLpCoreWakeupSource {
     }
 }
 
+/// GPIO wakeup source
+///
+/// Wake up from GPIO high or low level. Any pin can be used with this wake up
+/// source. Configure the pin for wake up via
+/// [crate::gpio::Input::wakeup_enable].
+///
+/// This wakeup source can be used to wake up from light sleep only.
+pub struct GpioWakeupSource {}
+
+impl GpioWakeupSource {
+    /// Create a new instance of [GpioWakeupSource]
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
+impl Default for GpioWakeupSource {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl WakeSource for GpioWakeupSource {
+    fn apply(&self, _rtc: &Rtc, triggers: &mut WakeTriggers, _sleep_config: &mut RtcSleepConfig) {
+        triggers.set_gpio(true);
+    }
+}
+
 #[cfg(not(pmu))]
 bitfield::bitfield! {
     #[derive(Default, Clone, Copy)]
