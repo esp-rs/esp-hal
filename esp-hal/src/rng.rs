7dc0cb486ff3a6b45a9aeaf64345aeb22b3dd463
Kirill Mikhailov <konnor1980@yandex.ru>
2024-07-01 15:11:47 +0200

    Doc comments fix
    fmt

--- esp-hal/src/rng.rs
+++ esp-hal/src/rng.rs
@@ -131,27 +131,37 @@ impl rand_core::RngCore for Rng {
 /// Due to pulling the entropy source from the ADC, it uses the associated
 /// regiters, so to use TRNG we need to "occupy" the ADC peripheral.
 ///
-/// ```rust, ignore
+/// ```rust, no_run
+#[doc = crate::before_snippet!()]
+/// # use esp_hal::rng::Trng;
+/// # use esp_hal::peripherals::Peripherals;
+/// # use esp_hal::peripherals::ADC1;
+/// # use esp_hal::analog::adc::{AdcConfig, Attenuation, Adc};
+/// # use esp_hal::gpio::Io;
+///
+/// let mut peripherals = Peripherals::take();
+/// let io = Io::new(peripherals.GPIO, peripherals.IO_MUX);
 /// let mut buf = [0u8; 16];
+///
+/// // ADC is not available from now
 /// let mut trng = Trng::new(peripherals.RNG, &mut peripherals.ADC1);
 /// trng.read(&mut buf);
-///
-/// println!("TRNG: Random bytes: {:?}", buf);
-/// println!("TRNG: Random u32:  {}", rng.random());
+/// let mut true_rand = trng.random();
 ///
 /// let mut rng = trng.downgrade();
 ///
+/// // ADC is available now
 /// let analog_pin = io.pins.gpio3;
 /// let mut adc1_config = AdcConfig::new();
-/// let mut adc1_pin = adc1_config.enable_pin(analog_pin, Attenuation::Attenuation11dB);
-/// let mut adc1 = Adc::<ADC1>::new(peripherals.ADC1, adc1_config);
-/// let pin_value: u16 = nb::block!(adc1.read_oneshot(&mut adc1_pin)).unwrap();
+/// let mut adc1_pin = adc1_config.enable_pin(analog_pin,
+/// Attenuation::Attenuation11dB); let mut adc1 =
+/// Adc::<ADC1>::new(peripherals.ADC1, adc1_config); let pin_value: u16 =
+/// nb::block!(adc1.read_oneshot(&mut adc1_pin)).unwrap();
 ///
 /// rng.read(&mut buf);
-/// println!("Random bytes: {:?}", buf);
-/// println!("Random u32:  {}", rng.random());
+/// true_rand = rng.random();
 /// let pin_value: u16 = nb::block!(adc1.read_oneshot(&mut adc1_pin)).unwrap();
-/// println!("ADC reading = {}", pin_value);
+/// # }
 /// ```
 
 pub struct Trng<'d> {
