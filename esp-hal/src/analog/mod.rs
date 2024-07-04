fdc4020da9199d0aae8c8ec5bead33d57e0d01cd
Sergio Gasquez <sergio.gasquez@gmail.com>
2024-07-04 12:53:52 +0200

    docs: Update Analog and ADC mod documentation

--- esp-hal/src/analog/mod.rs
+++ esp-hal/src/analog/mod.rs
@@ -1,5 +1,6 @@
 //! # Analog Peripherals
 //!
+//! ## Overview
 //! The `analog` module provides drivers for the various analog peripherals
 //! available on the device. For more information about a peripheral driver,
 //! please refer to the relevant module documentation.
