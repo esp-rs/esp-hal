c6f0a39d166c462294050a105da7eca506745d8f
Dominic Fischer <git@dominicfischer.me>
2024-07-06 00:46:25 +0100

    Improve PCNT API (with HIL tests)

--- esp-hal/src/pcnt/mod.rs
+++ esp-hal/src/pcnt/mod.rs
@@ -49,6 +49,27 @@ pub mod unit;
 
 pub struct Pcnt<'d> {
     _instance: PeripheralRef<'d, peripherals::PCNT>,
+
+    /// Unit 0
+    pub unit0: Unit<'d, 0>,
+    /// Unit 1
+    pub unit1: Unit<'d, 1>,
+    /// Unit 2
+    pub unit2: Unit<'d, 2>,
+    /// Unit 3
+    pub unit3: Unit<'d, 3>,
+    #[cfg(esp32)]
+    /// Unit 4
+    pub unit4: Unit<'d, 4>,
+    #[cfg(esp32)]
+    /// Unit 5
+    pub unit5: Unit<'d, 5>,
+    #[cfg(esp32)]
+    /// Unit 6
+    pub unit6: Unit<'d, 6>,
+    #[cfg(esp32)]
+    /// Unit 7
+    pub unit7: Unit<'d, 7>,
 }
 
 impl<'d> Pcnt<'d> {
@@ -59,6 +80,7 @@ impl<'d> Pcnt<'d> {
     ) -> Self {
         crate::into_ref!(_instance);
         // Enable the PCNT peripherals clock in the system peripheral
+        PeripheralClockControl::reset(crate::system::Peripheral::Pcnt);
         PeripheralClockControl::enable(crate::system::Peripheral::Pcnt);
 
         if let Some(interrupt) = interrupt {
@@ -68,11 +90,44 @@ impl<'d> Pcnt<'d> {
             }
         }
 
-        Pcnt { _instance }
-    }
+        let pcnt = unsafe { &*crate::peripherals::PCNT::ptr() };
 
-    /// Return a unit
-    pub fn get_unit(&self, number: unit::Number) -> Unit {
-        Unit::new(number)
+        // disable filter, all events, and channel settings
+        for unit in pcnt.unit_iter() {
+            unit.conf0().write(|w| unsafe {
+                // All bits are accounted for in the TRM.
+                w.bits(0)
+            });
+        }
+
+        // Remove reset bit from units.
+        pcnt.ctrl().modify(|_, w| {
+            #[cfg(not(esp32))]
+            let unit_count = 4;
+            #[cfg(esp32)]
+            let unit_count = 8;
+
+            for i in 0..unit_count {
+                w.cnt_rst_u(i).clear_bit();
+            }
+
+            w.clk_en().set_bit()
+        });
+
+        Pcnt {
+            _instance,
+            unit0: Unit::new(),
+            unit1: Unit::new(),
+            unit2: Unit::new(),
+            unit3: Unit::new(),
+            #[cfg(esp32)]
+            unit4: Unit::new(),
+            #[cfg(esp32)]
+            unit5: Unit::new(),
+            #[cfg(esp32)]
+            unit6: Unit::new(),
+            #[cfg(esp32)]
+            unit7: Unit::new(),
+        }
     }
 }
