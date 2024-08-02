4a39335476bf3e56da8b274b915b43edec4932d5
bjoernQ <bjoern.quentin@mobile-j.de>
2024-08-02 14:22:21 +0200

    Fix

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -792,13 +792,14 @@ fn lint_packages(workspace: &Path, args: LintPackagesArgs) -> Result<()> {
                 }
 
                 Package::EspRustlsProvider => {
-                    if matches!(device.arch(), Arch::RiscV) {
+                    if [Chip::Esp32, Chip::Esp32s3, Chip::Esp32c6].contains(chip) {
+                        let features = format!("--features=esp-hal/{chip}");
                         lint_package(
                             chip,
                             &path,
                             &[
                                 "-Zbuild-std=core,alloc",
-                                "--features=esp-hal/esp32c6",
+                                &features,
                                 &format!("--target={}", chip.target()),
                             ],
                             args.fix,
