7bb59df5dc4cf0b971d2040075569d87ac057133
Juraj Sadel <juraj.sadel@espressif.com>
2024-08-16 11:39:31 +0200

    clean up

--- hil-test/tests/spi_full_duplex_dma_async.rs
+++ hil-test/tests/spi_full_duplex_dma_async.rs
@@ -38,17 +38,7 @@ use esp_hal::{
         SpiMode,
     },
     system::SystemControl,
-    timer::{timg::TimerGroup, ErasedTimer, OneShotTimer},
 };
-
-macro_rules! mk_static {
-    ($t:ty,$val:expr) => {{
-        static STATIC_CELL: static_cell::StaticCell<$t> = static_cell::StaticCell::new();
-        #[deny(unused_attributes)]
-        let x = STATIC_CELL.uninit().write(($val));
-        x
-    }};
-}
 
 #[cfg(test)]
 #[embedded_test::tests(executor = esp_hal_embassy::Executor::new())]
@@ -128,12 +118,6 @@ mod tests {
         let peripherals = Peripherals::take();
         let system = SystemControl::new(peripherals.SYSTEM);
         let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
-
-        let timg0 = TimerGroup::new(peripherals.TIMG0, &clocks);
-        let timer0: ErasedTimer = timg0.timer0.into();
-        let timers = [OneShotTimer::new(timer0)];
-        let timers = mk_static!([OneShotTimer<ErasedTimer>; 1], timers);
-        esp_hal_embassy::init(&clocks, timers);
 
         let io = Io::new(peripherals.GPIO, peripherals.IO_MUX);
         let pcnt = Pcnt::new(peripherals.PCNT);
