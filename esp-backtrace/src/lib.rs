6b4079be3adff4ee7e51171c5c11255879be7cfa
Scott Mabin <scott@mabez.dev>
2024-09-02 12:11:36 +0100

    release prep xtensa-lx-rt@0.17.1 (#2060)

--- esp-backtrace/src/lib.rs
+++ esp-backtrace/src/lib.rs
@@ -100,10 +100,10 @@ unsafe fn __user_exception(cause: arch::ExceptionCause, context: arch::Context) 
 
     // Unfortunately, a different formatter string is used
     #[cfg(not(feature = "defmt"))]
-    esp_println::println!("\n\nException occured '{:?}'", cause);
+    esp_println::println!("\n\nException occurred '{:?}'", cause);
 
     #[cfg(feature = "defmt")]
-    defmt::error!("\n\nException occured '{}'", cause);
+    defmt::error!("\n\nException occurred '{}'", cause);
 
     println!("{:?}", context);
 
