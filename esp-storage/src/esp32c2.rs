08d406ee2b240384610bea8199d444f44942f157
Dániel Buga <bugadani@gmail.com>
2024-09-02 17:24:41 +0200

    storage: Clean up ROM function declarations (#2058)
    * Clean up external function declarations
    
    * Tweak syntax

--- esp-storage/src/esp32c2.rs
+++ esp-storage/src/esp32c2.rs
@@ -1,38 +1,24 @@
 use crate::maybe_with_critical_section;
 
-const ESP_ROM_SPIFLASH_READ: u32 = 0x4000013c;
-const ESP_ROM_SPIFLASH_UNLOCK: u32 = 0x40000140;
-const ESP_ROM_SPIFLASH_ERASE_SECTOR: u32 = 0x40000130;
-const ESP_ROM_SPIFLASH_WRITE: u32 = 0x40000138;
-
-pub(crate) fn esp_rom_spiflash_read(src_addr: u32, data: *const u32, len: u32) -> i32 {
-    maybe_with_critical_section(|| unsafe {
-        let esp_rom_spiflash_read: unsafe extern "C" fn(u32, *const u32, u32) -> i32 =
-            core::mem::transmute(ESP_ROM_SPIFLASH_READ);
-        esp_rom_spiflash_read(src_addr, data, len)
-    })
+crate::rom_fn! {
+    fn esp_rom_spiflash_read(src_addr: u32, data: *const u32, len: u32) -> i32 = 0x4000013c;
+    fn esp_rom_spiflash_unlock() -> i32 = 0x40000140;
+    fn esp_rom_spiflash_erase_sector(sector_number: u32) -> i32 = 0x40000130;
+    fn esp_rom_spiflash_write(dest_addr: u32, data: *const u32, len: u32) -> i32 = 0x40000138;
 }
 
-pub(crate) fn esp_rom_spiflash_unlock() -> i32 {
-    maybe_with_critical_section(|| unsafe {
-        let esp_rom_spiflash_unlock: unsafe extern "C" fn() -> i32 =
-            core::mem::transmute(ESP_ROM_SPIFLASH_UNLOCK);
-        esp_rom_spiflash_unlock()
-    })
+pub(crate) fn spiflash_read(src_addr: u32, data: *const u32, len: u32) -> i32 {
+    maybe_with_critical_section(|| esp_rom_spiflash_read(src_addr, data, len))
 }
 
-pub(crate) fn esp_rom_spiflash_erase_sector(sector_number: u32) -> i32 {
-    maybe_with_critical_section(|| unsafe {
-        let esp_rom_spiflash_erase_sector: unsafe extern "C" fn(u32) -> i32 =
-            core::mem::transmute(ESP_ROM_SPIFLASH_ERASE_SECTOR);
-        esp_rom_spiflash_erase_sector(sector_number)
-    })
+pub(crate) fn spiflash_unlock() -> i32 {
+    maybe_with_critical_section(esp_rom_spiflash_unlock)
 }
 
-pub(crate) fn esp_rom_spiflash_write(dest_addr: u32, data: *const u32, len: u32) -> i32 {
-    maybe_with_critical_section(|| unsafe {
-        let esp_rom_spiflash_write: unsafe extern "C" fn(u32, *const u32, u32) -> i32 =
-            core::mem::transmute(ESP_ROM_SPIFLASH_WRITE);
-        esp_rom_spiflash_write(dest_addr, data, len)
-    })
+pub(crate) fn spiflash_erase_sector(sector_number: u32) -> i32 {
+    maybe_with_critical_section(|| esp_rom_spiflash_erase_sector(sector_number))
+}
+
+pub(crate) fn spiflash_write(dest_addr: u32, data: *const u32, len: u32) -> i32 {
+    maybe_with_critical_section(|| esp_rom_spiflash_write(dest_addr, data, len))
 }
