3334ab27c9f62b1b77c274c4f591a4dbb7dfe81f
Jesse Braham <jesse@beta7.io>
2024-09-04 11:19:43 -0700

    Improve tests slightly

--- hil-test/tests/delay_async.rs
+++ hil-test/tests/delay_async.rs
@@ -8,7 +8,7 @@
 #![no_std]
 #![no_main]
 
-use embedded_hal_async::delay::DelayNs as _;
+use embedded_hal_async::delay::DelayNs;
 use esp_hal::{
     peripherals::Peripherals,
     timer::{
@@ -20,6 +20,19 @@ use hil_test as _;
 
 struct Context {
     peripherals: Peripherals,
+}
+
+async fn test_async_delay_ns(mut timer: impl DelayNs, duration: u32) {
+    let t1 = esp_hal::time::current_time();
+    timer.delay_ns(duration).await;
+    let t2 = esp_hal::time::current_time();
+
+    assert!(t2 > t1);
+    assert!(
+        (t2 - t1).to_nanos() >= duration as u64,
+        "diff: {:?}",
+        (t2 - t1).to_nanos()
+    );
 }
 
 #[cfg(test)]
@@ -39,35 +52,29 @@ mod tests {
     async fn test_systimer_async_delay_ns(ctx: Context) {
         let mut alarms = SystemTimer::new(ctx.peripherals.SYSTIMER);
         let unit = FrozenUnit::new(&mut alarms.unit0);
-        let mut alarm0 = Alarm::new_async(alarms.comparator0, &unit).into_periodic();
+        let alarm0 = Alarm::new_async(alarms.comparator0, &unit).into_periodic();
 
-        let t1 = esp_hal::time::current_time();
-        alarm0.delay_ns(600_000_000).await;
-        let t2 = esp_hal::time::current_time();
-
-        assert!(t2 > t1);
-        assert!(
-            (t2 - t1).to_nanos() >= 600_000_000u64,
-            "diff: {:?}",
-            (t2 - t1).to_nanos()
-        );
+        test_async_delay_ns(alarm0, 600_000_000).await;
     }
 
     #[test]
     #[timeout(2)]
     async fn test_timg0_async_delay_ns(ctx: Context) {
         let timg0 = TimerGroup::new_async(ctx.peripherals.TIMG0);
-        let mut timer0 = timg0.timer0;
 
-        let t1 = esp_hal::time::current_time();
-        timer0.delay_ns(600_000_000).await;
-        let t2 = esp_hal::time::current_time();
+        test_async_delay_ns(timg0.timer0, 600_000_000).await;
+        #[cfg(timg_timer1)]
+        test_async_delay_ns(timg0.timer1, 600_000_000).await;
+    }
 
-        assert!(t2 > t1);
-        assert!(
-            (t2 - t1).to_nanos() >= 600_000_000u64,
-            "diff: {:?}",
-            (t2 - t1).to_nanos()
-        );
+    #[cfg(timg1)]
+    #[test]
+    #[timeout(2)]
+    async fn test_timg1_async_delay_ns(ctx: Context) {
+        let timg1 = TimerGroup::new_async(ctx.peripherals.TIMG1);
+
+        test_async_delay_ns(timg1.timer0, 600_000_000).await;
+        #[cfg(timg_timer1)]
+        test_async_delay_ns(timg1.timer1, 600_000_000).await;
     }
 }
