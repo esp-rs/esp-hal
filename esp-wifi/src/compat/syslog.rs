81f046316358aea7e0bef5363d39dcf4b78dfae5
bjoernQ <bjoern.quentin@mobile-j.de>
2024-09-09 08:50:50 +0200

    Fix `wifi-logs` feature

--- esp-wifi/src/compat/syslog.rs
+++ esp-wifi/src/compat/syslog.rs
@@ -6,15 +6,19 @@ pub unsafe extern "C" fn syslog(_priority: u32, _format: *const u8, _args: VaLis
     cfg_if::cfg_if! {
         if #[cfg(any(target_arch = "riscv32", all(target_arch = "xtensa", xtensa_has_vaarg)))]
         {
+            extern "C" {
+                fn vsnprintf(buffer: *mut u8, len: usize, fmt: *const u8, args: VaListImpl);
+            }
+
             let mut buf = [0u8; 512];
             vsnprintf(&mut buf as *mut u8, 512, _format, _args);
-            let res_str = str_from_c(&buf as *const u8);
-            info!("{}", res_str);
+            let res_str = core::ffi::CStr::from_ptr(&buf as *const _ as *const i8);
+            info!("{}", res_str.to_str().unwrap());
         }
         else
         {
-            let res_str = str_from_c(_format);
-            info!("{}", res_str);
+            let res_str = core::ffi::CStr::from_ptr(_format as *const i8);
+            info!("{}", res_str.to_str().unwrap());
         }
     }
 }
