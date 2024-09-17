42056811fd0d3153a543cafa36259cc0ffee9b95
Dániel Buga <bugadani@gmail.com>
2024-09-17 15:03:53 +0200

    Move SPI tests into the same file

--- hil-test/tests/spi_full_duplex.rs
+++ hil-test/tests/spi_full_duplex.rs
@@ -1,26 +1,45 @@
-//! SPI Full Duplex Test
+//! SPI Full Duplex test suite.
 
 //% CHIPS: esp32 esp32c2 esp32c3 esp32c6 esp32h2 esp32s2 esp32s3
+//% FEATURES: generic-queue
+
+// FIXME: add async test cases that don't rely on PCNT
 
 #![no_std]
 #![no_main]
 
 use embedded_hal::spi::SpiBus;
 #[cfg(pcnt)]
+use embedded_hal_async::spi::SpiBus as SpiBusAsync;
 use esp_hal::{
-    gpio::interconnect::InputSignal,
-    pcnt::{channel::EdgeMode, unit::Unit, Pcnt},
-};
-use esp_hal::{
-    gpio::Io,
+    dma::{Dma, DmaPriority, DmaRxBuf, DmaTxBuf},
+    dma_buffers,
+    gpio::{Io, Level},
     peripherals::SPI2,
     prelude::*,
     spi::{master::Spi, FullDuplexMode, SpiMode},
 };
+#[cfg(pcnt)]
+use esp_hal::{
+    gpio::interconnect::InputSignal,
+    pcnt::{channel::EdgeMode, unit::Unit, Pcnt},
+};
 use hil_test as _;
+
+cfg_if::cfg_if! {
+    if #[cfg(any(
+        feature = "esp32",
+        feature = "esp32s2",
+    ))] {
+        type DmaChannelCreator = esp_hal::dma::Spi2DmaChannelCreator;
+    } else {
+        type DmaChannelCreator = esp_hal::dma::ChannelCreator<0>;
+    }
+}
 
 struct Context {
     spi: Spi<'static, SPI2, FullDuplexMode>,
+    dma_channel: DmaChannelCreator,
     #[cfg(pcnt)]
     pcnt_source: InputSignal,
     #[cfg(pcnt)]
@@ -28,7 +47,7 @@ struct Context {
 }
 
 #[cfg(test)]
-#[embedded_test::tests]
+#[embedded_test::tests(executor = esp_hal_embassy::Executor::new())]
 mod tests {
     use defmt::assert_eq;
 
@@ -39,27 +58,38 @@ mod tests {
         let peripherals = esp_hal::init(esp_hal::Config::default());
 
         let io = Io::new(peripherals.GPIO, peripherals.IO_MUX);
-
         let sclk = io.pins.gpio0;
         let (_, mosi) = hil_test::common_test_pins!(io);
 
-        let mosi_loopback = mosi.peripheral_input();
+        let dma = Dma::new(peripherals.DMA);
+
+        cfg_if::cfg_if! {
+            if #[cfg(any(feature = "esp32", feature = "esp32s2"))] {
+                let dma_channel = dma.spi2channel;
+            } else {
+                let dma_channel = dma.channel0;
+            }
+        }
+
         #[cfg(pcnt)]
         let mosi_loopback_pcnt = mosi.peripheral_input();
-        let spi = Spi::new(peripherals.SPI2, 100.kHz(), SpiMode::Mode0)
+        let mosi_loopback = mosi.peripheral_input();
+        let spi = Spi::new(peripherals.SPI2, 10000.kHz(), SpiMode::Mode0)
             .with_sck(sclk)
             .with_mosi(mosi)
             .with_miso(mosi_loopback);
 
-        #[cfg(pcnt)]
-        let pcnt = Pcnt::new(peripherals.PCNT);
-
-        Context {
-            spi,
-            #[cfg(pcnt)]
-            pcnt_source: mosi_loopback_pcnt,
-            #[cfg(pcnt)]
-            pcnt_unit: pcnt.unit0,
+        cfg_if::cfg_if! {
+            if #[cfg(pcnt)] {
+                let pcnt = Pcnt::new(peripherals.PCNT);
+                Context {
+                    spi, dma_channel,
+                    pcnt_source: mosi_loopback_pcnt,
+                    pcnt_unit: pcnt.unit0,
+                }
+            } else {
+                Context { spi, dma_channel }
+            }
         }
     }
 
@@ -151,5 +181,313 @@ mod tests {
         for byte in 0..write.len() {
             assert_eq!(write[byte], byte as u8);
         }
+    }
+
+    #[test]
+    #[timeout(3)]
+    #[cfg(pcnt)]
+    fn test_dma_read_dma_write_pcnt(ctx: Context) {
+        const DMA_BUFFER_SIZE: usize = 5;
+        let (rx_buffer, rx_descriptors, tx_buffer, tx_descriptors) = dma_buffers!(DMA_BUFFER_SIZE);
+        let mut dma_rx_buf = DmaRxBuf::new(rx_descriptors, rx_buffer).unwrap();
+        let mut dma_tx_buf = DmaTxBuf::new(tx_descriptors, tx_buffer).unwrap();
+
+        let unit = ctx.pcnt_unit;
+        let mut spi = ctx
+            .spi
+            .with_dma(ctx.dma_channel.configure(false, DmaPriority::Priority0));
+
+        unit.channel0.set_edge_signal(ctx.pcnt_source);
+        unit.channel0
+            .set_input_mode(EdgeMode::Hold, EdgeMode::Increment);
+
+        // Fill the buffer where each byte has 3 pos edges.
+        dma_tx_buf.as_mut_slice().fill(0b0110_1010);
+
+        for i in 1..4 {
+            dma_rx_buf.as_mut_slice().copy_from_slice(&[5, 5, 5, 5, 5]);
+            let transfer = spi.dma_read(dma_rx_buf).map_err(|e| e.0).unwrap();
+            (spi, dma_rx_buf) = transfer.wait();
+            assert_eq!(dma_rx_buf.as_slice(), &[0, 0, 0, 0, 0]);
+
+            let transfer = spi.dma_write(dma_tx_buf).map_err(|e| e.0).unwrap();
+            (spi, dma_tx_buf) = transfer.wait();
+            assert_eq!(unit.get_value(), (i * 3 * DMA_BUFFER_SIZE) as _);
+        }
+    }
+
+    #[test]
+    #[timeout(3)]
+    #[cfg(pcnt)]
+    fn test_dma_read_dma_transfer_pcnt(ctx: Context) {
+        const DMA_BUFFER_SIZE: usize = 5;
+        let (rx_buffer, rx_descriptors, tx_buffer, tx_descriptors) = dma_buffers!(DMA_BUFFER_SIZE);
+        let mut dma_rx_buf = DmaRxBuf::new(rx_descriptors, rx_buffer).unwrap();
+        let mut dma_tx_buf = DmaTxBuf::new(tx_descriptors, tx_buffer).unwrap();
+
+        let unit = ctx.pcnt_unit;
+        let mut spi = ctx
+            .spi
+            .with_dma(ctx.dma_channel.configure(false, DmaPriority::Priority0));
+
+        unit.channel0.set_edge_signal(ctx.pcnt_source);
+        unit.channel0
+            .set_input_mode(EdgeMode::Hold, EdgeMode::Increment);
+
+        // Fill the buffer where each byte has 3 pos edges.
+        dma_tx_buf.as_mut_slice().fill(0b0110_1010);
+
+        for i in 1..4 {
+            dma_rx_buf.as_mut_slice().copy_from_slice(&[5, 5, 5, 5, 5]);
+            let transfer = spi.dma_read(dma_rx_buf).map_err(|e| e.0).unwrap();
+            (spi, dma_rx_buf) = transfer.wait();
+            assert_eq!(dma_rx_buf.as_slice(), &[0, 0, 0, 0, 0]);
+
+            let transfer = spi
+                .dma_transfer(dma_rx_buf, dma_tx_buf)
+                .map_err(|e| e.0)
+                .unwrap();
+            (spi, (dma_rx_buf, dma_tx_buf)) = transfer.wait();
+            assert_eq!(unit.get_value(), (i * 3 * DMA_BUFFER_SIZE) as _);
+        }
+    }
+
+    #[test]
+    fn test_symmetric_dma_transfer(ctx: Context) {
+        // This test case sends a large amount of data, twice, to verify that
+        // https://github.com/esp-rs/esp-hal/issues/2151 is and remains fixed.
+        let (rx_buffer, rx_descriptors, tx_buffer, tx_descriptors) = dma_buffers!(32000);
+        let mut dma_rx_buf = DmaRxBuf::new(rx_descriptors, rx_buffer).unwrap();
+        let mut dma_tx_buf = DmaTxBuf::new(tx_descriptors, tx_buffer).unwrap();
+
+        for (i, v) in dma_tx_buf.as_mut_slice().iter_mut().enumerate() {
+            *v = (i % 255) as u8;
+        }
+
+        let mut spi = ctx
+            .spi
+            .with_dma(ctx.dma_channel.configure(false, DmaPriority::Priority0));
+
+        for i in 0..4 {
+            dma_tx_buf.as_mut_slice()[0] = i as u8;
+            *dma_tx_buf.as_mut_slice().last_mut().unwrap() = i as u8;
+            let transfer = spi
+                .dma_transfer(dma_rx_buf, dma_tx_buf)
+                .map_err(|e| e.0)
+                .unwrap();
+
+            (spi, (dma_rx_buf, dma_tx_buf)) = transfer.wait();
+            assert_eq!(dma_tx_buf.as_slice(), dma_rx_buf.as_slice());
+        }
+    }
+
+    #[test]
+    #[timeout(3)]
+    #[cfg(not(feature = "esp32s2"))]
+    fn test_asymmetric_dma_transfer(ctx: Context) {
+        let (rx_buffer, rx_descriptors, tx_buffer, tx_descriptors) = dma_buffers!(2, 4);
+        let dma_rx_buf = DmaRxBuf::new(rx_descriptors, rx_buffer).unwrap();
+        let mut dma_tx_buf = DmaTxBuf::new(tx_descriptors, tx_buffer).unwrap();
+
+        dma_tx_buf.fill(&[0xde, 0xad, 0xbe, 0xef]);
+
+        let spi = ctx
+            .spi
+            .with_dma(ctx.dma_channel.configure(false, DmaPriority::Priority0));
+        let transfer = spi
+            .dma_transfer(dma_rx_buf, dma_tx_buf)
+            .map_err(|e| e.0)
+            .unwrap();
+        let (_, (dma_rx_buf, dma_tx_buf)) = transfer.wait();
+        assert_eq!(dma_tx_buf.as_slice()[0..1], dma_rx_buf.as_slice()[0..1]);
+    }
+
+    #[test]
+    #[timeout(3)]
+    fn test_symmetric_dma_transfer_huge_buffer(ctx: Context) {
+        let (rx_buffer, rx_descriptors, tx_buffer, tx_descriptors) = dma_buffers!(4096);
+        let dma_rx_buf = DmaRxBuf::new(rx_descriptors, rx_buffer).unwrap();
+        let mut dma_tx_buf = DmaTxBuf::new(tx_descriptors, tx_buffer).unwrap();
+
+        for (i, d) in dma_tx_buf.as_mut_slice().iter_mut().enumerate() {
+            *d = i as _;
+        }
+
+        let spi = ctx
+            .spi
+            .with_dma(ctx.dma_channel.configure(false, DmaPriority::Priority0));
+        let transfer = spi
+            .dma_transfer(dma_rx_buf, dma_tx_buf)
+            .map_err(|e| e.0)
+            .unwrap();
+        let (_, (dma_rx_buf, dma_tx_buf)) = transfer.wait();
+        assert_eq!(dma_tx_buf.as_slice(), dma_rx_buf.as_slice());
+    }
+
+    #[test]
+    #[timeout(3)]
+    fn test_dma_bus_symmetric_transfer(ctx: Context) {
+        let (rx_buffer, rx_descriptors, tx_buffer, tx_descriptors) = dma_buffers!(4);
+        let dma_rx_buf = DmaRxBuf::new(rx_descriptors, rx_buffer).unwrap();
+        let dma_tx_buf = DmaTxBuf::new(tx_descriptors, tx_buffer).unwrap();
+
+        let mut spi = ctx
+            .spi
+            .with_dma(ctx.dma_channel.configure(false, DmaPriority::Priority0))
+            .with_buffers(dma_rx_buf, dma_tx_buf);
+
+        let tx_buf = [0xde, 0xad, 0xbe, 0xef];
+        let mut rx_buf = [0; 4];
+
+        spi.transfer(&mut rx_buf, &tx_buf).unwrap();
+
+        assert_eq!(tx_buf, rx_buf);
+    }
+
+    #[test]
+    #[timeout(3)]
+    fn test_dma_bus_asymmetric_transfer(ctx: Context) {
+        let (rx_buffer, rx_descriptors, tx_buffer, tx_descriptors) = dma_buffers!(4);
+        let dma_rx_buf = DmaRxBuf::new(rx_descriptors, rx_buffer).unwrap();
+        let dma_tx_buf = DmaTxBuf::new(tx_descriptors, tx_buffer).unwrap();
+
+        let mut spi = ctx
+            .spi
+            .with_dma(ctx.dma_channel.configure(false, DmaPriority::Priority0))
+            .with_buffers(dma_rx_buf, dma_tx_buf);
+
+        let tx_buf = [0xde, 0xad, 0xbe, 0xef];
+        let mut rx_buf = [0; 4];
+
+        spi.transfer(&mut rx_buf, &tx_buf).unwrap();
+
+        assert_eq!(&tx_buf[0..1], &rx_buf[0..1]);
+    }
+
+    #[test]
+    #[timeout(3)]
+    fn test_dma_bus_symmetric_transfer_huge_buffer(ctx: Context) {
+        const DMA_BUFFER_SIZE: usize = 4096;
+
+        let (rx_buffer, rx_descriptors, tx_buffer, tx_descriptors) = dma_buffers!(40);
+        let dma_rx_buf = DmaRxBuf::new(rx_descriptors, rx_buffer).unwrap();
+        let dma_tx_buf = DmaTxBuf::new(tx_descriptors, tx_buffer).unwrap();
+
+        let mut spi = ctx
+            .spi
+            .with_dma(ctx.dma_channel.configure(false, DmaPriority::Priority0))
+            .with_buffers(dma_rx_buf, dma_tx_buf);
+
+        let tx_buf = core::array::from_fn(|i| i as _);
+        let mut rx_buf = [0; DMA_BUFFER_SIZE];
+
+        spi.transfer(&mut rx_buf, &tx_buf).unwrap();
+
+        assert_eq!(tx_buf, rx_buf);
+    }
+
+    #[test]
+    #[timeout(3)]
+    #[cfg(pcnt)]
+    async fn test_async_dma_read_dma_write_pcnt(ctx: Context) {
+        const DMA_BUFFER_SIZE: usize = 5;
+        let (rx_buffer, rx_descriptors, tx_buffer, tx_descriptors) = dma_buffers!(DMA_BUFFER_SIZE);
+        let dma_rx_buf = DmaRxBuf::new(rx_descriptors, rx_buffer).unwrap();
+        let dma_tx_buf = DmaTxBuf::new(tx_descriptors, tx_buffer).unwrap();
+        let mut spi = ctx
+            .spi
+            .with_dma(
+                ctx.dma_channel
+                    .configure_for_async(false, DmaPriority::Priority0),
+            )
+            .with_buffers(dma_rx_buf, dma_tx_buf);
+
+        ctx.pcnt_unit.channel0.set_edge_signal(ctx.pcnt_source);
+        ctx.pcnt_unit
+            .channel0
+            .set_input_mode(EdgeMode::Hold, EdgeMode::Increment);
+
+        let mut receive = [0; DMA_BUFFER_SIZE];
+
+        // Fill the buffer where each byte has 3 pos edges.
+        let transmit = [0b0110_1010; DMA_BUFFER_SIZE];
+
+        for i in 1..4 {
+            receive.copy_from_slice(&[5, 5, 5, 5, 5]);
+            SpiBusAsync::read(&mut spi, &mut receive).await.unwrap();
+            assert_eq!(receive, [0, 0, 0, 0, 0]);
+
+            SpiBusAsync::write(&mut spi, &transmit).await.unwrap();
+            assert_eq!(ctx.pcnt_unit.get_value(), (i * 3 * DMA_BUFFER_SIZE) as _);
+        }
+    }
+
+    #[test]
+    #[timeout(3)]
+    #[cfg(pcnt)]
+    async fn test_async_dma_read_dma_transfer_pcnt(ctx: Context) {
+        const DMA_BUFFER_SIZE: usize = 5;
+        let (rx_buffer, rx_descriptors, tx_buffer, tx_descriptors) = dma_buffers!(DMA_BUFFER_SIZE);
+        let dma_rx_buf = DmaRxBuf::new(rx_descriptors, rx_buffer).unwrap();
+        let dma_tx_buf = DmaTxBuf::new(tx_descriptors, tx_buffer).unwrap();
+        let mut spi = ctx
+            .spi
+            .with_dma(
+                ctx.dma_channel
+                    .configure_for_async(false, DmaPriority::Priority0),
+            )
+            .with_buffers(dma_rx_buf, dma_tx_buf);
+
+        ctx.pcnt_unit.channel0.set_edge_signal(ctx.pcnt_source);
+        ctx.pcnt_unit
+            .channel0
+            .set_input_mode(EdgeMode::Hold, EdgeMode::Increment);
+
+        let mut receive = [0; DMA_BUFFER_SIZE];
+
+        // Fill the buffer where each byte has 3 pos edges.
+        let transmit = [0b0110_1010; DMA_BUFFER_SIZE];
+
+        for i in 1..4 {
+            receive.copy_from_slice(&[5, 5, 5, 5, 5]);
+            SpiBusAsync::read(&mut spi, &mut receive).await.unwrap();
+            assert_eq!(receive, [0, 0, 0, 0, 0]);
+
+            SpiBusAsync::transfer(&mut spi, &mut receive, &transmit)
+                .await
+                .unwrap();
+            assert_eq!(ctx.pcnt_unit.get_value(), (i * 3 * DMA_BUFFER_SIZE) as _);
+        }
+    }
+
+    #[test]
+    #[timeout(3)]
+    fn test_write_read(ctx: Context) {
+        let spi = ctx
+            .spi
+            .with_miso(Level::High)
+            .with_dma(ctx.dma_channel.configure(false, DmaPriority::Priority0));
+
+        let (rx_buffer, rx_descriptors, tx_buffer, tx_descriptors) = dma_buffers!(4);
+        let mut dma_rx_buf = DmaRxBuf::new(rx_descriptors, rx_buffer).unwrap();
+        let mut dma_tx_buf = DmaTxBuf::new(tx_descriptors, tx_buffer).unwrap();
+
+        dma_tx_buf.fill(&[0xde, 0xad, 0xbe, 0xef]);
+
+        let transfer = spi.dma_write(dma_tx_buf).map_err(|e| e.0).unwrap();
+        let (spi, dma_tx_buf) = transfer.wait();
+
+        dma_rx_buf.as_mut_slice().fill(0);
+        let transfer = spi.dma_read(dma_rx_buf).map_err(|e| e.0).unwrap();
+        let (spi, mut dma_rx_buf) = transfer.wait();
+
+        let transfer = spi.dma_write(dma_tx_buf).map_err(|e| e.0).unwrap();
+        let (spi, _dma_tx_buf) = transfer.wait();
+
+        dma_rx_buf.as_mut_slice().fill(0);
+        let transfer = spi.dma_read(dma_rx_buf).map_err(|e| e.0).unwrap();
+        let (_, dma_rx_buf) = transfer.wait();
+
+        assert_eq!(&[0xff, 0xff, 0xff, 0xff], dma_rx_buf.as_slice());
     }
 }
