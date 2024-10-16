59c4b3ac35b73c1f5d66a739ef3a9971049fdc56
Jesse Braham <jesse@beta7.io>
2024-10-16 10:43:05 +0200

    Fix nightly lint errors for ESP32-H2

--- esp-hal/src/parl_io.rs
+++ esp-hal/src/parl_io.rs
@@ -576,8 +576,8 @@ impl<P0, P1, P2, P3, P4, P5, P6, P7> NotContainsValidSignalPin
 }
 
 #[cfg(esp32h2)]
-impl<'d, P0, P1, P2, P3, P4, P5, P6, P7> ContainsValidSignalPin
-    for TxEightBits<'d, P0, P1, P2, P3, P4, P5, P6, P7>
+impl<P0, P1, P2, P3, P4, P5, P6, P7> ContainsValidSignalPin
+    for TxEightBits<'_, P0, P1, P2, P3, P4, P5, P6, P7>
 {
 }
 
@@ -822,8 +822,8 @@ impl<P0, P1, P2, P3, P4, P5, P6, P7> NotContainsValidSignalPin
 }
 
 #[cfg(esp32h2)]
-impl<'d, P0, P1, P2, P3, P4, P5, P6, P7> ContainsValidSignalPin
-    for RxEightBits<'d, P0, P1, P2, P3, P4, P5, P6, P7>
+impl<P0, P1, P2, P3, P4, P5, P6, P7> ContainsValidSignalPin
+    for RxEightBits<'_, P0, P1, P2, P3, P4, P5, P6, P7>
 {
 }
 
