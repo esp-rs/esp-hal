beb8215c60458614e3b2ee4d1be6e7a9571d34be
Dániel Buga <bugadani@gmail.com>
2024-10-21 15:48:06 +0200

    Explain peripherals macro

--- esp-hal/src/peripheral.rs
+++ esp-hal/src/peripheral.rs
@@ -207,6 +207,20 @@ impl<T: Peripheral> Peripheral for PeripheralRef<'_, T> {
 }
 
 mod peripheral_macros {
+    /// Creates a new `Peripherals` struct and its associated methods.
+    ///
+    /// The macro has a few fields doing different things, in the form of
+    /// `[first] second <= third (fourth)`.
+    /// - The first field is the name of the `Peripherals` enum variant. This is
+    ///   optional and used to create `PeripheralMarker` implementations for
+    ///   DMA-eligible peripherals.
+    /// - The second field is the name of the peripheral, as it appears in the
+    ///   `Peripherals` struct.
+    /// - The third field is the name of the peripheral as it appears in the
+    ///   PAC. This may be `virtual` if the peripheral is not present in the
+    ///   PAC.
+    /// - The fourth field is an optional list of interrupts that can be bound
+    ///   to the peripheral.
     #[doc(hidden)]
     #[macro_export]
     macro_rules! peripherals {
