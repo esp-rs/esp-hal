56cd5a288fd7dc7b4a072f48c276e4a008256459
Jesse Braham <jessebraham@users.noreply.github.com>
2024-10-22 05:33:36 -0700

    Update PACs to latest git revision (#2383)
    * Update to latest git revision of all PACs
    
    * Update RSA module to reflect changes in PACs
    
    * Update TWAI module to reflect changes in PACs
    
    * Further simplification

--- esp-hal/src/rsa/esp32sX.rs
+++ esp-hal/src/rsa/esp32sX.rs
@@ -27,16 +27,7 @@ impl<'d, DM: crate::Mode> Rsa<'d, DM> {
     /// When enabled rsa peripheral would generate an interrupt when a operation
     /// is finished.
     pub fn enable_disable_interrupt(&mut self, enable: bool) {
-        match enable {
-            true => self
-                .rsa
-                .interrupt_ena()
-                .write(|w| w.interrupt_ena().set_bit()),
-            false => self
-                .rsa
-                .interrupt_ena()
-                .write(|w| w.interrupt_ena().clear_bit()),
-        }
+        self.rsa.int_ena().write(|w| w.int_ena().bit(enable))
     }
 
     fn write_mode(&mut self, mode: u32) {
@@ -121,9 +112,7 @@ impl<'d, DM: crate::Mode> Rsa<'d, DM> {
 
     /// Clears the RSA interrupt flag.
     pub(super) fn clear_interrupt(&mut self) {
-        self.rsa
-            .clear_interrupt()
-            .write(|w| w.clear_interrupt().set_bit());
+        self.rsa.int_clr().write(|w| w.int_clr().set_bit());
     }
 
     /// Checks if the RSA peripheral is idle.
