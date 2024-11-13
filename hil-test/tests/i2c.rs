e590f75103e86c3f3fe35887a86ec702f70dadd8
Dániel Buga <bugadani@gmail.com>
2024-11-13 10:33:31 +0100

    Name addresses

--- hil-test/tests/i2c.rs
+++ hil-test/tests/i2c.rs
@@ -24,6 +24,9 @@ fn _async_driver_is_compatible_with_blocking_ehal() {
     fn _with_ehal(_: impl embedded_hal::i2c::I2c) {}
 }
 
+const DUT_ADDRESS: u8 = 0x77;
+const NON_EXISTENT_ADDRESS: u8 = 0x6b;
+
 #[cfg(test)]
 #[embedded_test::tests]
 mod tests {
@@ -47,8 +50,8 @@ mod tests {
     #[test]
     #[timeout(3)]
     fn empty_write_returns_ack_error_for_unknown_address(mut ctx: Context) {
-        assert_eq!(ctx.i2c.write(0xab, &[]), Err(Error::AckCheckFailed));
-        assert_eq!(ctx.i2c.write(0x77, &[]), Ok(()));
+        assert_eq!(ctx.i2c.write(NON_EXISTENT_ADDRESS, &[]), Err(Error::AckCheckFailed));
+        assert_eq!(ctx.i2c.write(DUT_ADDRESS, &[]), Ok(()));
     }
 
     #[test]
@@ -58,10 +61,10 @@ mod tests {
 
         // have a failing read which might could leave the peripheral in an undesirable
         // state
-        ctx.i2c.write_read(0x55, &[0xaa], &mut read_data).ok();
+        ctx.i2c.write_read(NON_EXISTENT_ADDRESS, &[0xaa], &mut read_data).ok();
 
         // do the real read which should succeed
-        ctx.i2c.write_read(0x77, &[0xaa], &mut read_data).ok();
+        ctx.i2c.write_read(DUT_ADDRESS, &[0xaa], &mut read_data).ok();
 
         assert_ne!(read_data, [0u8; 22])
     }
@@ -74,7 +77,7 @@ mod tests {
         // do the real read which should succeed
         ctx.i2c
             .transaction(
-                0x77,
+                DUT_ADDRESS,
                 &mut [Operation::Write(&[0xaa]), Operation::Read(&mut read_data)],
             )
             .ok();
