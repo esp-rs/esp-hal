98e469285b74c7f457868cde43bcdf667b15433a
Scott Mabin <scott@mabez.dev>
2024-11-21 12:13:42 +0000

    fix examples and tests (by removing them :D)

--- examples/src/lib.rs
+++ examples/src/lib.rs
@@ -1,13 +1,1 @@
 #![no_std]
-
-pub fn cycles() -> u64 {
-    #[cfg(feature = "esp32")]
-    {
-        esp_hal::xtensa_lx::timer::get_cycle_count() as u64
-    }
-
-    #[cfg(not(feature = "esp32"))]
-    {
-        esp_hal::timer::systimer::SystemTimer::unit_count(Unit::Unit0)
-    }
-}
