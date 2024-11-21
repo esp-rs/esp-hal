90f1eb8f997b8fe2c1cb60b8a177003c565d0073
Scott Mabin <scott@mabez.dev>
2024-11-21 21:46:47 +0000

    re add etm example, fix etm for systimer

---
+++ examples/src/bin/etm_timer.rs
@@ -0,0 +1,62 @@
+//! Control LED by the boot button via ETM without involving the CPU.
+
+//! The following wiring is assumed:
+//! - LED => GPIO1
+
+//% CHIPS: esp32c6 esp32h2
+
+#![no_std]
+#![no_main]
+
+use esp_backtrace as _;
+use esp_hal::{
+    etm::Etm,
+    gpio::{
+        etm::{Channels, OutputConfig},
+        Level,
+        Output,
+        Pull,
+    },
+    prelude::*,
+    timer::{
+        systimer::{etm::Event, SystemTimer},
+        PeriodicTimer,
+    },
+};
+
+#[entry]
+fn main() -> ! {
+    let peripherals = esp_hal::init(esp_hal::Config::default());
+
+    let mut led = Output::new(peripherals.GPIO2, Level::Low);
+    led.set_high();
+
+    let syst = SystemTimer::new(peripherals.SYSTIMER);
+    let alarm = syst.alarm0;
+
+    let timer_event = Event::new(&alarm);
+
+    // setup ETM
+    let gpio_ext = Channels::new(peripherals.GPIO_SD);
+    let led_task = gpio_ext.channel0_task.toggle(
+        led,
+        OutputConfig {
+            open_drain: false,
+            pull: Pull::None,
+            initial_state: Level::Low,
+        },
+    );
+
+    let etm = Etm::new(peripherals.SOC_ETM);
+    let channel0 = etm.channel0;
+
+    // make sure the configured channel doesn't get dropped - dropping it will
+    // disable the channel
+    let _configured_channel = channel0.setup(&timer_event, &led_task);
+
+    let mut timer = PeriodicTimer::new(alarm);
+    timer.start(1u64.secs()).unwrap();
+
+    // the LED is controlled by the button without involving the CPU
+    loop {}
+}
