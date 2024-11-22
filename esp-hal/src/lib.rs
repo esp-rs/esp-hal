869f4bc3ed2430b381c536957af15372daaf819d
Dániel Buga <bugadani@gmail.com>
2024-11-22 21:31:16 +0100

    Move AtomicWaker, reimplement using Generic

--- esp-hal/src/lib.rs
+++ esp-hal/src/lib.rs
@@ -145,6 +145,8 @@
 // MUST be the first module
 mod fmt;
 
+pub mod asynch;
+
 #[cfg(riscv)]
 pub use esp_riscv_rt::{self, entry, riscv};
 #[cfg(xtensa)]
@@ -571,49 +573,4 @@ pub fn init(config: Config) -> Peripherals {
     crate::gpio::bind_default_interrupt_handler();
 
     peripherals
-}
-
-/// Asynchronous utilities.
-pub mod asynch {
-    use core::{cell::Cell, task::Waker};
-
-    use embassy_sync::blocking_mutex::Mutex;
-
-    use crate::sync::RawMutex;
-
-    /// TODO: this just exists to test RawMutex, otherwise it should be replaced
-    /// by embassy_sync::waitqueue::atomic_waker::GenericAtomicWaker.
-    pub struct AtomicWaker {
-        waker: Mutex<RawMutex, Cell<Option<Waker>>>,
-    }
-
-    impl AtomicWaker {
-        /// Create a new `AtomicWaker`.
-        #[allow(clippy::new_without_default)]
-        pub const fn new() -> Self {
-            Self {
-                waker: Mutex::const_new(RawMutex::new(), Cell::new(None)),
-            }
-        }
-
-        /// Register a waker. Overwrites the previous waker, if any.
-        pub fn register(&self, w: &Waker) {
-            self.waker.lock(|cell| {
-                cell.set(match cell.replace(None) {
-                    Some(w2) if (w2.will_wake(w)) => Some(w2),
-                    _ => Some(w.clone()),
-                })
-            })
-        }
-
-        /// Wake the registered waker, if any.
-        pub fn wake(&self) {
-            self.waker.lock(|cell| {
-                if let Some(w) = cell.replace(None) {
-                    w.wake_by_ref();
-                    cell.set(Some(w));
-                }
-            })
-        }
-    }
 }
