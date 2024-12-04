ad3aea8817ff30e8bdd86b588084129490766c85
Juraj Sadel <juraj.sadel@espressif.com>
2024-12-04 12:12:50 +0100

    sha: increase test_digest_of_size_1_to_200 timeout from 10 to 15 seconds

--- hil-test/tests/sha.rs
+++ hil-test/tests/sha.rs
@@ -216,7 +216,7 @@ mod tests {
     /// A test that runs a hashing on a digest of every size between 1 and 200
     /// inclusively.
     #[test]
-    #[timeout(10)]
+    #[timeout(15)]
     fn test_digest_of_size_1_to_200(mut ctx: Context) {
         for i in 1..=200 {
             assert_sha::<Sha1, 20>(&mut ctx.sha, &SOURCE_DATA[..i]);
