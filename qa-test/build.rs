62994578c21a467b1f6862e97981b8eabd4ee4b2
Björn Quentin <bjoernQ@users.noreply.github.com>
2024-12-10 16:32:29 +0100

    Fix qa-test build.rs (#2722)
    * Fix qa-test build.rs
    
    * Once is enough

--- qa-test/build.rs
+++ qa-test/build.rs
@@ -2,8 +2,11 @@ fn main() {
     // Allow building QA tests in CI in debug mode
     println!("cargo:rustc-check-cfg=cfg(is_not_release)");
     println!("cargo:rerun-if-env-changed=CI");
-    #[cfg(debug_assertions)]
     if std::env::var("CI").is_err() {
-        println!("cargo::rustc-cfg=is_not_release");
+        if let Ok(level) = std::env::var("OPT_LEVEL") {
+            if level == "0" || level == "1" {
+                println!("cargo::rustc-cfg=is_not_release");
+            }
+        }
     }
 }
