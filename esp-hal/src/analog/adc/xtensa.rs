a0364f492bad4c05c950feedf35b959f39289896
Sergio Gasquez <sergio.gasquez@gmail.com>
2025-01-02 16:20:44 +0100

    feat: Avoid using nb::Result in adc::read_oneshot

--- esp-hal/src/analog/adc/xtensa.rs
+++ esp-hal/src/analog/adc/xtensa.rs
@@ -502,10 +502,7 @@ where
     /// This method takes an [AdcPin](super::AdcPin) reference, as it is
     /// expected that the ADC will be able to sample whatever channel
     /// underlies the pin.
-    pub fn read_oneshot<PIN, CS>(
-        &mut self,
-        pin: &mut super::AdcPin<PIN, ADCI, CS>,
-    ) -> nb::Result<u16, ()>
+    pub fn read_oneshot<PIN, CS>(&mut self, pin: &mut super::AdcPin<PIN, ADCI, CS>) -> Option<u16>
     where
         PIN: super::AdcChannel,
         CS: super::AdcCalScheme<ADCI>,
@@ -515,7 +512,7 @@ where
             // - if it's for a different channel try again later
             // - if it's for the given channel, go ahead and check progress
             if active_channel != PIN::CHANNEL {
-                return Err(nb::Error::WouldBlock);
+                return None;
             }
         } else {
             // If no conversions are in progress, start a new one for given channel
@@ -527,7 +524,7 @@ where
         // Wait for ADC to finish conversion
         let conversion_finished = ADCI::is_done();
         if !conversion_finished {
-            return Err(nb::Error::WouldBlock);
+            return None;
         }
 
         // Get converted value
@@ -540,7 +537,7 @@ where
         // Mark that no conversions are currently in progress
         self.active_channel = None;
 
-        Ok(converted_value)
+        Some(converted_value)
     }
 
     fn start_sample<PIN, CS>(&mut self, pin: &mut AdcPin<PIN, ADCI, CS>)
