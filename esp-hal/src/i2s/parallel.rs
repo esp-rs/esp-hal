1d88d53713c19fa8cc977b84e95dda6c9029626a
liebman <liebman@zod.com>
2025-01-04 10:23:05 -0800

    i2s: fix pin offset for 8bit parallel on i2s1

--- esp-hal/src/i2s/parallel.rs
+++ esp-hal/src/i2s/parallel.rs
@@ -736,8 +736,9 @@ impl Instance for I2S1 {
             bits
         );
 
-        // signals for 8bit and 16bit both start at an offset of 8 for I2S1
-        // https://github.com/espressif/esp-idf/blob/9106c43accd9f5e75379f62f12597677213f5023/components/esp_lcd/i80/esp_lcd_panel_io_i2s.c#L701
+        // signals for 8bit  start at an offset of  0 and 16bit start at an offset of 8 for I2S1
+        let pin_offset = if bits == 8 { 0 } else { 8 };
+
         match i + 8 {
             0 => OutputSignal::I2S1O_DATA_0,
             1 => OutputSignal::I2S1O_DATA_1,
