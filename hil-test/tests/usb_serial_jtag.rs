bb614ebca511f44ad98c83c38cce1ffa24ca673b
Dániel Buga <bugadani@gmail.com>
2025-01-07 15:56:40 +0100

    Fix usb serial jtag

--- hil-test/tests/usb_serial_jtag.rs
+++ hil-test/tests/usb_serial_jtag.rs
@@ -9,15 +9,12 @@
 #[cfg(test)]
 #[embedded_test::tests(default_timeout = 3)]
 mod tests {
-    use esp_hal::{timer::timg::TimerGroup, usb_serial_jtag::UsbSerialJtag};
+    use esp_hal::usb_serial_jtag::UsbSerialJtag;
     use hil_test as _;
 
     #[test]
     fn creating_peripheral_does_not_break_debug_connection() {
         let peripherals = esp_hal::init(esp_hal::Config::default());
-
-        let timg0 = TimerGroup::new(peripherals.TIMG0);
-        esp_hal_embassy::init(timg0.timer0);
 
         _ = UsbSerialJtag::new(peripherals.USB_DEVICE)
             .into_async()
