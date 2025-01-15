20ad8d178cc4498b2b45bd7e5b834a6d35e83d39
Dániel Buga <bugadani@gmail.com>
2025-01-15 09:49:53 +0100

    Create newtypes for all PAC peripherals, mark unstable peripheral singletons

--- esp-hal/src/soc/esp32h2/peripherals.rs
+++ esp-hal/src/soc/esp32h2/peripherals.rs
@@ -8,7 +8,7 @@
 //! from the PAC, allowing users to handle interrupts associated with these
 //! peripherals.
 
-use esp32h2 as pac;
+pub(crate) use esp32h2 as pac;
 // We need to export this for users to use
 pub use pac::Interrupt;
 
@@ -21,24 +21,32 @@ pub(crate) use self::peripherals::*;
 // creating "virtual peripherals" for them.
 crate::peripherals! {
     peripherals: [
+        I2C0 <= I2C0,
+        I2C1 <= I2C1,
+        IO_MUX <= IO_MUX,
+        SPI2 <= SPI2 (SPI2),
+        UART0 <= UART0,
+        UART1 <= UART1,
+    ],
+    unstable_peripherals: [
         ADC1 <= virtual,
         AES <= AES,
+        APB_SARADC <= APB_SARADC,
         ASSIST_DEBUG <= ASSIST_DEBUG,
         BT <= virtual,
+        DMA <= DMA,
         DS <= DS,
         ECC <= ECC,
         EFUSE <= EFUSE,
+        GPIO <= GPIO,
         GPIO_SD <= GPIO_SD,
         HMAC <= HMAC,
         HP_APM <= HP_APM,
         HP_SYS <= HP_SYS,
-        I2C0 <= I2C0,
-        I2C1 <= I2C1,
         I2S0 <= I2S0 (I2S0),
         IEEE802154 <= IEEE802154,
         INTERRUPT_CORE0 <= INTERRUPT_CORE0,
         INTPRI <= INTPRI,
-        IO_MUX <= IO_MUX,
         LEDC <= LEDC,
         LPWR <= LP_CLKRST,
         LP_ANA <= LP_ANA,
@@ -55,6 +63,8 @@ crate::peripherals! {
         PARL_IO <= PARL_IO (PARL_IO_TX, PARL_IO_RX),
         PAU <= PAU,
         PCNT <= PCNT,
+        PCR <= PCR,
+        PLIC_MX <= PLIC_MX,
         PMU <= PMU,
         RADIO_CLK <= virtual,
         RMT <= RMT,
@@ -64,7 +74,6 @@ crate::peripherals! {
         SOC_ETM <= SOC_ETM,
         SPI0 <= SPI0,
         SPI1 <= SPI1,
-        SPI2 <= SPI2 (SPI2),
         SYSTEM <= PCR,
         SYSTIMER <= SYSTIMER,
         SW_INTERRUPT <= virtual,
@@ -73,8 +82,6 @@ crate::peripherals! {
         TIMG1 <= TIMG1,
         TRACE0 <= TRACE,
         TWAI0 <= TWAI0,
-        UART0 <= UART0,
-        UART1 <= UART1,
         UHCI0 <= UHCI0,
         USB_DEVICE <= USB_DEVICE,
         MEM2MEM1 <= virtual,
