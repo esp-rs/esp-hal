399d308405ffa183c9fabd1dc8e1388941f1affb
Björn Quentin <bjoernQ@users.noreply.github.com>
2025-01-31 12:43:25 +0100

    Explain

--- hil-test/tests/esp_wifi_ble_controller.rs
+++ hil-test/tests/esp_wifi_ble_controller.rs
@@ -1,4 +1,14 @@
 //! Test we can talk to the BLE controller.
+//!
+//! We just send a reset command and check that we get a command complete for
+//! the command.
+//!
+//! This is at least enough to know BLE is initialized find and the controller
+//! responds as expected.
+//!
+//! See Bluetooth Core Specification v5.3
+//!
+//! THIS DOESN'T ACTUALLY TEST THE RADIO HOWEVER.
 
 //% CHIPS: esp32 esp32c2 esp32c3 esp32c6 esp32h2 esp32s3
 //% FEATURES: unstable esp-wifi esp-alloc esp-wifi/ble
@@ -7,21 +17,19 @@
 #![no_main]
 
 use embedded_io::{Read, Write};
+use esp_hal::{clock::CpuClock, peripherals::Peripherals, rng::Rng, timer::timg::TimerGroup};
+use esp_wifi::ble::controller::BleConnector;
 use hil_test as _;
 
 #[cfg(test)]
 #[embedded_test::tests(default_timeout = 3)]
 mod tests {
-    use esp_hal::{clock::CpuClock, peripherals::Peripherals, rng::Rng, timer::timg::TimerGroup};
-    use esp_wifi::ble::controller::BleConnector;
-
     use super::*;
 
     const H4_TYPE_COMMAND: u8 = 1;
-    const HCI_GRP_HOST_CONT_BASEBAND_CMDS: u16 = 0x03 << 10;
 
-    const HCI_RESET_BYTE0: u8 = ((0x0003 | HCI_GRP_HOST_CONT_BASEBAND_CMDS) & 0xff00 >> 8) as u8;
-    const HCI_RESET_BYTE1: u8 = ((0x0003 | HCI_GRP_HOST_CONT_BASEBAND_CMDS) & 0xff) as u8;
+    pub const CONTROLLER_OGF: u8 = 0x03;
+    pub const RESET_OCF: u16 = 0x03;
 
     #[init]
     fn init() -> Peripherals {
@@ -43,24 +51,41 @@ mod tests {
 
         let mut connector = BleConnector::new(&init, &mut peripherals.BT);
 
-        // send reset
-        connector
-            .write(&[H4_TYPE_COMMAND, HCI_RESET_BYTE0, HCI_RESET_BYTE1, 0])
-            .unwrap();
+        // send reset cmd
+        pub const fn opcode(ogf: u8, ocf: u16) -> [u8; 2] {
+            let opcode = ((ogf as u16) << 10) + ocf as u16;
+            opcode.to_le_bytes()
+        }
 
-        // wait for an expected response - we might get some other events, too
+        let opcode = opcode(CONTROLLER_OGF, RESET_OCF);
+
+        let mut buf = [0u8; 4];
+        buf[0] = H4_TYPE_COMMAND;
+        buf[1] = opcode[0];
+        buf[2] = opcode[1];
+        buf[3] = 0; // payload length - RESET COMMAND doesn't have any parameters
+
+        connector.write(&buf).unwrap();
+
+        // wait for an expected response - we might get some other events, too, since
+        // different chips have different defaults for which events are masked away
         // so if we don't get the expected response we'll timeout and fail
         let mut buf = [0u8; 255];
         loop {
             let len = connector.read(&mut buf).unwrap();
             if len == 7 {
-                assert_eq!(buf[0], 4);
-                assert_eq!(buf[1], 14);
-                assert_eq!(buf[2], 4);
+                assert_eq!(buf[0], 4, "Expected packet type = 4 (EVENT)");
+                assert_eq!(buf[1], 14, "Expected event code = 14 (COMMAND_COMPLETE)");
+                assert_eq!(buf[2], 4, "Expected payload length = 4");
 
-                assert_eq!(buf[4], 3);
-                assert_eq!(buf[5], 3);
-                assert_eq!(buf[6], 1);
+                // don't care about NUM_HANDLES (different for different chips)
+
+                assert_eq!(buf[4], opcode[0], "Unexpected op-code first byte");
+                assert_eq!(buf[5], opcode[1], "Unexpected op-code second byte");
+                assert_eq!(
+                    buf[6], 0,
+                    "Expected RESET COMMAND to succeed (return code = 0)"
+                );
                 break;
             }
         }
