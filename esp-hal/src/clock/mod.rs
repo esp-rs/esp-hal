ea24d4ec6d39cc697f3a2b74890abf86fd19db83
Dániel Buga <bugadani@gmail.com>
2025-02-03 09:36:06 +0100

    Explain Other(0)

--- esp-hal/src/clock/mod.rs
+++ esp-hal/src/clock/mod.rs
@@ -341,7 +341,7 @@ impl Clocks {
         } else {
             const {
                 match esp_config::esp_config_str!("ESP_HAL_CONFIG_XTAL_FREQUENCY").as_bytes() {
-                    b"auto" => XtalClock::Other(0),
+                    b"auto" => XtalClock::Other(0), // Can't be `unreachable!` due to const eval.
                     b"26" => XtalClock::_26M,
                     b"40" => XtalClock::_40M,
                     other => XtalClock::Other(esp_config::esp_config_int_parse!(u32, other)),
@@ -392,7 +392,7 @@ impl Clocks {
         } else {
             const {
                 match esp_config::esp_config_str!("ESP_HAL_CONFIG_XTAL_FREQUENCY").as_bytes() {
-                    b"auto" => XtalClock::Other(0),
+                    b"auto" => XtalClock::Other(0), // Can't be `unreachable!` due to const eval.
                     b"26" => XtalClock::_26M,
                     b"40" => XtalClock::_40M,
                     other => XtalClock::Other(esp_config::esp_config_int_parse!(u32, other)),
