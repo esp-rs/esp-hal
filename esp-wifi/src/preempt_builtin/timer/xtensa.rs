9abdcdc01007e576445f691e05847664bbaaafca
Dániel Buga <bugadani@gmail.com>
2025-02-28 08:45:42 +0100

    Keep delegating to the SW interrupt on ESP32

--- esp-wifi/src/preempt_builtin/timer/xtensa.rs
+++ esp-wifi/src/preempt_builtin/timer/xtensa.rs
@@ -11,7 +11,9 @@ const TIMESLICE_FREQUENCY: Rate = Rate::from_hz(crate::CONFIG.tick_rate_hz);
 
 use super::TIMER;
 
-const SW_INTERRUPT: u32 = 1 << 7;
+// ESP32 uses Software1 (priority 3) for task switching, because it reserves
+// Software0 for the Bluetooth stack.
+const SW_INTERRUPT: u32 = if cfg!(esp32) { 1 << 29 } else { 1 << 7 };
 
 pub(crate) fn setup_timer(mut timer1: TimeBase) {
     // The timer needs to tick at Priority 1 to prevent accidentally interrupting
@@ -53,18 +55,29 @@ pub(crate) fn disable_multitasking() {
     xtensa_lx::interrupt::disable_mask(SW_INTERRUPT);
 }
 
-extern "C" fn timer_tick_handler(context: &mut TrapFrame) {
+extern "C" fn timer_tick_handler(_context: &mut TrapFrame) {
     TIMER.with(|timer| {
         let timer = unwrap!(timer.as_mut());
         timer.clear_interrupt();
     });
 
-    task_switch(context);
+    // `task_switch` must be called on a single interrupt priority level only.
+    // Because on ESP32 the software interrupt is triggered at priority 3 but
+    // the timer interrupt is triggered at priority 1, we need to trigger the
+    // software interrupt manually.
+    cfg_if::cfg_if! {
+        if #[cfg(esp32)] {
+            yield_task();
+        } else {
+            task_switch(_context);
+        }
+    }
 }
 
 #[allow(non_snake_case)]
-#[no_mangle]
-fn Software0(_level: u32, context: &mut TrapFrame) {
+#[cfg_attr(not(esp32), export_name = "Software0")]
+#[cfg_attr(esp32, export_name = "Software1")]
+fn task_switch_interrupt(_level: u32, context: &mut TrapFrame) {
     let intr = SW_INTERRUPT;
     unsafe { core::arch::asm!("wsr.intclear  {0}", in(reg) intr, options(nostack)) };
 
