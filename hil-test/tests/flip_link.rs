b72c02a3577cfe048807417062d36392cf422855
Dániel Buga <bugadani@gmail.com>
2025-03-04 08:47:29 +0100

    Test flip-link

---
+++ hil-test/tests/flip_link.rs
@@ -0,0 +1,20 @@
+//! Tests flip_link
+
+//% CHIPS: esp32c6 esp32h2
+//% ENV: ESP_HAL_CONFIG_FLIP_LINK = true
+
+#![no_std]
+#![no_main]
+
+use hil_test as _;
+
+#[cfg(test)]
+#[embedded_test::tests(default_timeout = 3)]
+mod tests {
+    #[test]
+    fn test() {
+        let _p = esp_hal::init(Default::default());
+        defmt::info!("Hello, world!");
+        defmt::assert_eq!(1 + 1, 2);
+    }
+}
