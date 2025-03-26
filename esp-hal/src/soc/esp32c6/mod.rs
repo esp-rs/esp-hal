51747a0ad64806a7a2b744a9531beb3b40be0737
Björn Quentin <bjoernQ@users.noreply.github.com>
2025-03-26 09:44:07 +0100

    Define DRAM memory region in esp-metadata

--- esp-hal/src/soc/esp32c6/mod.rs
+++ esp-hal/src/soc/esp32c6/mod.rs
@@ -64,11 +64,6 @@ pub(crate) mod constants {
     /// The clock frequency for the Parallel IO peripheral in Hertz.
     pub const PARL_IO_SCLK: u32 = 240_000_000;
 
-    /// The lower address boundary for system DRAM.
-    pub const SOC_DRAM_LOW: usize = 0x4080_0000;
-    /// The upper address boundary for system DRAM.
-    pub const SOC_DRAM_HIGH: usize = 0x4088_0000;
-
     /// RC FAST Clock value (Hertz).
     pub const RC_FAST_CLK: Rate = Rate::from_khz(17_500);
 }
