c7df6a1a81ad1df918fedd6c6ca335fa88fd07b6
Dániel Buga <bugadani@gmail.com>
2025-04-04 09:50:57 +0200

    Use allocator_api2 in esp-wifi (#3320)
    * Add esp_wifi_deallocate_internal_ram
    
    * Polyfill InternalMemory allocator
    
    * Clean up RawQueue
    
    * Scheduler spring-cleaning

--- esp-wifi/src/compat/malloc.rs
+++ esp-wifi/src/compat/malloc.rs
@@ -1,24 +1,18 @@
-use core::alloc::Layout;
-
 #[no_mangle]
 pub unsafe extern "C" fn malloc(size: usize) -> *mut u8 {
     trace!("alloc {}", size);
-
-    let total_size = size + 4;
 
     extern "C" {
         fn esp_wifi_allocate_from_internal_ram(size: usize) -> *mut u8;
     }
 
-    let ptr = unsafe { esp_wifi_allocate_from_internal_ram(total_size) };
+    let ptr = unsafe { esp_wifi_allocate_from_internal_ram(size) };
 
     if ptr.is_null() {
         warn!("Unable to allocate {} bytes", size);
-        return ptr;
     }
 
-    *(ptr as *mut usize) = total_size;
-    ptr.offset(4)
+    ptr
 }
 
 #[no_mangle]
@@ -26,14 +20,15 @@ pub unsafe extern "C" fn free(ptr: *mut u8) {
     trace!("free {:?}", ptr);
 
     if ptr.is_null() {
+        warn!("Attempt to free null pointer");
         return;
     }
 
-    let ptr = ptr.offset(-4);
-    let total_size = *(ptr as *const usize);
+    extern "C" {
+        fn esp_wifi_deallocate_internal_ram(ptr: *mut u8);
+    }
 
-    let layout = Layout::from_size_align_unchecked(total_size, 4);
-    alloc::alloc::dealloc(ptr, layout);
+    esp_wifi_deallocate_internal_ram(ptr);
 }
 
 #[no_mangle]
@@ -85,10 +80,66 @@ pub extern "C" fn esp_wifi_free_internal_heap() -> usize {
 #[doc(hidden)]
 #[no_mangle]
 pub extern "C" fn esp_wifi_allocate_from_internal_ram(size: usize) -> *mut u8 {
+    let total_size = size + 4;
     unsafe {
-        esp_alloc::HEAP.alloc_caps(
+        let ptr = esp_alloc::HEAP.alloc_caps(
             esp_alloc::MemoryCapability::Internal.into(),
-            core::alloc::Layout::from_size_align_unchecked(size, 4),
+            core::alloc::Layout::from_size_align_unchecked(total_size, 4),
+        );
+
+        if ptr.is_null() {
+            return ptr;
+        }
+
+        *(ptr as *mut usize) = total_size;
+        ptr.offset(4)
+    }
+}
+
+#[cfg(feature = "esp-alloc")]
+#[doc(hidden)]
+#[no_mangle]
+pub extern "C" fn esp_wifi_deallocate_internal_ram(ptr: *mut u8) {
+    use core::alloc::GlobalAlloc;
+
+    unsafe {
+        let ptr = ptr.offset(-4);
+        let total_size = *(ptr as *const usize);
+
+        esp_alloc::HEAP.dealloc(
+            ptr,
+            core::alloc::Layout::from_size_align_unchecked(total_size, 4),
         )
     }
 }
+
+// Polyfill the InternalMemory allocator
+#[cfg(not(feature = "esp-alloc"))]
+mod esp_alloc {
+    use core::{alloc::Layout, ptr::NonNull};
+
+    use allocator_api2::alloc::{AllocError, Allocator};
+
+    /// An allocator that uses internal memory only.
+    pub struct InternalMemory;
+
+    unsafe impl Allocator for InternalMemory {
+        fn allocate(&self, layout: Layout) -> Result<NonNull<[u8]>, AllocError> {
+            extern "C" {
+                fn esp_wifi_allocate_from_internal_ram(size: usize) -> *mut u8;
+            }
+            let raw_ptr = unsafe { esp_wifi_allocate_from_internal_ram(layout.size()) };
+            let ptr = NonNull::new(raw_ptr).ok_or(AllocError)?;
+            Ok(NonNull::slice_from_raw_parts(ptr, layout.size()))
+        }
+
+        unsafe fn deallocate(&self, ptr: NonNull<u8>, _layout: Layout) {
+            extern "C" {
+                fn esp_wifi_deallocate_internal_ram(ptr: *mut u8);
+            }
+            esp_wifi_deallocate_internal_ram(ptr.as_ptr());
+        }
+    }
+}
+
+pub(crate) use esp_alloc::InternalMemory;
--- esp-wifi/src/preempt_builtin/preempt_xtensa.rs
+++ esp-wifi/src/preempt_builtin/preempt_xtensa.rs
@@ -1,68 +1,41 @@
 use esp_wifi_sys::c_types;
 
 use super::*;
-use crate::hal::trapframe::TrapFrame;
+pub use crate::hal::trapframe::TrapFrame;
 
-#[derive(Debug, Clone, Copy)]
-pub struct Context {
-    trap_frame: TrapFrame,
-    pub thread_semaphore: u32,
-    pub next: *mut Context,
-    pub allocated_stack: *const u8,
-}
-
-impl Context {
-    pub(crate) fn new() -> Self {
-        Context {
-            trap_frame: TrapFrame::new(),
-            thread_semaphore: 0,
-            next: core::ptr::null_mut(),
-            allocated_stack: core::ptr::null(),
-        }
-    }
-}
-
-pub(crate) fn task_create(
-    task: extern "C" fn(*mut c_types::c_void),
+pub(crate) fn new_task_context(
+    task_fn: extern "C" fn(*mut c_types::c_void),
     param: *mut c_types::c_void,
-    task_stack_size: usize,
-) -> *mut Context {
-    trace!("task_create {:?} {:?} {}", task, param, task_stack_size);
+    stack_top: *mut (),
+) -> TrapFrame {
+    // stack must be aligned by 16
+    let stack_top = stack_top as u32;
+    let stack_top = stack_top - (stack_top % 16);
+
     unsafe {
-        let ctx = allocate_task();
+        *((stack_top - 4) as *mut u32) = 0;
+        *((stack_top - 8) as *mut u32) = 0;
+        *((stack_top - 12) as *mut u32) = stack_top;
+        *((stack_top - 16) as *mut u32) = 0;
+    }
 
-        (*ctx).trap_frame.PC = task as usize as u32;
-        (*ctx).trap_frame.A6 = param as usize as u32;
+    TrapFrame {
+        PC: task_fn as usize as u32,
+        A0: 0,
+        A1: stack_top,
+        A6: param as usize as u32,
 
-        let stack = malloc(task_stack_size as u32);
-        (*ctx).allocated_stack = stack.cast();
+        // For windowed ABI set WOE and CALLINC (pretend task was 'call4'd)
+        PS: 0x00040000 | ((1 & 3) << 16),
 
-        // stack must be aligned by 16
-        let task_stack_ptr = stack as usize + task_stack_size;
-        let stack_ptr = task_stack_ptr - (task_stack_ptr % 16);
-        (*ctx).trap_frame.A1 = stack_ptr as u32;
-
-        (*ctx).trap_frame.PS = 0x00040000 | ((1 & 3) << 16); // For windowed ABI set WOE and CALLINC (pretend task was 'call4'd).
-
-        (*ctx).trap_frame.A0 = 0;
-
-        *((task_stack_ptr - 4) as *mut u32) = 0;
-        *((task_stack_ptr - 8) as *mut u32) = 0;
-        *((task_stack_ptr - 12) as *mut u32) = stack_ptr as u32;
-        *((task_stack_ptr - 16) as *mut u32) = 0;
-
-        ctx
+        ..Default::default()
     }
 }
 
-pub(crate) fn restore_task_context(ctx: *mut Context, trap_frame: &mut TrapFrame) {
-    unsafe {
-        *trap_frame = (*ctx).trap_frame;
-    }
+pub(crate) fn restore_task_context(ctx: &mut Context, trap_frame: &mut TrapFrame) {
+    *trap_frame = ctx.trap_frame;
 }
 
-pub(crate) fn save_task_context(ctx: *mut Context, trap_frame: &TrapFrame) {
-    unsafe {
-        (*ctx).trap_frame = *trap_frame;
-    }
+pub(crate) fn save_task_context(ctx: &mut Context, trap_frame: &TrapFrame) {
+    ctx.trap_frame = *trap_frame;
 }
