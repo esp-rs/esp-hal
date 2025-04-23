893cdcaecae2369d892036d48bb71eaf1686cd7e
Dániel Buga <bugadani@gmail.com>
2025-04-23 11:43:21 +0200

    Add test case

--- hil-test/tests/gpio_custom_handler.rs
+++ hil-test/tests/gpio_custom_handler.rs
@@ -11,14 +11,40 @@
 #![no_std]
 #![no_main]
 
+use embassy_executor::task;
+use embassy_sync::{blocking_mutex::raw::CriticalSectionRawMutex, signal::Signal};
 use embassy_time::{Duration, Timer};
 use esp_hal::{
-    gpio::{Flex, Input, InputConfig, InputPin, Io, Level, Output, OutputConfig, OutputPin, Pull},
+    gpio::{
+        AnyPin,
+        Flex,
+        Input,
+        InputConfig,
+        InputPin,
+        Io,
+        Level,
+        Output,
+        OutputConfig,
+        OutputPin,
+        Pin,
+        Pull,
+    },
     handler,
+    interrupt::{Priority, software::SoftwareInterruptControl},
     timer::timg::TimerGroup,
 };
+use esp_hal_embassy::InterruptExecutor;
 use hil_test as _;
 use portable_atomic::{AtomicUsize, Ordering};
+
+macro_rules! mk_static {
+    ($t:ty,$val:expr) => {{
+        static STATIC_CELL: static_cell::StaticCell<$t> = static_cell::StaticCell::new();
+        #[deny(unused_attributes)]
+        let x = STATIC_CELL.uninit().write(($val));
+        x
+    }};
+}
 
 #[unsafe(no_mangle)]
 unsafe extern "C" fn GPIO() {
@@ -28,10 +54,11 @@ unsafe extern "C" fn GPIO() {
 
     let (gpio1, _) = hil_test::common_test_pins!(peripherals);
 
-    // Using flex will not mutate the pin.
+    // Using flex will reinitialize the pin, but it's okay here since we access an
+    // Input.
     let mut gpio1 = Flex::new(gpio1);
 
-    gpio1.clear_interrupt();
+    gpio1.unlisten();
 }
 
 #[handler]
@@ -64,10 +91,31 @@ async fn drive_pins(gpio1: impl InputPin, gpio2: impl OutputPin) -> usize {
     counter.load(Ordering::SeqCst)
 }
 
-#[cfg(test)]
-#[embedded_test::tests(executor = hil_test::Executor::new())]
-mod tests {
+#[task]
+async fn drive_pin(gpio: AnyPin<'static>) {
+    let mut test_gpio = Output::new(gpio, Level::Low, OutputConfig::default());
+    for _ in 0..5 {
+        test_gpio.set_high();
+        Timer::after(Duration::from_millis(25)).await;
+        test_gpio.set_low();
+        Timer::after(Duration::from_millis(25)).await;
+    }
+}
 
+#[task]
+async fn sense_pin(gpio: AnyPin<'static>, done: &'static Signal<CriticalSectionRawMutex, ()>) {
+    let mut test_gpio = Input::new(gpio, InputConfig::default().with_pull(Pull::Down));
+    test_gpio.wait_for_rising_edge().await;
+    test_gpio.wait_for_rising_edge().await;
+    test_gpio.wait_for_rising_edge().await;
+    test_gpio.wait_for_rising_edge().await;
+    test_gpio.wait_for_rising_edge().await;
+    done.signal(());
+}
+
+#[cfg(test)]
+#[embedded_test::tests(executor = hil_test::Executor::new(), default_timeout = 3)]
+mod tests {
     use super::*;
 
     #[test]
@@ -109,5 +157,37 @@ mod tests {
 
         // We expect the async API to keep working even if a user handler is set.
         assert_eq!(counter, 5);
+    }
+
+    #[test]
+    async fn task_that_runs_at_handlers_priority_is_not_locked_up() {
+        let peripherals = esp_hal::init(esp_hal::Config::default());
+
+        // Register an interrupt handler. Since we are not dealing with raw interrupts
+        // here, it's okay to do nothing. Handling async GPIO events will
+        // disable the corresponding interrupts.
+        let mut io = Io::new(peripherals.IO_MUX);
+        io.set_interrupt_handler(interrupt_handler);
+
+        let (gpio1, gpio2) = hil_test::common_test_pins!(peripherals);
+
+        let timg0 = TimerGroup::new(peripherals.TIMG0);
+        esp_hal_embassy::init(timg0.timer0);
+
+        let sw_ints = SoftwareInterruptControl::new(peripherals.SW_INTERRUPT);
+        let interrupt_executor = mk_static!(
+            InterruptExecutor<1>,
+            InterruptExecutor::new(sw_ints.software_interrupt1)
+        );
+        // Run the executor at interrupt priority 1, which is the same as the default
+        // interrupt priority of the GPIO interrupt handler.
+        let interrupt_spwaner = interrupt_executor.start(Priority::Priority1);
+
+        let done = mk_static!(Signal<CriticalSectionRawMutex, ()>, Signal::new());
+
+        interrupt_spwaner.must_spawn(sense_pin(gpio1.degrade(), done));
+        interrupt_spwaner.must_spawn(drive_pin(gpio2.degrade()));
+
+        done.wait().await;
     }
 }
