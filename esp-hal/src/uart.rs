e173d9e3d3e009509c04f424a9045313e49ff4b2
Kirill Mikhailov <konnor1980@yandex.ru>
2025-04-28 16:17:08 +0200

    Add HW and SW flow control option

--- esp-hal/src/uart.rs
+++ esp-hal/src/uart.rs
@@ -219,6 +219,44 @@ pub enum StopBits {
     _2,
 }
 
+/// Software flow control settings.
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq)]
+#[cfg_attr(feature = "defmt", derive(defmt::Format))]
+#[instability::unstable]
+pub enum SwFlowControl {
+    #[default]
+    /// Disables software flow control.
+    Disabled,
+    /// Enables software flow control with configured parameters
+    Enabled {
+        /// Xon flow control char.
+        xon_char: u8,
+        /// Xoff flow control char.
+        xoff_char: u8,
+        /// If the software flow control is enabled and the data amount in rxfifo is less than xon_thrd, an xon_char will be sent.
+        xon_threshold: u8,
+        /// If the software flow control is enabled and the data amount in rxfifo is more than xoff_thrd, an xoff_char will be sent
+        xoff_threshold: u8,
+    },
+}
+
+/// Hardware flow control configuration.
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq)]
+#[cfg_attr(feature = "defmt", derive(defmt::Format))]
+#[instability::unstable]
+pub enum HwFlowControl {
+    /// Enables RTS flow control with a configurable FIFO threshold.
+    Rts(u8),
+    /// Enables CTS flow control.
+    Cts,
+    /// Enables both RTS and CTS flow control. RTS uses a configurable
+    /// threshold.
+    Full(u8),
+    #[default]
+    /// Disables hardware flow control.
+    Disabled,
+}
+
 /// Defines how strictly the requested baud rate must be met.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Default)]
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
@@ -252,6 +290,12 @@ pub struct Config {
     parity: Parity,
     /// Number of stop bits in each frame (1, 1.5, or 2 bits).
     stop_bits: StopBits,
+    /// Software flow control.
+    #[builder_lite(unstable)]
+    sw_flow_ctrl: SwFlowControl,
+    /// Hardware flow control.
+    #[builder_lite(unstable)]
+    hw_flow_ctrl: HwFlowControl,
     /// Clock source used by the UART peripheral.
     #[builder_lite(unstable)]
     clock_source: ClockSource,
@@ -271,6 +315,8 @@ impl Default for Config {
             data_bits: Default::default(),
             parity: Default::default(),
             stop_bits: Default::default(),
+            sw_flow_ctrl: Default::default(),
+            hw_flow_ctrl: Default::default(),
             clock_source: Default::default(),
         }
     }
@@ -2513,6 +2559,7 @@ impl Info {
         self.change_data_bits(config.data_bits);
         self.change_parity(config.parity);
         self.change_stop_bits(config.stop_bits);
+        self.change_flow_control(config.sw_flow_ctrl, config.hw_flow_ctrl);
 
         Ok(())
     }
@@ -2849,6 +2896,116 @@ impl Info {
         self.regs()
             .conf0()
             .modify(|_, w| unsafe { w.stop_bit_num().bits(stop_bits as u8 + 1) });
+    }
+
+    #[cfg(feature = "unstable")]
+    fn change_flow_control(&self, sw_flow_ctrl: SwFlowControl, hw_flow_ctrl: HwFlowControl) {
+        // set SW flow control
+        match sw_flow_ctrl {
+            SwFlowControl::Enabled {
+                xon_char,
+                xoff_char,
+                xon_threshold,
+                xoff_threshold,
+            } => {
+                cfg_if::cfg_if! {
+                    if #[cfg(any(esp32c6, esp32h2))] {
+                        self.regs().swfc_conf0().modify(|_, w| w.xonoff_del().set_bit());
+                        self.regs().swfc_conf0().modify(|_, w| w.sw_flow_con_en().set_bit());
+                        self.regs().swfc_conf1().modify(|_, w| unsafe { w.xon_threshold().bits(xon_threshold) });
+                        self.regs().swfc_conf1().modify(|_, w| unsafe { w.xoff_threshold().bits(xoff_threshold) });
+                        self.regs().swfc_conf0().modify(|_, w| unsafe { w.xon_char().bits(xon_char) });
+                        self.regs().swfc_conf0().modify(|_, w| unsafe { w.xoff_char().bits(xoff_char) });
+                    } else if #[cfg(esp32)]{
+                        self.regs().flow_conf().modify(|_, w| w.xonoff_del().set_bit());
+                        self.regs().flow_conf().modify(|_, w| unsafe { w.sw_flow_con_en().set_bit() });
+                        self.regs().swfc_conf().modify(|_, w| unsafe { w.xon_threshold().bits(xon_threshold) });
+                        self.regs().swfc_conf().modify(|_, w| unsafe { w.xoff_threshold().bits(xoff_threshold) });
+                        self.regs().swfc_conf().modify(|_, w| unsafe { w.xon_char().bits(xon_char) });
+                        self.regs().swfc_conf().modify(|_, w| unsafe { w.xoff_char().bits(xoff_char) });
+                    } else {
+                        self.regs().flow_conf().modify(|_, w| w.xonoff_del().set_bit());
+                        self.regs().flow_conf().modify(|_, w| unsafe { w.sw_flow_con_en().set_bit() });
+                        self.regs().swfc_conf1().modify(|_, w| unsafe { w.xon_threshold().bits(xon_threshold) });
+                        self.regs().swfc_conf0().modify(|_, w| unsafe { w.xoff_threshold().bits(xoff_threshold) });
+                        self.regs().swfc_conf1().modify(|_, w| unsafe { w.xon_char().bits(xon_char) });
+                        self.regs().swfc_conf0().modify(|_, w| unsafe { w.xoff_char().bits(xoff_char) });
+                    }
+                }
+                #[cfg(any(esp32c6, esp32h2))]
+                sync_regs(self.regs());
+            }
+            SwFlowControl::Disabled => {
+                cfg_if::cfg_if! {
+                    if #[cfg(any(esp32c6, esp32h2))] {
+                        let reg = self.regs().swfc_conf0();
+                    } else {
+                        let reg = self.regs().flow_conf();
+                    }
+                }
+
+                reg.modify(|_, w| w.sw_flow_con_en().clear_bit() );
+                reg.modify(|_, w| w.xonoff_del().clear_bit() );
+
+                #[cfg(any(esp32c6, esp32h2))]
+                sync_regs(self.regs());
+            }
+        }
+
+        // set HW flow control
+        match hw_flow_ctrl {
+            HwFlowControl::Disabled => {
+                self.set_rts(false, None);
+                self.set_cts(false);
+            }
+        
+            HwFlowControl::Cts => {
+                self.set_rts(false, None);
+                self.set_cts(true);
+            }
+        
+            HwFlowControl::Rts(threshold) => {
+                self.set_rts(true, Some(threshold));
+                self.set_cts(false);
+            }
+        
+            HwFlowControl::Full(rts_threshold) => {
+                self.set_rts(true, Some(rts_threshold));
+                self.set_cts(true);
+            }
+        }
+    }
+
+    fn set_cts(&self, enable: bool) {
+        self.regs().conf0().modify(|_, w| {
+            w.tx_flow_en().bit(enable)
+        });
+    }
+    
+    fn set_rts(&self, enable: bool, threshold: Option<u8>) {
+        if let Some(threshold) = threshold {
+            cfg_if::cfg_if! {
+                if #[cfg(esp32)] {
+                    self.regs().conf1().modify(|_, w| unsafe { w.rx_flow_thrhd().bits(threshold) });
+                } else if #[cfg(any(esp32c6, esp32h2))] {
+                    self.regs().hwfc_conf().modify(|_, w| unsafe { w.rx_flow_thrhd().bits(threshold) });
+                } else {
+                    self.regs().mem_conf().modify(|_, w| unsafe { w.rx_flow_thrhd().bits(threshold) });
+                }
+            }
+        }
+    
+        cfg_if::cfg_if! {
+            if #[cfg(any(esp32c6, esp32h2))] {
+                self.regs().hwfc_conf().modify(|_, w| {
+                    w.rx_flow_en().bit(enable)
+                });
+            } else {
+                self.regs().conf1().modify(|_, w| {
+                    w.rx_flow_en().bit(enable)
+                });
+            }
+        }
     }
 
     fn rxfifo_reset(&self) {
