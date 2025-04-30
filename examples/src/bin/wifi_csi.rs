a641eb0fb69a431f427f08792014fdddc40c8dd7
Dániel Buga <bugadani@gmail.com>
2025-04-30 19:16:54 +0200

    Fix examples

--- examples/src/bin/wifi_csi.rs
+++ examples/src/bin/wifi_csi.rs
@@ -4,7 +4,7 @@
 //! Set SSID and PASSWORD env variable before running this example.
 //!
 
-//% FEATURES: esp-wifi esp-wifi/wifi esp-wifi/smoltcp esp-wifi/log esp-wifi/csi esp-hal/unstable
+//% FEATURES: esp-wifi esp-wifi/wifi esp-wifi/smoltcp esp-wifi/log-04 esp-wifi/csi esp-hal/unstable
 //% CHIPS: esp32 esp32s2 esp32s3 esp32c2 esp32c3 esp32c6
 
 #![no_std]
