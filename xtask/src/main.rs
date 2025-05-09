ac4f4cdedec2d1ba345dc36e9ecaeee15689ac1e
Dániel Buga <bugadani@gmail.com>
2025-05-09 14:31:57 +0200

    Document bump-version better

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -28,6 +28,16 @@ enum Cli {
     Run(Run),
 
     /// Bump the version of the specified package(s).
+    ///
+    /// This command will, for each specified package:
+    /// - Verify that the crate can be released (e.g. it doesn't refer to git
+    ///   dependencies)
+    /// - Update the version in `Cargo.toml` files
+    /// - Update the version in dependencies' `Cargo.toml` files
+    /// - Check if the changelog can be finalized
+    /// - Update the version in the changelog
+    /// - Replaces `{{currentVersion}}` markers in source files and the
+    ///   migration guide.
     BumpVersion(BumpVersionArgs),
     /// Perform (parts of) the checks done in CI
     Ci(CiArgs),
