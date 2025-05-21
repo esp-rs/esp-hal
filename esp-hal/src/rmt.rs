59bafaf2b0e641dd9059675857e146cf5c6c62a3
wisp3rwind <17089248+wisp3rwind@users.noreply.github.com>
2025-05-21 08:48:16 +0200

    RMT: remove an unnecessary Iterator.take() that was a no-op

--- esp-hal/src/rmt.rs
+++ esp-hal/src/rmt.rs
@@ -1365,9 +1365,9 @@ impl<Raw: RxChannelInternal> RxTransaction<'_, Raw> {
         raw.update();
 
         let ptr = raw.channel_ram_start();
-        // FIXME: Remove the .take(), it's a no-op (why is it here??)
-        let len = self.data.len();
-        for (idx, entry) in self.data.iter_mut().take(len).enumerate() {
+        // SAFETY: RxChannel.receive() verifies that the length of self.data does not
+        // exceed the channel RAM size.
+        for (idx, entry) in self.data.iter_mut().enumerate() {
             *entry = unsafe { ptr.add(idx).read_volatile() };
         }
 
