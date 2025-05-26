0f4b29d49d5249f13a5237b2ff58bf9de184ec6b
Björn Quentin <bjoernQ@users.noreply.github.com>
2025-05-26 13:29:36 +0200

    No need to ref-count wifi anymore (#3550)
    * No need to ref-count wifi anymore
    
    * CHANGELOG.md

--- esp-wifi/src/lib.rs
+++ esp-wifi/src/lib.rs
@@ -251,10 +251,10 @@ const _: () = {
 type TimeBase = PeriodicTimer<'static, Blocking>;
 
 pub(crate) mod flags {
-    use portable_atomic::{AtomicBool, AtomicUsize};
+    use portable_atomic::AtomicBool;
 
     pub(crate) static ESP_WIFI_INITIALIZED: AtomicBool = AtomicBool::new(false);
-    pub(crate) static WIFI: AtomicUsize = AtomicUsize::new(0);
+    pub(crate) static WIFI: AtomicBool = AtomicBool::new(false);
     pub(crate) static BLE: AtomicBool = AtomicBool::new(false);
 }
 
@@ -267,7 +267,7 @@ pub struct EspWifiController<'d> {
 impl EspWifiController<'_> {
     /// Is the WiFi part of the radio running
     pub fn wifi(&self) -> bool {
-        crate::flags::WIFI.load(Ordering::Acquire) > 0
+        crate::flags::WIFI.load(Ordering::Acquire)
     }
 
     /// Is the BLE part of the radio running
@@ -449,7 +449,7 @@ pub unsafe fn deinit_unchecked() -> Result<(), InitializationError> {
     if controller.wifi() {
         #[cfg(feature = "wifi")]
         crate::wifi::wifi_deinit()?;
-        crate::flags::WIFI.store(0, Ordering::Release);
+        crate::flags::WIFI.store(false, Ordering::Release);
     }
 
     if controller.ble() {
