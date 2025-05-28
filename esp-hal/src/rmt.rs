6730bfbb6194304878ce12648fe7b86c0b18a842
wisp3rwind <17089248+wisp3rwind@users.noreply.github.com>
2025-05-28 12:11:04 +0200

    RMT: add a few #[inline] attributes
    this wasn't always inlined, but it probably makes sense to always do so
    since the arguments are (almost) always compile time constants

--- esp-hal/src/rmt.rs
+++ esp-hal/src/rmt.rs
@@ -2699,6 +2699,8 @@ mod chip_specific {
             self.update();
         }
 
+        // Ensure that this is always inlined in (un)listen_tx_interrupt
+        #[inline]
         fn set_tx_interrupt(&self, events: EnumSet<Event>, enable: bool) {
             let rmt = crate::peripherals::RMT::regs();
             rmt.int_ena().modify(|_, w| {
@@ -2854,6 +2856,8 @@ mod chip_specific {
                 .modify(|_, w| unsafe { w.idle_thres().bits(value) });
         }
 
+        // Ensure that this is always inlined in (un)listen_rx_interrupt
+        #[inline]
         fn set_rx_interrupt(&self, events: EnumSet<Event>, enable: bool) {
             let rmt = crate::peripherals::RMT::regs();
             let ch_idx = ch_idx(self);
@@ -3126,6 +3130,8 @@ mod chip_specific {
             }
         }
 
+        // Ensure that this is always inlined in (un)listen_tx_interrupt
+        #[inline]
         fn set_tx_interrupt(&self, events: EnumSet<Event>, enable: bool) {
             let rmt = crate::peripherals::RMT::regs();
             let ch = self.channel();
@@ -3251,6 +3257,8 @@ mod chip_specific {
                 .modify(|_, w| unsafe { w.idle_thres().bits(value) });
         }
 
+        // Ensure that this is always inlined in (un)listen_rx_interrupt
+        #[inline]
         fn set_rx_interrupt(&self, events: EnumSet<Event>, enable: bool) {
             let rmt = crate::peripherals::RMT::regs();
             let ch = self.channel();
