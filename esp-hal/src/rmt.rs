2afb8e4277de9632389245acfc2bccfae2bde1b7
wisp3rwind <17089248+wisp3rwind@users.noreply.github.com>
2025-05-28 12:56:41 +0200

    silence clippy for now

--- esp-hal/src/rmt.rs
+++ esp-hal/src/rmt.rs
@@ -1722,6 +1722,7 @@ where
             return Err(Error::InvalidArgument);
         }
 
+        #[allow(clippy::single_match)]
         match writer.state {
             WriterState::DoneNoEnd => return Err(Error::EndMarkerMissing),
             // WriterState::DoneEarly => return Err(Error::UnexpectedEndMarker),
