94bacc589b4024fb281c1e66f7318816ba8f44d0
wisp3rwind <17089248+wisp3rwind@users.noreply.github.com>
2025-05-28 23:54:48 +0200

    RMT: track STATE in blocking rx
    not really necessary, but that's one difference between blocking/async
    rx: I'm making them more similar here in the hope to figure out why the
    one works while the other fails in loopback tests

--- esp-hal/src/rmt.rs
+++ esp-hal/src/rmt.rs
@@ -1501,6 +1501,9 @@ enum RmtState {
     // The channel is configured for rx and currently performing an async transaction
     RxAsync      = RX_STATE_BASE + 1,
 
+    // The channel is configured for rx and currently performing a blocking transaction
+    RxBlocking   = RX_STATE_BASE + 2,
+
     // The channel is configured for tx and currently performing an async transaction
     TxAsync      = TX_STATE_BASE + 1,
 }
@@ -1509,7 +1512,7 @@ impl RmtState {
     #[allow(unused)]
     fn is_tx(&self) -> Option<bool> {
         match self {
-            Self::RxIdle | Self::RxAsync => Some(false),
+            Self::RxIdle | Self::RxAsync | Self::RxBlocking => Some(false),
             Self::TxIdle | Self::TxAsync => Some(true),
             _ => None,
         }
@@ -1999,7 +2002,10 @@ where
             }
         };
 
-        self.channel.raw().clear_rx_interrupts();
+        let raw = self.channel.raw();
+
+        raw.clear_rx_interrupts();
+        STATE[raw.channel() as usize].store(RmtState::RxIdle as u8, Ordering::Relaxed);
 
         // Disable Drop handler since the receiver is stopped already.
         let _ = ManuallyDrop::new(self);
@@ -2020,10 +2026,17 @@ where
         // safe (i.e. start from a state where the hardware is stopped).
         let raw = self.channel.raw();
 
-        raw.stop_rx();
-        raw.update();
+        // STATE should be RxIdle if the transaction was polled to completion
+        if STATE[raw.channel() as usize].load(Ordering::Relaxed) == RmtState::RxAsync as u8 {
+            raw.stop_rx();
+            raw.update();
 
-        while !matches!(raw.get_rx_status(), Some(Event::Error | Event::End)) {}
+            // block until the channel is safe to use again
+            while !matches!(raw.get_rx_status(), Some(Event::Error | Event::End)) {}
+
+            raw.clear_rx_interrupts();
+            STATE[raw.channel() as usize].store(RmtState::RxIdle as u8, Ordering::Relaxed);
+        }
     }
 }
 
@@ -2075,6 +2088,8 @@ where
 
         let data = data.into_iter();
         let reader = RmtReader::new();
+
+        STATE[raw.channel() as usize].store(RmtState::RxBlocking as u8, Ordering::Relaxed);
 
         raw.clear_rx_interrupts();
         raw.start_receive(true);
