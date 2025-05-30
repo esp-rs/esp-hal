33dbbc1caa2df8f5ede2b7fa690f4e709d75b842
Dániel Buga <bugadani@gmail.com>
2025-05-30 13:09:59 +0200

    Reserve and use FROM_CPU_INTR2 on RISC-V

--- esp-wifi/src/preempt_builtin/timer/riscv.rs
+++ esp-wifi/src/preempt_builtin/timer/riscv.rs
@@ -22,7 +22,7 @@ pub(crate) fn setup_timer(timer: TimeBase) {
 
 pub(crate) fn setup_multitasking() {
     unwrap!(interrupt::enable(
-        Interrupt::FROM_CPU_INTR3,
+        Interrupt::FROM_CPU_INTR2,
         interrupt::Priority::Priority1,
     ));
 
@@ -32,21 +32,21 @@ pub(crate) fn setup_multitasking() {
 }
 
 pub(crate) fn disable_multitasking() {
-    interrupt::disable(crate::hal::system::Cpu::ProCpu, Interrupt::FROM_CPU_INTR3);
+    interrupt::disable(crate::hal::system::Cpu::ProCpu, Interrupt::FROM_CPU_INTR2);
 }
 
 #[unsafe(no_mangle)]
-extern "C" fn FROM_CPU_INTR3(trap_frame: &mut TrapFrame) {
+extern "C" fn FROM_CPU_INTR2(trap_frame: &mut TrapFrame) {
     // clear FROM_CPU_INTR3
     SystemPeripheral::regs()
-        .cpu_intr_from_cpu_3()
-        .modify(|_, w| w.cpu_intr_from_cpu_3().clear_bit());
+        .cpu_intr_from_cpu_2()
+        .modify(|_, w| w.cpu_intr_from_cpu_2().clear_bit());
 
     task_switch(trap_frame);
 }
 
 pub(crate) fn yield_task() {
     SystemPeripheral::regs()
-        .cpu_intr_from_cpu_3()
-        .modify(|_, w| w.cpu_intr_from_cpu_3().set_bit());
+        .cpu_intr_from_cpu_2()
+        .modify(|_, w| w.cpu_intr_from_cpu_2().set_bit());
 }
