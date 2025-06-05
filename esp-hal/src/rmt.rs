5326f7b7e14639b041c5f38d50bb6e83627532ad
wisp3rwind <17089248+wisp3rwind@users.noreply.github.com>
2025-06-05 23:00:34 +0200

    prevent some inlining that blows up code size a lot

--- esp-hal/src/rmt.rs
+++ esp-hal/src/rmt.rs
@@ -552,6 +552,8 @@ impl RmtWriter {
 
     // TODO: better check that hw ptr matches expectation when done! that also helps
     // with underruns
+    // FIXME: degrade, then no inline
+    #[inline(never)]
     fn write(
         &mut self,
         data: &mut impl Iterator<Item: Borrow<PulseCode>>,
@@ -2556,6 +2558,11 @@ where
     }
 }
 
+#[inline(never)]
+fn wake(ch_num: u8) {
+    WAKER[ch_num as usize].wake();
+}
+
 // #[cfg(any(esp32, esp32s2))]
 #[handler]
 fn async_interrupt_handler() {
@@ -2567,7 +2574,7 @@ fn async_interrupt_handler() {
         };
         raw.unlisten_tx_interrupt(events_to_unlisten);
 
-        WAKER[raw.channel() as usize].wake();
+        wake(raw.channel());
     }
 
     fn on_rx(raw: &impl RawChannelAccess<Dir = Rx>, event: Event) {
@@ -2578,7 +2585,7 @@ fn async_interrupt_handler() {
         };
         raw.unlisten_rx_interrupt(events_to_unlisten);
 
-        WAKER[raw.channel() as usize].wake();
+        wake(raw.channel());
     }
 
     chip_specific::handle_channel_interrupts(on_tx, on_rx);
