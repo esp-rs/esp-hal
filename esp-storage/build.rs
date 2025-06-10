7c322ec31c74512f4ac3d71fb97289e914d5e26f
Juraj Sadel <juraj.sadel@espressif.com>
2025-06-10 11:31:25 +0200

    esp-wifi and esp-storage can be used with opt-level=s (#3618)

--- esp-storage/build.rs
+++ esp-storage/build.rs
@@ -7,9 +7,9 @@ fn main() -> Result<(), String> {
     if cfg!(feature = "esp32") {
         match std::env::var("OPT_LEVEL") {
             Ok(level) if std::env::var("CI").is_err() => {
-                if level != "2" && level != "3" {
+                if level != "2" && level != "3" && level != "s" {
                     Err(format!(
-                        "Building esp-storage for ESP32 needs optimization level 2 or 3 - yours is {level}. See https://github.com/esp-rs/esp-storage"
+                        "Building esp-storage for ESP32 needs optimization level 2, 3 or s - yours is {level}. See https://github.com/esp-rs/esp-storage"
                     ))
                 } else {
                     Ok(())
