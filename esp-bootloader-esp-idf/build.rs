23dc729c62ace8376c5da6671ec419383e9300f1
Björn Quentin <bjoernQ@users.noreply.github.com>
2025-06-10 12:10:33 +0200

    Typo

--- esp-bootloader-esp-idf/build.rs
+++ esp-bootloader-esp-idf/build.rs
@@ -18,6 +18,6 @@ fn main() {
     // emit config
     println!("cargo:rerun-if-changed=./esp_config.yml");
     let cfg_yaml = std::fs::read_to_string("./esp_config.yml")
-        .expect("Failed to read esp_config.yml fro esp-bootloader-esp-idf");
+        .expect("Failed to read esp_config.yml for esp-bootloader-esp-idf");
     generate_config_from_yaml_definition(&cfg_yaml, true, true, None).unwrap();
 }
