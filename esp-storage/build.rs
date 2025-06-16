0403ef278f280fd2ed42857a5f7a2debb3911974
Björn Quentin <bjoernQ@users.noreply.github.com>
2025-06-16 14:43:14 +0200

    Use esp-idf rom-function patches in esp-storage for esp32

--- esp-storage/build.rs
+++ esp-storage/build.rs
@@ -5,6 +5,12 @@ fn main() -> Result<(), String> {
     }
 
     if cfg!(feature = "esp32") {
+        // for now only use `libesp_rom.a` for the original ESP32
+        let out = std::path::PathBuf::from(std::env::var_os("OUT_DIR").unwrap());
+        println!("cargo:rustc-link-search={}", out.display());
+        std::fs::copy("./libs/esp32/libesp_rom.a", out.join("libesp_rom.a")).unwrap();
+        println!("cargo:rustc-link-lib={}", "esp_rom");
+
         match std::env::var("OPT_LEVEL") {
             Ok(level) if std::env::var("CI").is_err() => {
                 if level != "2" && level != "3" && level != "s" {
