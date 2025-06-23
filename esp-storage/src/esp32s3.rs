cec87b30fe100029ef4c5e27013a6639607d7047
Björn Quentin <bjoernQ@users.noreply.github.com>
2025-06-23 17:41:14 +0200

    Don't use link_section on MacOS

--- esp-storage/src/esp32s3.rs
+++ esp-storage/src/esp32s3.rs
@@ -8,25 +8,25 @@ crate::rom_fn! {
 }
 
 #[inline(always)]
-#[unsafe(link_section = ".rwtext")]
+#[cfg_attr(not(not(target_os = "macos")), unsafe(link_section = ".rwtext"))]
 pub(crate) fn spiflash_read(src_addr: u32, data: *const u32, len: u32) -> i32 {
     maybe_with_critical_section(|| esp_rom_spiflash_read(src_addr, data, len))
 }
 
 #[inline(always)]
-#[unsafe(link_section = ".rwtext")]
+#[cfg_attr(not(not(target_os = "macos")), unsafe(link_section = ".rwtext"))]
 pub(crate) fn spiflash_unlock() -> i32 {
     maybe_with_critical_section(esp_rom_spiflash_unlock)
 }
 
 #[inline(always)]
-#[unsafe(link_section = ".rwtext")]
+#[cfg_attr(not(not(target_os = "macos")), unsafe(link_section = ".rwtext"))]
 pub(crate) fn spiflash_erase_sector(sector_number: u32) -> i32 {
     maybe_with_critical_section(|| esp_rom_spiflash_erase_sector(sector_number))
 }
 
 #[inline(always)]
-#[unsafe(link_section = ".rwtext")]
+#[cfg_attr(not(not(target_os = "macos")), unsafe(link_section = ".rwtext"))]
 pub(crate) fn spiflash_write(dest_addr: u32, data: *const u32, len: u32) -> i32 {
     maybe_with_critical_section(|| esp_rom_spiflash_write(dest_addr, data, len))
 }
