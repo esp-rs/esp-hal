7a221cecf6419b214d1e661712d2427a9e8d6c2e
rmsyn <github@weathered-steel.dev>
2025-06-26 22:02:33 +0000

    sdio: add set timing method
    Adds the `Timing` struct to represent SDIO timing settings.
    
    Adds a method to set low-level timing configuration for the SDIO
    peripheral.

--- esp-hal/src/sdio/config.rs
+++ esp-hal/src/sdio/config.rs
@@ -1,15 +1,50 @@
+use super::Timing;
+
 /// Represents SDIO configuration parameters.
 #[repr(C)]
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub struct Config {
-    /// Indicates support for high-speed mode.
-    pub hs: bool,
+    hs: bool,
+    timing: Timing,
 }
 
 impl Config {
     /// Creates a new [Config].
     pub const fn new() -> Self {
-        Self { hs: false }
+        Self {
+            hs: false,
+            timing: Timing::new(),
+        }
+    }
+
+    /// Gets the highspeed enable setting.
+    pub const fn hs(&self) -> bool {
+        self.hs
+    }
+
+    /// Sets the highspeed enable setting.
+    pub fn set_hs(&mut self, hs: bool) {
+        self.hs = hs;
+    }
+
+    /// Builder funciton that sets the highspeed enable setting.
+    pub fn with_hs(self, hs: bool) -> Self {
+        Self { hs, ..self }
+    }
+
+    /// Gets the timing setting.
+    pub const fn timing(&self) -> Timing {
+        self.timing
+    }
+
+    /// Sets the timing setting.
+    pub fn set_timing(&mut self, timing: Timing) {
+        self.timing = timing;
+    }
+
+    /// Builder funciton that sets the timing setting.
+    pub fn with_timing(self, timing: Timing) -> Self {
+        Self { timing, ..self }
     }
 }
 
