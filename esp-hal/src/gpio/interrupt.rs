29b696f370dfcaccd8e70c2b74e31e9b49c4301a
Scott Mabin <scott@mabez.dev>
2025-06-26 16:46:28 +0100

    clean up warnings

--- esp-hal/src/gpio/interrupt.rs
+++ esp-hal/src/gpio/interrupt.rs
@@ -65,13 +65,10 @@ use crate::{
     gpio::{AnyPin, GpioBank, InputPin, set_int_enable},
     interrupt::{self, DEFAULT_INTERRUPT_HANDLER, Priority},
     peripherals::{GPIO, Interrupt},
-    sync::RawMutex,
 };
 
 /// Convenience constant for `Option::None` pin
 pub(super) static USER_INTERRUPT_HANDLER: CFnPtr = CFnPtr::new();
-
-pub(super) static GPIO_LOCK: RawMutex = RawMutex::new();
 
 pub(super) struct CFnPtr(AtomicPtr<()>);
 impl CFnPtr {
