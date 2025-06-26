fc4c756a574307796ea41950d3cb7c44c8d6930f
rmsyn <github@weathered-steel.dev>
2025-06-26 22:43:09 +0000

    sdio: change interrupt types to bitfields
    Changes the SDIO interrupt types to be bitfield structs.
    
    Adds the `DeviceInterrupt` type to represents interrupts sent from the
    host to the device.

--- esp-hal/src/sdio/interrupt.rs
+++ esp-hal/src/sdio/interrupt.rs
@@ -1,97 +1,114 @@
-use super::Error;
+use bitfield::bitfield;
 
-/// Represents an interrupt to send to the host.
-///
-/// # Note
-///
-/// Values derived from [esp-idf](https://github.com/espressif/esp-idf/blob/v5.4.1/components/hal/include/hal/sdio_slave_types.h) SDIO driver.
-#[repr(u32)]
-#[derive(Clone, Copy, Debug, Eq, PartialEq)]
-pub enum HostInterrupt {
+bitfield! {
+    /// Represents an interrupt to send to the host.
+    ///
+    /// # Note
+    ///
+    /// Values derived from [esp-idf](https://github.com/espressif/esp-idf/blob/v5.4.1/components/hal/include/hal/sdio_slave_types.h) SDIO driver.
+    #[repr(C)]
+    #[derive(Clone, Copy, Debug, Eq, PartialEq)]
+    pub struct HostInterrupt(u8);
     /// General purpose host interrupt: 0.
-    General0      = 1 << 0,
+    pub general0, set_general0: 0;
     /// General purpose host interrupt: 1.
-    General1      = 1 << 1,
+    pub general1, set_general1: 1;
     /// General purpose host interrupt: 2.
-    General2      = 1 << 2,
+    pub general2, set_general2: 2;
     /// General purpose host interrupt: 3.
-    General3      = 1 << 3,
+    pub general3, set_general3: 3;
     /// General purpose host interrupt: 4.
-    General4      = 1 << 4,
+    pub general4, set_general4: 4;
     /// General purpose host interrupt: 5.
-    General5      = 1 << 5,
+    pub general5, set_general5: 5;
     /// General purpose host interrupt: 6.
-    General6      = 1 << 6,
+    pub general6, set_general6: 6;
     /// General purpose host interrupt: 7.
-    General7      = 1 << 7,
-    /// New packet available to send to host.
-    SendNewPacket = 1 << 23,
+    pub general7, set_general7: 7;
 }
 
 impl HostInterrupt {
-    /// Bit value for general purpose host interrupt: 0.
-    pub const GENERAL_0: u32 = 1 << 0;
-    /// Bit value for general purpose host interrupt: 1.
-    pub const GENERAL_1: u32 = 1 << 1;
-    /// Bit value for general purpose host interrupt: 2.
-    pub const GENERAL_2: u32 = 1 << 2;
-    /// Bit value for general purpose host interrupt: 3.
-    pub const GENERAL_3: u32 = 1 << 3;
-    /// Bit value for general purpose host interrupt: 4.
-    pub const GENERAL_4: u32 = 1 << 4;
-    /// Bit value for general purpose host interrupt: 5.
-    pub const GENERAL_5: u32 = 1 << 5;
-    /// Bit value for general purpose host interrupt: 6.
-    pub const GENERAL_6: u32 = 1 << 6;
-    /// Bit value for general purpose host interrupt: 7.
-    pub const GENERAL_7: u32 = 1 << 7;
-    /// Bit value for new packet available to send to host.
-    pub const SEND_NEW_PACKET: u32 = 1 << 23;
-
     /// Creates a new [HostInterrupt].
     pub const fn new() -> Self {
-        Self::General0
+        Self(0)
     }
 
-    /// Converts the [HostInterrupt] into a [`u32`].
-    #[inline]
-    pub const fn to_u32(self) -> u32 {
-        self as u32
-    }
-
-    /// Attempts to convert a [`u32`] into a [HostInterrupt].
-    #[inline]
-    pub const fn try_from_u32(val: u32) -> Result<Self, Error> {
-        match val {
-            Self::GENERAL_0 => Ok(Self::General0),
-            Self::GENERAL_1 => Ok(Self::General1),
-            Self::GENERAL_2 => Ok(Self::General2),
-            Self::GENERAL_3 => Ok(Self::General3),
-            Self::GENERAL_4 => Ok(Self::General4),
-            Self::GENERAL_5 => Ok(Self::General5),
-            Self::GENERAL_6 => Ok(Self::General6),
-            Self::GENERAL_7 => Ok(Self::General7),
-            Self::SEND_NEW_PACKET => Ok(Self::SendNewPacket),
-            _ => Err(Error::General),
-        }
+    /// Gets the raw bit value of the [HostInterrupt].
+    pub const fn bits(&self) -> u8 {
+        self.0
     }
 }
 
-impl From<HostInterrupt> for u32 {
+impl From<HostInterrupt> for u8 {
     fn from(val: HostInterrupt) -> Self {
-        val.to_u32()
+        val.bits()
     }
 }
 
-impl TryFrom<u32> for HostInterrupt {
-    type Error = Error;
-
-    fn try_from(val: u32) -> Result<Self, Self::Error> {
-        Self::try_from_u32(val)
+impl From<u8> for HostInterrupt {
+    fn from(val: u8) -> Self {
+        Self(val)
     }
 }
 
 impl Default for HostInterrupt {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+bitfield! {
+    /// Represents an interrupt to sent from the host.
+    ///
+    /// # Note
+    ///
+    /// Values derived from [esp-idf](https://github.com/espressif/esp-idf/blob/v5.4.1/components/hal/esp32/include/hal/sdio_slave_ll.h) SDIO driver.
+    #[repr(C)]
+    #[derive(Clone, Copy, Debug, Eq, PartialEq)]
+    pub struct DeviceInterrupt(u8);
+    /// General purpose host interrupt: 0.
+    pub general0, set_general0: 0;
+    /// General purpose host interrupt: 1.
+    pub general1, set_general1: 1;
+    /// General purpose host interrupt: 2.
+    pub general2, set_general2: 2;
+    /// General purpose host interrupt: 3.
+    pub general3, set_general3: 3;
+    /// General purpose host interrupt: 4.
+    pub general4, set_general4: 4;
+    /// General purpose host interrupt: 5.
+    pub general5, set_general5: 5;
+    /// General purpose host interrupt: 6.
+    pub general6, set_general6: 6;
+    /// General purpose host interrupt: 7.
+    pub general7, set_general7: 7;
+}
+
+impl DeviceInterrupt {
+    /// Creates a new [DeviceInterrupt].
+    pub const fn new() -> Self {
+        Self(0)
+    }
+
+    /// Gets the raw bit value of the [DeviceInterrupt].
+    pub const fn bits(&self) -> u8 {
+        self.0
+    }
+}
+
+impl From<DeviceInterrupt> for u8 {
+    fn from(val: DeviceInterrupt) -> Self {
+        val.bits()
+    }
+}
+
+impl From<u8> for DeviceInterrupt {
+    fn from(val: u8) -> Self {
+        Self(val)
+    }
+}
+
+impl Default for DeviceInterrupt {
     fn default() -> Self {
         Self::new()
     }
