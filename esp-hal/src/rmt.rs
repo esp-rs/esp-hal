ecdf8adf186ff7766cae0e9e5f476bdc0a0eb3c9
wisp3rwind <17089248+wisp3rwind@users.noreply.github.com>
2025-06-29 13:04:17 +0200

    RMT: mark SingleShotTxTransaction as must_use

--- esp-hal/src/rmt.rs
+++ esp-hal/src/rmt.rs
@@ -1195,6 +1195,7 @@ where
 /// If the data size exceeds the size of the internal buffer, `.poll()` or
 /// `.wait()` needs to be called before the entire buffer has been sent to avoid
 /// underruns.
+#[must_use = "transactions need to be `poll()`ed / `wait()`ed for to ensure progress"]
 pub struct SingleShotTxTransaction<'a, T>
 where
     T: Into<PulseCode> + Copy,
