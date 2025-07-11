0d8ef6a8e0ad2abb09123b1071d8223c9e6692e6
Dániel Buga <bugadani@gmail.com>
2025-07-11 13:55:54 +0200

    Fix, simplify and ignore PSRAM doc example

--- esp-hal/src/soc/esp32s3/psram.rs
+++ esp-hal/src/soc/esp32s3/psram.rs
@@ -24,28 +24,13 @@
 //!
 //! Notice that PSRAM example **must** be built in release mode!
 //!
-//! ```rust, no_run
+//! ```rust, ignore
 //! # {before_snippet}
 //! # extern crate alloc;
 //! # use alloc::{string::String, vec::Vec};
-//! # use esp_alloc as _;
-//! # use esp_hal::psram;
-//!
-//! // Initialize PSRAM and add it as a heap memory region
-//! fn init_psram_heap(start: *mut u8, size: usize) {
-//!     unsafe {
-//!         esp_alloc::HEAP.add_region(esp_alloc::HeapRegion::new(
-//!             start,
-//!             size,
-//!             esp_alloc::MemoryCapability::External.into(),
-//!         ));
-//!     }
-//! }
-//!
-//! // Initialize PSRAM and add it to the heap
-//! let (start, size) = psram::init_psram(peripherals.PSRAM, psram::PsramConfig::default());
-//!
-//! init_psram_heap(start, size);
+//! #
+//! // Add PSRAM to the heap.
+//! esp_alloc::psram_allocator!(&peripherals.PSRAM, esp_hal::psram);
 //!
 //! let mut large_vec: Vec<u32> = Vec::with_capacity(500 * 1024 / 4);
 //!
@@ -54,7 +39,7 @@
 //! }
 //!
 //! let string = String::from("A string allocated in PSRAM");
-//! # }
+//! # {after_snippet}
 //! ```
 
 use crate::peripherals::{EXTMEM, IO_MUX, SPI0, SPI1};
