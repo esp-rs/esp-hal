e0e7a88e76ec2691196e13c50566ed3e4ffa5637
Kirill Mikhailov <konnor1980@yandex.ru>
2025-07-18 16:18:11 +0200

    oopsy

--- esp-backtrace/src/lib.rs
+++ esp-backtrace/src/lib.rs
@@ -3,12 +3,10 @@
 //! This is a lightweight crate for obtaining backtraces during panics, exceptions, and hard faults
 //! on Espressif devices. It provides optional panic and exception handlers and supports a range of
 //! output options, all configurable through feature flags.
-//!
 #![cfg_attr(
     target_arch = "riscv32",
     doc = "Please note that you **need** to force frame pointers (i.e. `\"-C\", \"force-frame-pointers\",` in your `.cargo/config.toml`)"
 )]
-//!
 //! You can get an array of backtrace addresses (limited to 10 entries by default) via
 //! `arch::backtrace()` if you want to create a backtrace yourself (i.e. not using the panic or
 //! exception handler).
