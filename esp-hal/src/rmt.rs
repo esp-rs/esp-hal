276de132595f05542574e6889db3d2ed42a7a0d6
wisp3rwind <17089248+wisp3rwind@users.noreply.github.com>
2025-08-07 19:28:38 +0200

    RMT: add minimal RmtReader
    - de-duplicates the copy-from-hardware code
    - this also paves the way for supporting other data types (like
      iterators instead of slices)

--- esp-hal/src/rmt.rs
+++ esp-hal/src/rmt.rs
@@ -225,6 +225,8 @@ use crate::{
     time::Rate,
 };
 
+mod reader;
+use reader::RmtReader;
 mod writer;
 use writer::RmtWriter;
 
@@ -1418,6 +1420,9 @@ where
     T: From<PulseCode>,
 {
     channel: Channel<Blocking, Rx>,
+
+    reader: RmtReader,
+
     data: &'a mut [T],
 }
 
@@ -1436,12 +1441,7 @@ where
             raw.stop_rx();
             raw.update();
 
-            let ptr = raw.channel_ram_start();
-            // SAFETY: RxChannel.receive() verifies that the length of self.data does not
-            // exceed the channel RAM size.
-            for (idx, entry) in self.data.iter_mut().enumerate() {
-                *entry = unsafe { ptr.add(idx).read_volatile() }.into();
-            }
+            self.reader.read(&mut self.data, raw, true);
         }
 
         status
@@ -1490,14 +1490,20 @@ impl Channel<Blocking, Rx> {
         Self: Sized,
         T: From<PulseCode>,
     {
+        let raw = self.raw;
+        let memsize = raw.memsize();
+
         if data.len() > self.raw.memsize().codes() {
             return Err(Error::InvalidDataLength);
         }
 
-        self.raw.start_receive();
+        let reader = RmtReader::new(memsize);
+
+        raw.start_receive();
 
         Ok(RxTransaction {
             channel: self,
+            reader,
             data,
         })
     }
@@ -1585,16 +1591,19 @@ impl Channel<Async, Rx> {
     /// The length of sequence cannot exceed the size of the allocated RMT
     /// RAM.
     #[cfg_attr(place_rmt_driver_in_ram, ram)]
-    pub async fn receive<T>(&mut self, data: &mut [T]) -> Result<(), Error>
+    pub async fn receive<T>(&mut self, mut data: &mut [T]) -> Result<(), Error>
     where
         Self: Sized,
         T: From<PulseCode>,
     {
         let raw = self.raw;
+        let memsize = raw.memsize();
 
-        if data.len() > raw.memsize().codes() {
+        if data.len() > memsize.codes() {
             return Err(Error::InvalidDataLength);
         }
+
+        let mut reader = RmtReader::new(memsize);
 
         raw.clear_rx_interrupts();
         raw.listen_rx_interrupt(Event::End | Event::Error);
@@ -1607,11 +1616,7 @@ impl Channel<Async, Rx> {
             raw.clear_rx_interrupts();
             raw.update();
 
-            let ptr = raw.channel_ram_start();
-            let len = data.len();
-            for (idx, entry) in data.iter_mut().take(len).enumerate() {
-                *entry = unsafe { ptr.add(idx).read_volatile().into() };
-            }
+            reader.read(&mut data, raw, true);
         }
 
         result
---
+++ esp-hal/src/rmt/reader.rs
@@ -0,0 +1,52 @@
+use super::{DynChannelAccess, MemSize, PulseCode, Rx};
+
+pub(crate) struct RmtReader {
+    memsize: MemSize,
+
+    // The position in channel RAM to continue reading from; must be either
+    // 0 or half the available RAM size if there's further data.
+    // The position may be invalid if there's no data left.
+    offset: u16,
+}
+
+impl RmtReader {
+    pub(crate) fn new(memsize: MemSize) -> Self {
+        Self { memsize, offset: 0 }
+    }
+
+    #[cfg_attr(place_rmt_driver_in_ram, ram)]
+    pub(crate) fn read<T>(&mut self, data: &mut &mut [T], raw: DynChannelAccess<Rx>, final_: bool)
+    where
+        T: From<PulseCode>,
+    {
+        let memsize = self.memsize.codes();
+        let max_count = if final_ { memsize } else { memsize / 2 };
+        let count = data.len().min(max_count);
+        let mut count0 = count.min(memsize - self.offset as usize);
+        let mut count1 = count - count0;
+
+        // Read in up to 2 chunks to allow wrapping around the buffer end
+        loop {
+            let ptr = unsafe { raw.channel_ram_start().add(self.offset as usize) };
+            for (idx, entry) in data.iter_mut().take(count0).enumerate() {
+                *entry = unsafe { ptr.add(idx).read_volatile() }.into();
+            }
+
+            if count1 == 0 {
+                break;
+            }
+
+            count0 = count1;
+            count1 = 0
+        }
+
+        // If count == memsize / 2 codes were written, update offset as
+        // - 0 -> memsize / 2
+        // - memsize / 2 -> 0
+        // Otherwise, for count < memsize / 2, the new position is invalid but the new slice is
+        // empty and we won't use the offset again.
+        self.offset = (memsize / 2) as u16 - self.offset;
+        data.split_off_mut(..count).unwrap();
+        debug_assert!(self.offset == 0 || self.offset as usize == memsize / 2 || data.is_empty());
+    }
+}
