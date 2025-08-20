a0895d10b8f2fa709ebc2f1b00beb47a5a1c004f
Dominic Fischer <git@dominicfischer.me>
2025-08-21 00:06:02 +0100

    core

--- esp-hal/src/soc/esp32s3/mod.rs
+++ esp-hal/src/soc/esp32s3/mod.rs
@@ -78,18 +78,18 @@ pub(crate) unsafe fn configure_cpu_caches() {
         _ if cfg!(data_cache_size_64kb) => 0x10000,
         _ if cfg!(data_cache_size_32kb) => 0x8000,
         _ if cfg!(data_cache_size_16kb) => 0x4000,
-        _ => unreachable!(),
+        _ => core::unreachable!(),
     };
     const CONFIG_ESP32S3_DCACHE_ASSOCIATED_WAYS: u8 = match () {
         _ if cfg!(dcache_associated_ways_8) => 8,
         _ if cfg!(dcache_associated_ways_4) => 4,
-        _ => unreachable!(),
+        _ => core::unreachable!(),
     };
     const CONFIG_ESP32S3_DATA_CACHE_LINE_SIZE: u8 = match () {
         _ if cfg!(data_cache_line_size_64b) => 64,
         _ if cfg!(data_cache_line_size_32b) => 32,
         _ if cfg!(data_cache_line_size_16b) => 16,
-        _ => unreachable!(),
+        _ => core::unreachable!(),
     };
 
     // Configure the mode of instruction cache: cache size, cache line size.
