069b684f34195404e20c9b9f457fd239d4d005f4
rmsyn <github@weathered-steel.dev>
2025-08-30 20:18:22 +0000

    sdio: add command types
    Adds SDIO commands supported by ESP SDIO controllers.

---
+++ esp-hal/src/sdio/command.rs
@@ -0,0 +1,19 @@
+//! SDIO command types.
+
+mod block_mode;
+mod cmd0;
+mod cmd5;
+mod cmd52;
+pub mod cmd53;
+mod crc;
+mod flag;
+mod fn_number;
+
+pub use block_mode::BlockMode;
+pub use cmd0::Cmd0;
+pub use cmd5::Cmd5;
+pub use cmd52::Cmd52;
+pub use cmd53::Cmd53;
+pub use crc::Crc;
+pub use flag::{RawFlag, RwFlag};
+pub use fn_number::FunctionNumber;
