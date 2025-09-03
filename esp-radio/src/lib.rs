e9cdf1944e40c0241694b3b033ee3c7a419d5ef8
Kirill Mikhailov <konnor1980@yandex.ru>
2025-09-03 17:49:36 +0200

    docs update

--- esp-radio/src/lib.rs
+++ esp-radio/src/lib.rs
@@ -54,7 +54,7 @@
 //!
 //! Please note that the configuration keys are usually named slightly different and not all configuration keys apply.
 //!
-//! By default the power-saving mode is [PowerSaveMode::None](crate::wifi::PowerSaveMode::None) and `ESP_RADIO_PHY_ENABLE_USB` is enabled by default.
+#![cfg_attr(feature = "wifi", doc = "By default the power-saving mode is [PowerSaveMode::None](crate::wifi::PowerSaveMode::None) and `ESP_RADIO_PHY_ENABLE_USB` is enabled by default.")]
 //!
 //! In addition pay attention to these configuration keys:
 //! - `ESP_RADIO_RX_QUEUE_SIZE`
