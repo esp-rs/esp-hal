889470eafea001737007c37da04bd408011ef09f
wisp3rwind <17089248+wisp3rwind@users.noreply.github.com>
2025-09-04 11:14:35 +0200

    RMT: mark tx hw_offset() as allow_unused for now

--- esp-hal/src/rmt.rs
+++ esp-hal/src/rmt.rs
@@ -2388,6 +2388,7 @@ mod chip_specific {
             });
         }
 
+        #[allow(unused)]
         #[inline]
         pub fn hw_offset(&self) -> usize {
             let rmt = crate::peripherals::RMT::regs();
@@ -2836,6 +2837,7 @@ mod chip_specific {
             });
         }
 
+        #[allow(unused)]
         #[inline]
         pub fn hw_offset(&self) -> usize {
             let rmt = crate::peripherals::RMT::regs();
