ba92075343b8250e79daabd338f91e6b51ec927d
wisp3rwind <17089248+wisp3rwind@users.noreply.github.com>
2025-09-04 13:26:57 +0200

    RMT: avoid duplicate call to clear_tx_interrupts() in start_send()
    which would happen for async tx

--- esp-hal/src/rmt.rs
+++ esp-hal/src/rmt.rs
@@ -1586,6 +1586,7 @@ impl<'ch> Channel<'ch, Blocking, Tx> {
         let mut writer = RmtWriter::new();
         writer.write(&mut data, raw, true);
 
+        raw.clear_tx_interrupts();
         raw.start_send(None, memsize);
 
         Ok(SingleShotTxTransaction {
@@ -1628,7 +1629,8 @@ impl<'ch> Channel<'ch, Blocking, Tx> {
         let mut writer = RmtWriter::new();
         writer.write(&mut data, raw, true);
 
-        self.raw.start_send(Some((loopcount, loopstop)), memsize);
+        raw.clear_tx_interrupts();
+        raw.start_send(Some((loopcount, loopstop)), memsize);
 
         Ok(ContinuousTxTransaction { channel: self })
     }
@@ -2043,7 +2045,6 @@ impl DynChannelAccess<Tx> {
     // able to deduplicate.
     #[inline]
     fn start_send(&self, loop_params: Option<(LoopCount, LoopStop)>, memsize: MemSize) {
-        self.clear_tx_interrupts();
         self.set_tx_threshold((memsize.codes() / 2) as u8);
         self.set_tx_continuous(loop_params.is_some());
         #[cfg(rmt_has_tx_loop_count)]
