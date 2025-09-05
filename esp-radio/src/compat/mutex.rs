5c1010f884526e8a3604f3925352bf5c6026cd83
Dániel Buga <bugadani@gmail.com>
2025-09-05 16:31:33 +0200

    Abstract away blob ticks

--- esp-radio/src/compat/mutex.rs
+++ esp-radio/src/compat/mutex.rs
@@ -1,9 +1,6 @@
 use esp_wifi_sys::c_types::c_void;
 
-use crate::{
-    compat::OSI_FUNCS_TIME_BLOCKING,
-    preempt::mutex::{MutexHandle, MutexPtr},
-};
+use crate::preempt::mutex::{MutexHandle, MutexPtr};
 
 pub(crate) fn mutex_create(recursive: bool) -> *mut c_void {
     MutexHandle::new(recursive).leak().as_ptr().cast()
