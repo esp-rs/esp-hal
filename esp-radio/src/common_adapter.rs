849bc3bacf679b9ae139f2caa55328f629c08c03
Frostie314159 <frostie.neuenhausen@gmail.com>
2025-09-11 15:19:08 +0200

    Fixed lint

--- esp-radio/src/common_adapter.rs
+++ esp-radio/src/common_adapter.rs
@@ -294,7 +294,7 @@ pub unsafe extern "C" fn __esp_radio_strrchr(_s: *const (), _c: u32) -> *const u
 static mut __ESP_RADIO_G_LOG_LEVEL: i32 = 0;
 
 #[unsafe(no_mangle)]
-pub static mut __ESP_RADIO_G_MISC_NVS: *mut u32 = unsafe { &raw mut NVS } as *mut u32;
+pub static mut __ESP_RADIO_G_MISC_NVS: *mut u32 = &raw mut NVS as *mut u32;
 
 pub static mut NVS: [u32; 15] = [0u32; 15];
 
