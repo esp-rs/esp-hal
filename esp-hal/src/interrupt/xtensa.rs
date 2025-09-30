fec6684dbc2f9728e9264b5212e1a8684485d244
Björn Quentin <bjoernQ@users.noreply.github.com>
2025-09-30 09:35:49 +0200

    Use correct cfg gates

--- esp-hal/src/interrupt/xtensa.rs
+++ esp-hal/src/interrupt/xtensa.rs
@@ -750,6 +750,8 @@ mod rt {
     unsafe extern "C" {
         fn level4_interrupt(save_frame: &mut Context);
         fn level5_interrupt(save_frame: &mut Context);
+        #[cfg(not(all(feature = "rt", feature = "exception-handler", stack_guard_monitoring)))]
+        fn level6_interrupt(save_frame: &mut Context);
         fn level7_interrupt(save_frame: &mut Context);
     }
 
@@ -769,7 +771,7 @@ mod rt {
     #[unsafe(link_section = ".rwtext")]
     unsafe fn __level_6_interrupt(save_frame: &mut Context) {
         cfg_if::cfg_if! {
-            if #[cfg(stack_guard_monitoring)] {
+            if #[cfg(all(feature = "rt", feature = "exception-handler", stack_guard_monitoring))] {
             crate::exception_handler::breakpoint_interrupt(save_frame);
             } else {
                 unsafe { level6_interrupt(save_frame) }
