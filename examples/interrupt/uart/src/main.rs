fd088aaaaf7fd2b1977e642155640b8dca5b82c9
Zach Grimaldi <owner@grimaldi.tech>
2025-10-09 16:28:46 -0400

    fix: pins now match embassy_serial example

--- examples/interrupt/uart/src/main.rs
+++ examples/interrupt/uart/src/main.rs
@@ -1,7 +1,8 @@
 //! Example of responding to UART interrupts.
 //!
 //! The following wiring is assumed:
-//! - RX => GPIO16
+//! - RX => GPIO3 (ESP32), GPIO19 (ESP32-C2), GPIO20 (ESP32-C3), GPIO17 (ESP32-C6), GPIO23
+//!   (ESP32-H2), GPIO44 (ESP32-S2/S3)
 
 #![no_std]
 #![no_main]
@@ -23,6 +24,23 @@ static SERIAL: Mutex<RefCell<Option<Uart<Blocking>>>> = Mutex::new(RefCell::new(
 #[main]
 fn main() -> ! {
     let peripherals = esp_hal::init(esp_hal::Config::default());
+
+    cfg_if::cfg_if! {
+        if #[cfg(feature = "esp32")] {
+            let rx_pin = peripherals.GPIO3;
+        } else if #[cfg(feature = "esp32c2")] {
+            let rx_pin = peripherals.GPIO19;
+        } else if #[cfg(feature = "esp32c3")] {
+            let rx_pin = peripherals.GPIO20;
+        } else if #[cfg(feature = "esp32c6")] {
+            let rx_pin = peripherals.GPIO17;
+        } else if #[cfg(feature = "esp32h2")] {
+            let rx_pin = peripherals.GPIO23;
+        } else if #[cfg(any(feature = "esp32s2", feature = "esp32s3"))] {
+            let rx_pin = peripherals.GPIO44;
+        }
+    }
+
     let uart_config = UartConfig::default()
         .with_baudrate(19200)
         .with_data_bits(DataBits::_8)
@@ -31,7 +49,7 @@ fn main() -> ! {
         .with_rx(RxConfig::default().with_fifo_full_threshold(1));
     let mut uart = Uart::new(peripherals.UART1, uart_config)
         .expect("Failed to initialize UART")
-        .with_rx(peripherals.GPIO16);
+        .with_rx(rx_pin);
 
     uart.set_interrupt_handler(handler);
 
--- examples/peripheral/uart/src/main.rs
+++ examples/peripheral/uart/src/main.rs
@@ -2,7 +2,8 @@
 //! Blocking mode.
 //!
 //! The following wiring is assumed:
-//! - TX => GPIO17
+//! - TX => GPIO1 (ESP32), GPIO20 (ESP32-C2), GPIO21 (ESP32-C3), GPIO16 (ESP32-C6), GPIO24
+//!   (ESP32-H2), GPIO43 (ESP32-S2/S3)
 
 #![no_std]
 #![no_main]
@@ -17,6 +18,23 @@ use esp_hal::{
 #[main]
 fn main() -> ! {
     let peripherals = esp_hal::init(esp_hal::Config::default());
+
+    cfg_if::cfg_if! {
+        if #[cfg(feature = "esp32")] {
+            let tx_pin = peripherals.GPIO1;
+        } else if #[cfg(feature = "esp32c2")] {
+            let tx_pin = peripherals.GPIO20;
+        } else if #[cfg(feature = "esp32c3")] {
+            let tx_pin = peripherals.GPIO21;
+        } else if #[cfg(feature = "esp32c6")] {
+            let tx_pin = peripherals.GPIO16;
+        } else if #[cfg(feature = "esp32h2")] {
+            let tx_pin = peripherals.GPIO24;
+        } else if #[cfg(any(feature = "esp32s2", feature = "esp32s3"))] {
+            let tx_pin = peripherals.GPIO43;
+        }
+    }
+
     let uart_config = UartConfig::default()
         .with_baudrate(19200)
         .with_data_bits(DataBits::_8)
@@ -24,7 +42,7 @@ fn main() -> ! {
         .with_stop_bits(StopBits::_1);
     let mut uart = Uart::new(peripherals.UART1, uart_config)
         .expect("Failed to initialize UART")
-        .with_tx(peripherals.GPIO17);
+        .with_tx(tx_pin);
     let delay = Delay::new();
 
     loop {
