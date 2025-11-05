41f91c561dfbbd6ca7da455375bd2e875749eaf5
Zach Grimaldi <owner@grimaldi.tech>
2025-11-05 08:11:42 -0500

    test: no sync after enable_listen_rx (just after conf0 writes)

--- esp-hal/src/uart/mod.rs
+++ esp-hal/src/uart/mod.rs
@@ -1246,9 +1246,6 @@ where
         self.uart
             .info()
             .enable_listen_rx(RxEvent::BreakDetected.into(), true);
-
-        #[cfg(any(esp32c6, esp32h2))]
-        sync_regs(self.regs());
     }
 
     /// Change the configuration.
