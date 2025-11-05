4e95ac421177c346749a604e118996e5f07041bd
Zach Grimaldi <owner@grimaldi.tech>
2025-11-05 07:36:08 -0500

    feat: added break sending to uart example

--- examples/interrupt/uart/src/main.rs
+++ examples/interrupt/uart/src/main.rs
@@ -1,8 +1,19 @@
-//! Example of responding to UART interrupts.
+//! Example of responding to UART interrupts and sending break conditions.
 //!
-//! The following wiring is assumed:
-//! - RX => GPIO3 (ESP32), GPIO19 (ESP32-C2), GPIO20 (ESP32-C3), GPIO17 (ESP32-C6), GPIO23
-//!   (ESP32-H2), GPIO44 (ESP32-S2/S3)
+//! This example can be flashed on two devices to demonstrate UART communication
+//! with break detection. Connect the devices as follows:
+//! - Device 1 TX => Device 2 RX
+//! - Device 2 TX => Device 1 RX
+//! - Connect GND between devices
+//!
+//! Default pins:
+//! - TX/RX => GPIO1/GPIO3 (ESP32), GPIO20/GPIO19 (ESP32-C2), GPIO21/GPIO20 (ESP32-C3),
+//!   GPIO16/GPIO17 (ESP32-C6), GPIO24/GPIO23 (ESP32-H2), GPIO43/GPIO44 (ESP32-S2/S3)
+//!
+//! Each device will:
+//! - Send a counter value every second
+//! - Send a break condition every 3 seconds
+//! - Print received data and break conditions via esp-println
 
 #![no_std]
 #![no_main]
@@ -16,6 +27,7 @@ use esp_hal::{
     handler,
     main,
     ram,
+    time::Instant,
     uart::{Config as UartConfig, DataBits, Parity, RxConfig, StopBits, Uart, UartInterrupt},
 };
 
@@ -27,17 +39,17 @@ fn main() -> ! {
 
     cfg_if::cfg_if! {
         if #[cfg(feature = "esp32")] {
-            let rx_pin = peripherals.GPIO3;
+            let (tx_pin, rx_pin) = (peripherals.GPIO1, peripherals.GPIO3);
         } else if #[cfg(feature = "esp32c2")] {
-            let rx_pin = peripherals.GPIO19;
+            let (tx_pin, rx_pin) = (peripherals.GPIO20, peripherals.GPIO19);
         } else if #[cfg(feature = "esp32c3")] {
-            let rx_pin = peripherals.GPIO20;
+            let (tx_pin, rx_pin) = (peripherals.GPIO21, peripherals.GPIO20);
         } else if #[cfg(feature = "esp32c6")] {
-            let rx_pin = peripherals.GPIO17;
+            let (tx_pin, rx_pin) = (peripherals.GPIO16, peripherals.GPIO17);
         } else if #[cfg(feature = "esp32h2")] {
-            let rx_pin = peripherals.GPIO23;
+            let (tx_pin, rx_pin) = (peripherals.GPIO24, peripherals.GPIO23);
         } else if #[cfg(any(feature = "esp32s2", feature = "esp32s3"))] {
-            let rx_pin = peripherals.GPIO44;
+            let (tx_pin, rx_pin) = (peripherals.GPIO43, peripherals.GPIO44);
         }
     }
 
@@ -49,6 +61,7 @@ fn main() -> ! {
         .with_rx(RxConfig::default().with_fifo_full_threshold(1));
     let mut uart = Uart::new(peripherals.UART1, uart_config)
         .expect("Failed to initialize UART")
+        .with_tx(tx_pin)
         .with_rx(rx_pin);
 
     uart.set_interrupt_handler(handler);
@@ -59,7 +72,49 @@ fn main() -> ! {
         SERIAL.borrow_ref_mut(cs).replace(uart);
     });
 
-    loop {}
+    esp_println::println!("UART interrupt example with break conditions");
+    esp_println::println!("Sending counter every second, break every 3 seconds");
+
+    let mut counter: u8 = 0;
+    let mut last_send = Instant::now();
+    let mut last_break = Instant::now();
+
+    loop {
+        let now = Instant::now();
+
+        if (now - last_send).as_millis() >= 1000 {
+            last_send = now;
+
+            critical_section::with(|cs| {
+                if let Some(uart) = SERIAL.borrow_ref_mut(cs).as_mut() {
+                    let msg = [
+                        b'T',
+                        b'X',
+                        b':',
+                        b' ',
+                        b'0' + (counter / 10),
+                        b'0' + (counter % 10),
+                        b'\n',
+                    ];
+                    let _ = uart.write(&msg);
+                    let _ = uart.flush();
+                }
+            });
+
+            counter = counter.wrapping_add(1);
+        }
+
+        if (now - last_break).as_millis() >= 3000 {
+            last_break = now;
+
+            critical_section::with(|cs| {
+                if let Some(uart) = SERIAL.borrow_ref_mut(cs).as_mut() {
+                    esp_println::println!("Sending break condition...");
+                    uart.send_break(14);
+                }
+            });
+        }
+    }
 }
 
 #[handler]
