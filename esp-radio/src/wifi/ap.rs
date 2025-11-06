840ae53b620ff12e36d765306706b45fd85227ac
Jesse Braham <jesse@beta7.io>
2025-11-06 08:19:15 -0800

    Move Wi-Fi AP types to their own module

---
+++ esp-radio/src/wifi/ap.rs
@@ -0,0 +1,143 @@
+//! Wi-Fi access point.
+
+use alloc::string::String;
+use core::fmt;
+
+use enumset::EnumSet;
+use procmacros::BuilderLite;
+
+use super::{AuthMethod, Country, Protocol, SecondaryChannel};
+use crate::WifiError;
+
+/// Information about a detected Wi-Fi access point.
+#[derive(Debug, Default, Clone, PartialEq, Eq)]
+#[cfg_attr(feature = "defmt", derive(defmt::Format))]
+#[non_exhaustive]
+pub struct AccessPointInfo {
+    /// The SSID of the access point.
+    // TODO: we can use the `alloc` feature once we have `defmt` 1.0.2
+    #[cfg_attr(feature = "defmt", defmt(Debug2Format))]
+    pub ssid: String,
+    /// The BSSID (MAC address) of the access point.
+    pub bssid: [u8; 6],
+    /// The channel the access point is operating on.
+    pub channel: u8,
+    /// The secondary channel configuration of the access point.
+    pub secondary_channel: SecondaryChannel,
+    /// The signal strength of the access point (RSSI).
+    pub signal_strength: i8,
+    /// The authentication method used by the access point.
+    pub auth_method: Option<AuthMethod>,
+    /// The country information of the access point (if available from beacon frames).
+    pub country: Option<Country>,
+}
+
+/// Configuration for a Wi-Fi access point.
+#[derive(Clone, PartialEq, Eq, BuilderLite)]
+pub struct AccessPointConfig {
+    /// The SSID of the access point.
+    #[builder_lite(reference)]
+    pub(crate) ssid: String,
+    /// Whether the SSID is hidden or visible.
+    pub(crate) ssid_hidden: bool,
+    /// The channel the access point will operate on.
+    pub(crate) channel: u8,
+    /// The secondary channel configuration.
+    pub(crate) secondary_channel: Option<u8>,
+    /// The set of protocols supported by the access point.
+    pub(crate) protocols: EnumSet<Protocol>,
+    /// The authentication method to be used by the access point.
+    pub(crate) auth_method: AuthMethod,
+    /// The password for securing the access point (if applicable).
+    #[builder_lite(reference)]
+    pub(crate) password: String,
+    /// The maximum number of connections allowed on the access point.
+    pub(crate) max_connections: u16,
+    /// Dtim period of the access point (Range: 1 ~ 10).
+    pub(crate) dtim_period: u8,
+    /// Time to force deauth the STA if the SoftAP doesn't receive any data.
+    #[builder_lite(unstable)]
+    pub(crate) beacon_timeout: u16,
+}
+
+impl AccessPointConfig {
+    pub(crate) fn validate(&self) -> Result<(), WifiError> {
+        if self.ssid.len() > 32 {
+            return Err(WifiError::InvalidArguments);
+        }
+
+        if self.password.len() > 64 {
+            return Err(WifiError::InvalidArguments);
+        }
+
+        if !(1..=10).contains(&self.dtim_period) {
+            return Err(WifiError::InvalidArguments);
+        }
+
+        Ok(())
+    }
+}
+
+impl Default for AccessPointConfig {
+    fn default() -> Self {
+        Self {
+            ssid: String::from("iot-device"),
+            ssid_hidden: false,
+            channel: 1,
+            secondary_channel: None,
+            protocols: (Protocol::P802D11B | Protocol::P802D11BG | Protocol::P802D11BGN),
+            auth_method: AuthMethod::None,
+            password: String::new(),
+            max_connections: 255,
+            dtim_period: 2,
+            beacon_timeout: 300,
+        }
+    }
+}
+
+impl fmt::Debug for AccessPointConfig {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("AccessPointConfig")
+            .field("ssid", &self.ssid)
+            .field("ssid_hidden", &self.ssid_hidden)
+            .field("channel", &self.channel)
+            .field("secondary_channel", &self.secondary_channel)
+            .field("protocols", &self.protocols)
+            .field("auth_method", &self.auth_method)
+            .field("password", &"**REDACTED**")
+            .field("max_connections", &self.max_connections)
+            .field("dtim_period", &self.dtim_period)
+            .field("beacon_timeout", &self.beacon_timeout)
+            .finish()
+    }
+}
+
+#[cfg(feature = "defmt")]
+impl defmt::Format for AccessPointConfig {
+    fn format(&self, fmt: defmt::Formatter<'_>) {
+        defmt::write!(
+            fmt,
+            "AccessPointConfig {{\
+            ssid: {}, \
+            ssid_hidden: {}, \
+            channel: {}, \
+            secondary_channel: {}, \
+            protocols: {}, \
+            auth_method: {}, \
+            password: **REDACTED**, \
+            max_connections: {}, \
+            dtim_period: {}, \
+            beacon_timeout: {} \
+            }}",
+            self.ssid.as_str(),
+            self.ssid_hidden,
+            self.channel,
+            self.secondary_channel,
+            self.protocols,
+            self.auth_method,
+            self.max_connections,
+            self.dtim_period,
+            self.beacon_timeout
+        );
+    }
+}
--- esp-radio/src/wifi/mod.rs
+++ esp-radio/src/wifi/mod.rs
@@ -19,6 +19,7 @@ use smoltcp::phy::{Device, DeviceCapabilities, RxToken, TxToken};
 pub(crate) use self::os_adapter::*;
 pub use self::state::*;
 use self::{
+    ap::{AccessPointConfig, AccessPointInfo},
     private::PacketBuffer,
     scan::{FreeApListOnDrop, ScanResults},
 };
@@ -36,6 +37,7 @@ use crate::{
     },
 };
 
+pub mod ap;
 pub mod event;
 #[cfg(all(feature = "sniffer", feature = "unstable"))]
 pub mod sniffer;
@@ -207,153 +209,6 @@ impl Country {
 impl defmt::Format for Country {
     fn format(&self, fmt: defmt::Formatter<'_>) {
         self.country_code().format(fmt)
-    }
-}
-
-/// Information about a detected Wi-Fi access point.
-#[derive(Clone, Debug, Default, Eq, PartialEq)]
-#[cfg_attr(feature = "defmt", derive(defmt::Format))]
-#[non_exhaustive]
-pub struct AccessPointInfo {
-    /// The SSID of the access point.
-    // TODO: we can use the `alloc` feature once we have `defmt` 1.0.2
-    #[cfg_attr(feature = "defmt", defmt(Debug2Format))]
-    pub ssid: String,
-
-    /// The BSSID (MAC address) of the access point.
-    pub bssid: [u8; 6],
-
-    /// The channel the access point is operating on.
-    pub channel: u8,
-
-    /// The secondary channel configuration of the access point.
-    pub secondary_channel: SecondaryChannel,
-
-    /// The signal strength of the access point (RSSI).
-    pub signal_strength: i8,
-
-    /// The authentication method used by the access point.
-    pub auth_method: Option<AuthMethod>,
-
-    /// The country information of the access point (if available from beacon frames).
-    pub country: Option<Country>,
-}
-
-/// Configuration for a Wi-Fi access point.
-#[derive(BuilderLite, Clone, Eq, PartialEq)]
-pub struct AccessPointConfig {
-    /// The SSID of the access point.
-    #[builder_lite(reference)]
-    ssid: String,
-
-    /// Whether the SSID is hidden or visible.
-    ssid_hidden: bool,
-
-    /// The channel the access point will operate on.
-    channel: u8,
-
-    /// The secondary channel configuration.
-    secondary_channel: Option<u8>,
-
-    /// The set of protocols supported by the access point.
-    protocols: EnumSet<Protocol>,
-
-    /// The authentication method to be used by the access point.
-    auth_method: AuthMethod,
-
-    /// The password for securing the access point (if applicable).
-    #[builder_lite(reference)]
-    password: String,
-
-    /// The maximum number of connections allowed on the access point.
-    max_connections: u16,
-    /// Dtim period of the access point (Range: 1 ~ 10).
-    dtim_period: u8,
-
-    /// Time to force deauth the STA if the SoftAP doesn't receive any data.
-    #[builder_lite(unstable)]
-    beacon_timeout: u16,
-}
-
-impl AccessPointConfig {
-    fn validate(&self) -> Result<(), WifiError> {
-        if self.ssid.len() > 32 {
-            return Err(WifiError::InvalidArguments);
-        }
-
-        if self.password.len() > 64 {
-            return Err(WifiError::InvalidArguments);
-        }
-
-        if !(1..=10).contains(&self.dtim_period) {
-            return Err(WifiError::InvalidArguments);
-        }
-
-        Ok(())
-    }
-}
-
-impl Default for AccessPointConfig {
-    fn default() -> Self {
-        Self {
-            ssid: String::from("iot-device"),
-            ssid_hidden: false,
-            channel: 1,
-            secondary_channel: None,
-            protocols: (Protocol::P802D11B | Protocol::P802D11BG | Protocol::P802D11BGN),
-            auth_method: AuthMethod::None,
-            password: String::new(),
-            max_connections: 255,
-            dtim_period: 2,
-            beacon_timeout: 300,
-        }
-    }
-}
-
-impl core::fmt::Debug for AccessPointConfig {
-    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        f.debug_struct("AccessPointConfig")
-            .field("ssid", &self.ssid)
-            .field("ssid_hidden", &self.ssid_hidden)
-            .field("channel", &self.channel)
-            .field("secondary_channel", &self.secondary_channel)
-            .field("protocols", &self.protocols)
-            .field("auth_method", &self.auth_method)
-            .field("password", &"**REDACTED**")
-            .field("max_connections", &self.max_connections)
-            .field("dtim_period", &self.dtim_period)
-            .field("beacon_timeout", &self.beacon_timeout)
-            .finish()
-    }
-}
-
-#[cfg(feature = "defmt")]
-impl defmt::Format for AccessPointConfig {
-    fn format(&self, fmt: defmt::Formatter<'_>) {
-        defmt::write!(
-            fmt,
-            "AccessPointConfig {{\
-            ssid: {}, \
-            ssid_hidden: {}, \
-            channel: {}, \
-            secondary_channel: {}, \
-            protocols: {}, \
-            auth_method: {}, \
-            password: **REDACTED**, \
-            max_connections: {}, \
-            dtim_period: {}, \
-            beacon_timeout: {} \
-            }}",
-            self.ssid.as_str(),
-            self.ssid_hidden,
-            self.channel,
-            self.secondary_channel,
-            self.protocols,
-            self.auth_method,
-            self.max_connections,
-            self.dtim_period,
-            self.beacon_timeout
-        );
     }
 }
 
