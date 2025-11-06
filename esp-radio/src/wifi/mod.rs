3c9633f283315acd191259143e6a9711bc2adcc1
Jesse Braham <jesse@beta7.io>
2025-11-06 08:03:44 -0800

    Move Wi-Fi sniffer types/functions to their own module

--- esp-radio/src/wifi/mod.rs
+++ esp-radio/src/wifi/mod.rs
@@ -37,10 +37,14 @@ use crate::{
 };
 
 pub mod event;
-mod internal;
+#[cfg(all(feature = "sniffer", feature = "unstable"))]
+pub mod sniffer;
+
 pub(crate) mod os_adapter;
-mod scan;
 pub(crate) mod state;
+
+mod internal;
+mod scan;
 
 const MTU: usize = esp_config_int!(usize, "ESP_RADIO_CONFIG_WIFI_MTU");
 
@@ -2137,111 +2141,6 @@ impl RxControlInfo {
         rx_control_info
     }
 }
-/// Represents a Wi-Fi packet in promiscuous mode.
-#[cfg(all(feature = "sniffer", feature = "unstable"))]
-#[instability::unstable]
-pub struct PromiscuousPkt<'a> {
-    /// Control information related to packet reception.
-    pub rx_cntl: RxControlInfo,
-    /// Frame type of the received packet.
-    pub frame_type: wifi_promiscuous_pkt_type_t,
-    /// Length of the received packet.
-    pub len: usize,
-    /// Data contained in the received packet.
-    pub data: &'a [u8],
-}
-#[cfg(all(feature = "sniffer", feature = "unstable"))]
-#[cfg_attr(docsrs, doc(cfg(feature = "unstable")))]
-impl PromiscuousPkt<'_> {
-    /// # Safety
-    /// When calling this, you have to ensure, that `buf` points to a valid
-    /// [wifi_promiscuous_pkt_t].
-    pub(crate) unsafe fn from_raw(
-        buf: *const wifi_promiscuous_pkt_t,
-        frame_type: wifi_promiscuous_pkt_type_t,
-    ) -> Self {
-        let rx_cntl = unsafe { RxControlInfo::from_raw(&(*buf).rx_ctrl) };
-        let len = rx_cntl.sig_len as usize;
-        PromiscuousPkt {
-            rx_cntl,
-            frame_type,
-            len,
-            data: unsafe {
-                core::slice::from_raw_parts(
-                    (buf as *const u8).add(core::mem::size_of::<wifi_pkt_rx_ctrl_t>()),
-                    len,
-                )
-            },
-        }
-    }
-}
-
-#[cfg(all(feature = "sniffer", feature = "unstable"))]
-static SNIFFER_CB: NonReentrantMutex<Option<fn(PromiscuousPkt<'_>)>> = NonReentrantMutex::new(None);
-
-#[cfg(all(feature = "sniffer", feature = "unstable"))]
-unsafe extern "C" fn promiscuous_rx_cb(buf: *mut core::ffi::c_void, frame_type: u32) {
-    unsafe {
-        if let Some(sniffer_callback) = SNIFFER_CB.with(|callback| *callback) {
-            let promiscuous_pkt = PromiscuousPkt::from_raw(buf as *const _, frame_type);
-            sniffer_callback(promiscuous_pkt);
-        }
-    }
-}
-
-#[cfg(all(feature = "sniffer", feature = "unstable"))]
-#[instability::unstable]
-/// A Wi-Fi sniffer.
-#[non_exhaustive]
-pub struct Sniffer<'d> {
-    _phantom: PhantomData<&'d ()>,
-}
-
-#[cfg(all(feature = "sniffer", feature = "unstable"))]
-impl Sniffer<'_> {
-    pub(crate) fn new() -> Self {
-        // This shouldn't fail, since the way this is created, means that wifi will
-        // always be initialized.
-        unwrap!(esp_wifi_result!(unsafe {
-            esp_wifi_set_promiscuous_rx_cb(Some(promiscuous_rx_cb))
-        }));
-        Self {
-            _phantom: PhantomData,
-        }
-    }
-    /// Set promiscuous mode enabled or disabled.
-    #[instability::unstable]
-    pub fn set_promiscuous_mode(&self, enabled: bool) -> Result<(), WifiError> {
-        esp_wifi_result!(unsafe { esp_wifi_set_promiscuous(enabled) })?;
-        Ok(())
-    }
-    /// Transmit a raw frame.
-    #[instability::unstable]
-    pub fn send_raw_frame(
-        &mut self,
-        use_sta_interface: bool,
-        buffer: &[u8],
-        use_internal_seq_num: bool,
-    ) -> Result<(), WifiError> {
-        esp_wifi_result!(unsafe {
-            esp_wifi_80211_tx(
-                if use_sta_interface {
-                    wifi_interface_t_WIFI_IF_STA
-                } else {
-                    wifi_interface_t_WIFI_IF_AP
-                } as wifi_interface_t,
-                buffer.as_ptr() as *const _,
-                buffer.len() as i32,
-                use_internal_seq_num,
-            )
-        })
-    }
-    /// Set the callback for receiving a packet.
-    #[instability::unstable]
-    pub fn set_receive_cb(&mut self, cb: fn(PromiscuousPkt<'_>)) {
-        SNIFFER_CB.with(|callback| *callback = Some(cb));
-    }
-}
 
 // see https://docs.rs/smoltcp/0.7.1/smoltcp/phy/index.html
 #[cfg(all(feature = "smoltcp", feature = "unstable"))]
@@ -2544,7 +2443,7 @@ pub struct Interfaces<'d> {
     /// Wi-Fi sniffer interface.
     #[cfg(all(feature = "sniffer", feature = "unstable"))]
     #[cfg_attr(docsrs, doc(cfg(feature = "unstable")))]
-    pub sniffer: Sniffer<'d>,
+    pub sniffer: self::sniffer::Sniffer<'d>,
 }
 
 /// Wi-Fi operating class.
@@ -2875,7 +2774,7 @@ pub fn new<'d>(
             #[cfg(all(feature = "esp-now", feature = "unstable"))]
             esp_now: crate::esp_now::EspNow::new_internal(),
             #[cfg(all(feature = "sniffer", feature = "unstable"))]
-            sniffer: Sniffer::new(),
+            sniffer: self::sniffer::Sniffer::new(),
         },
     ))
 }
