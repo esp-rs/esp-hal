6cbb4c4b91988966d1a36dd811e6c2c6246ad418
Dániel Buga <bugadani@gmail.com>
2025-11-07 14:19:34 +0100

    Wrap PeripheralClocks into DeviceClocks (#4464)

--- esp-metadata/src/lib.rs
+++ esp-metadata/src/lib.rs
@@ -269,7 +269,7 @@ impl PeripheralDef {
     }
 }
 
-#[derive(Debug, Clone, serde::Deserialize, serde::Serialize)]
+#[derive(Debug, Clone, serde::Deserialize)]
 struct Device {
     name: String,
     arch: Arch,
@@ -295,7 +295,7 @@ fn number_hex(n: impl std::fmt::Display + std::fmt::UpperHex) -> TokenStream {
 }
 
 /// Device configuration file format.
-#[derive(Debug, Clone, serde::Deserialize, serde::Serialize)]
+#[derive(Debug, Clone, serde::Deserialize)]
 pub struct Config {
     device: Device,
     #[serde(skip)]
