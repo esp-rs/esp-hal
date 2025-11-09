a8deae5122255210fbe8032347864a0d3a2c6ef7
owenthewizard <owen@walpole.dev>
2025-11-09 03:48:26 -0600

    fix: correct module name `eth` -> `rmii`
    Signed-off-by: owenthewizard <owen@walpole.dev>

--- esp-hal/src/lib.rs
+++ esp-hal/src/lib.rs
@@ -366,7 +366,7 @@ unstable_driver! {
     #[cfg(soc_has_ecc)]
     pub mod ecc;
     #[cfg(all(soc_has_emac_dma, soc_has_emac_ext, soc_has_emac_mac))]
-    pub mod eth;
+    pub mod rmii;
     #[cfg(soc_has_hmac)]
     pub mod hmac;
     #[cfg(any(soc_has_i2s0, soc_has_i2s1))]
