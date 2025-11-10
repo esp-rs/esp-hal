e2c998bedc018e2ca06e134efa6a552725c1169c
Jesse Braham <jesse@beta7.io>
2025-11-10 10:17:23 -0800

    Move `convert_ap_info` function to `ap` module

--- esp-radio/src/wifi/ap.rs
+++ esp-radio/src/wifi/ap.rs
@@ -6,8 +6,16 @@ use core::fmt;
 use enumset::EnumSet;
 use procmacros::BuilderLite;
 
-use super::{AuthMethod, Country, Protocol, SecondaryChannel};
-use crate::WifiError;
+use super::{AuthMethod, AuthMethodExt as _, Country, Protocol, SecondaryChannel};
+use crate::{
+    WifiError,
+    sys::include::{
+        wifi_ap_record_t,
+        wifi_second_chan_t_WIFI_SECOND_CHAN_ABOVE,
+        wifi_second_chan_t_WIFI_SECOND_CHAN_BELOW,
+        wifi_second_chan_t_WIFI_SECOND_CHAN_NONE,
+    },
+};
 
 /// Information about a detected Wi-Fi access point.
 #[derive(Debug, Default, Clone, PartialEq, Eq)]
@@ -139,5 +147,30 @@ impl defmt::Format for AccessPointConfig {
             self.dtim_period,
             self.beacon_timeout
         );
+    }
+}
+
+#[allow(non_upper_case_globals)]
+pub(crate) fn convert_ap_info(record: &wifi_ap_record_t) -> AccessPointInfo {
+    let str_len = record
+        .ssid
+        .iter()
+        .position(|&c| c == 0)
+        .unwrap_or(record.ssid.len());
+    let ssid = alloc::string::String::from_utf8_lossy(&record.ssid[..str_len]).into_owned();
+
+    AccessPointInfo {
+        ssid,
+        bssid: record.bssid,
+        channel: record.primary,
+        secondary_channel: match record.second {
+            wifi_second_chan_t_WIFI_SECOND_CHAN_NONE => SecondaryChannel::None,
+            wifi_second_chan_t_WIFI_SECOND_CHAN_ABOVE => SecondaryChannel::Above,
+            wifi_second_chan_t_WIFI_SECOND_CHAN_BELOW => SecondaryChannel::Below,
+            _ => panic!(),
+        },
+        signal_strength: record.rssi,
+        auth_method: Some(AuthMethod::from_raw(record.authmode)),
+        country: Country::try_from_c(&record.country),
     }
 }
