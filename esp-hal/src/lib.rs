e8e4bba34c3442b39ef909cdd455a0b92b7fae5f
Dániel Buga <bugadani@gmail.com>
2025-12-08 17:54:55 +0100

    Rewrite time, fix timestamp conversion

--- esp-hal/src/lib.rs
+++ esp-hal/src/lib.rs
@@ -754,7 +754,7 @@ pub fn init(config: Config) -> Peripherals {
     #[cfg(timergroup_timg1)]
     crate::timer::timg::Wdt::<crate::peripherals::TIMG1<'static>>::new().disable();
 
-    crate::time::time_init();
+    crate::time::implem::time_init();
 
     crate::gpio::interrupt::bind_default_interrupt_handler();
 
