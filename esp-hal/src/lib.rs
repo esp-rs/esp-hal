26c0843da7654bd579ba7d9d3f5b1332c91baa80
Dániel Buga <bugadani@gmail.com>
2025-12-08 16:58:37 +0100

    Add timer init hook for all chips

--- esp-hal/src/lib.rs
+++ esp-hal/src/lib.rs
@@ -754,7 +754,6 @@ pub fn init(config: Config) -> Peripherals {
     #[cfg(timergroup_timg1)]
     crate::timer::timg::Wdt::<crate::peripherals::TIMG1<'static>>::new().disable();
 
-    #[cfg(esp32)]
     crate::time::time_init();
 
     crate::gpio::interrupt::bind_default_interrupt_handler();
