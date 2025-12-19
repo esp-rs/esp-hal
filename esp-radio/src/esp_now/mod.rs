3e68b81f67129fae6e0f6d05d223b8765dbc6465
Juraj Sadel <juraj.sadel@espressif.com>
2025-12-19 12:24:40 +0100

    esp-radio: Hide `wifi_csi_info_t` from pub API (#4643)
    * esp-radio: Hide wifi_csi_info_t from pub API
    
    * reviews
    
    * changelog
    
    * Really don't expose the C type
    
    * Comment about the known bindgen bug  workaround
    
    * refactor and clean

--- esp-radio/src/esp_now/mod.rs
+++ esp-radio/src/esp_now/mod.rs
@@ -422,7 +422,7 @@ impl EspNowManager<'_> {
     pub fn set_csi(
         &mut self,
         mut csi: CsiConfig,
-        cb: impl FnMut(crate::wifi::wifi_csi_info_t) + Send,
+        cb: impl FnMut(crate::wifi::csi::WifiCsiInfo<'_>) + Send,
     ) -> Result<(), WifiError> {
         csi.apply_config()?;
         csi.set_receive_cb(cb)?;
