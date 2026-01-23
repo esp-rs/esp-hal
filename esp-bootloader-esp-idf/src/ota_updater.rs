78a102fb45e29251a254ffad7080101ebc3718cc
B83C <b83c@outlook.com>
2026-01-23 21:30:35 +0800

    Fixed PartitionEntry usage in tests

--- esp-bootloader-esp-idf/src/ota_updater.rs
+++ esp-bootloader-esp-idf/src/ota_updater.rs
@@ -79,7 +79,7 @@ where
                 crate::partitions::DataPartitionSubType::Ota,
             ))?;
         if let Some(ota_part) = ota_part {
-            let ota_part = ota_part.as_embedded_storage(self.flash);
+            let ota_part = ota_part.clone().as_embedded_storage(self.flash);
             let ota = crate::ota::Ota::new(ota_part, self.ota_count)?;
             Ok(ota)
         } else {
@@ -152,6 +152,7 @@ where
             .pt
             .find_partition(crate::partitions::PartitionType::App(next_slot))?
             .ok_or(Error::Invalid)?
+            .clone()
             .as_embedded_storage(self.flash);
 
         Ok((flash_region, next_slot))
