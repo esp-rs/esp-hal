1ed6252673a3b6531c4bf1750b013ea7064bfddf
Dániel Buga <bugadani@gmail.com>
2026-02-03 15:31:31 +0100

    Restore interrupt HIL tests

--- hil-test/src/bin/interrupt.rs
+++ hil-test/src/bin/interrupt.rs
@@ -22,12 +22,11 @@ use esp_hal::{
     peripherals::Interrupt,
 };
 use hil_test as _;
-use portable_atomic::{AtomicU32, Ordering};
 
 static SWINT0: Mutex<RefCell<Option<SoftwareInterrupt<0>>>> = Mutex::new(RefCell::new(None));
 
 #[unsafe(no_mangle)]
-static LAST_PERF: AtomicU32 = AtomicU32::new(333);
+static mut LAST_PERF: u32 = 0;
 
 #[unsafe(no_mangle)]
 static mut SW_TRIGGER_ADDR: *mut u32 = core::ptr::null_mut();
@@ -41,15 +40,14 @@ struct Context {
 #[unsafe(no_mangle)]
 #[unsafe(naked)]
 #[rustfmt::skip]
-#[cfg(not(esp32c5))]
 unsafe extern "C" fn interrupt_handler() {
     core::arch::naked_asm! {"
         # save affected registers
-        addi sp, sp, -4*4 # allocate 4 words for saving regs (will work with just 2, but RISC-V wants it to be 16 byte aligned)
+        addi sp, sp, -16*4 # allocate 16 words for saving regs (will work with just 2, but RISC-V wants it to be aligned by 16)
         sw t0, 1*4(sp)
         sw t1, 2*4(sp)
 
-        csrrwi x0, 0x7e1, 0 # disable timer
+        csrrwi x0, 0x7e1, 0 #disable timer
 
         lw t0, {sw}  # t0 <- &SW_TRIGGER_ADDR
         sw x0, 0(t0) # clear (deassert) *SW_TRIGGER_ADDR
@@ -61,7 +59,7 @@ unsafe extern "C" fn interrupt_handler() {
         # restore affected registers
         lw t0, 1*4(sp)
         lw t1, 2*4(sp)
-        addi sp, sp, 4*4
+        addi sp, sp, 16*4
         mret
         ",
         x = sym LAST_PERF,
@@ -80,10 +78,10 @@ mod tests {
         let sw_ints = SoftwareInterruptControl::new(peripherals.SW_INTERRUPT);
 
         cfg_if::cfg_if! {
-            if #[cfg(any(esp32c5, esp32c6, esp32h2))] {
-                let cpu_intr = peripherals.INTPRI;
+            if #[cfg(any(feature = "esp32c6", feature = "esp32h2"))] {
+                let cpu_intr = &peripherals.INTPRI;
             } else {
-                let cpu_intr = peripherals.SYSTEM;
+                let cpu_intr = &peripherals.SYSTEM;
             }
         }
 
@@ -101,7 +99,7 @@ mod tests {
         interrupt::enable_direct(
             Interrupt::FROM_CPU_INTR0,
             Priority::Priority3,
-            CpuInterrupt::Interrupt5,
+            CpuInterrupt::Interrupt20,
             interrupt_handler,
         )
         .unwrap();
@@ -117,30 +115,37 @@ mod tests {
         unsafe {
             asm!(
                 "
-                    csrrwi x0, 0x7e0, 1 # what to count, for cycles write 1 for instructions write 2
-                    csrrwi x0, 0x7e1, 0 # disable counter
-                    csrrwi x0, 0x7e2, 0 # reset counter
+                    csrrwi x0, 0x7e0, 1 #what to count, for cycles write 1 for instructions write 2
+                    csrrwi x0, 0x7e1, 0 #disable counter
+                    csrrwi x0, 0x7e2, 0 #reset counter
+                "
+            );
+        }
 
-                    # interrupt is raised from assembly for max timer granularity.
+        // interrupt is raised from assembly for max timer granularity.
+        unsafe {
+            asm!(
+                "
+                    li {bit}, 1                   # Flip flag (bit 0)
                     csrrwi x0, 0x7e1, 1           # enable timer
                     sw {bit}, 0({addr})           # trigger FROM_CPU_INTR0
                 ",
                 options(nostack),
                 addr = in(reg) ctx.sw0_trigger_addr,
-                bit = in(reg) 1,
+                bit = out(reg) _,
             )
         }
 
-        let perf_counter = LAST_PERF.load(Ordering::SeqCst);
+        let perf_counter = unsafe { LAST_PERF };
         defmt::info!("Performance counter: {}", perf_counter);
 
         // TODO c3/c2 values should be adjusted to catch smaller regressions
         cfg_if::cfg_if! {
-            if #[cfg(any(esp32c2, esp32c3))] {
-                assert!(perf_counter < 400);
-            } else {
-                assert!(perf_counter < 155);
-            }
+        if #[cfg(any(feature = "esp32c3", feature = "esp32c2"))] {
+            assert!(perf_counter < 400);
+        } else {
+            assert!(perf_counter < 155);
         }
+    }
     }
 }
