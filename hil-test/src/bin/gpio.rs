78aa92036825043d96c9a9b3587649579e2b3e5f
tzyt <zyt2006613@outlook.com>
2026-02-06 02:57:41 -0600

    edit dedicated_gpio_different_cores_panics, now it creates the driver at core1 and use it at core0

--- hil-test/src/bin/gpio.rs
+++ hil-test/src/bin/gpio.rs
@@ -680,27 +680,51 @@ mod tests {
         assert_eq!(output_dedicated.output_level(), Level::High);
     }
 
-    #[test]
-    #[ignore]
-    #[should_panic]
-    #[cfg(all(dedicated_gpio, multi_core, feature = "unstable"))]
-    // TODO: this somehow will not panic as expected, even directly assesrt!(false)
-    // so added ignore as for now
-    // maybe panics are not detected on core1?
-    fn dedicated_gpio_different_cores_panics(ctx: Context) {
-        use esp_hal::system::Stack;
-        use hil_test::mk_static;
-        let input = Input::new(ctx.test_gpio1, InputConfig::default().with_pull(Pull::Down));
-        let input_dedicated = DedicatedGpioInput::new(ctx.dedicated_gpio.channel1.input, input);
+#[test]
+#[should_panic]
+#[cfg(all(dedicated_gpio, multi_core, feature = "unstable", debug_assertions))]
+fn dedicated_gpio_different_cores_panics(ctx: Context) {
+    use core::{
+        mem::MaybeUninit,
+        sync::atomic::{AtomicBool, Ordering},
+    };
+    use esp_hal::system::Stack;
+    use hil_test::mk_static;
 
-        let app_core_stack = mk_static!(Stack<4096>, Stack::new());
-        // create on core0 and then use on core1.
-        // this should panic
-        esp_rtos::start_second_core(ctx.cpu_ctrl, ctx.int1, app_core_stack, move || {
-            let _level = input_dedicated.level();
-        });
-        loop {}
+    let pin1 = ctx.test_gpio1;
+    let static_mem_slot =
+        mk_static!(MaybeUninit<DedicatedGpioInput<'static>>, MaybeUninit::uninit());
+    let slot_addr: usize = static_mem_slot.as_mut_ptr() as usize; 
+    // cast to usize, otherwise compiler will complain about !Send
+    let ready = &*mk_static!(AtomicBool, AtomicBool::new(false));
+    let app_core_stack = mk_static!(Stack<4096>, Stack::new());
+
+    // creating the driver at core1, and then use it at core
+    // this should panic
+    esp_rtos::start_second_core(ctx.cpu_ctrl, ctx.int1, app_core_stack, move || {
+        let input = Input::new(pin1, InputConfig::default().with_pull(Pull::Down));
+        let driver = DedicatedGpioInput::new(ctx.dedicated_gpio.channel1.input, input);
+
+        let driver_static: DedicatedGpioInput<'static> = unsafe {
+            core::mem::transmute::<DedicatedGpioInput<'_>, DedicatedGpioInput<'static>>(driver)
+        };
+
+        let slot_ptr = slot_addr as *mut DedicatedGpioInput<'static>;
+        unsafe { slot_ptr.write(driver_static) };
+        ready.store(true, Ordering::Release);
+
+    });
+
+    while !ready.load(Ordering::Acquire) {
+        core::hint::spin_loop();
+        // wait until the driver is transferred to the static slot
     }
+
+    let input_dedicated: DedicatedGpioInput<'static> =
+        unsafe { static_mem_slot.assume_init_read() };
+
+    let _level = input_dedicated.level();
+}
 
     #[cfg(all(dedicated_gpio, feature = "unstable"))]
     macro_rules! make_dedicated_io_and_bundles {
