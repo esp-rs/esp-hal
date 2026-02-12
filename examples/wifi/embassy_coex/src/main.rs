ffe55e37ebd4f9cd382543d8532c99c625dc9237
Ulf Lilleengen <ulf@digili.no>
2026-02-12 08:36:49 +0100

    fix: update coex example

--- examples/wifi/embassy_coex/src/main.rs
+++ examples/wifi/embassy_coex/src/main.rs
@@ -60,7 +60,7 @@ struct Server {
 struct BatteryService {
     /// Battery Level
     #[descriptor(uuid = descriptors::VALID_RANGE, read, value = [0, 100])]
-    #[descriptor(uuid = descriptors::MEASUREMENT_DESCRIPTION, name = "hello", read, value = "Battery Level")]
+    #[descriptor(uuid = descriptors::MEASUREMENT_DESCRIPTION, name = "hello", read, value = "Battery Level", type = &'static str)]
     #[characteristic(uuid = characteristic::BATTERY_LEVEL, read, notify, value = 10)]
     level: u8,
     #[characteristic(uuid = "408813df-5dd4-1f87-ec11-cdb001100000", write, read, notify)]
