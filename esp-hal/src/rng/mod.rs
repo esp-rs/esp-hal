b555520595f02574a84ad13c90ba4519f8b33288
Juraj Sadel <juraj.sadel@espressif.com>
2026-03-03 15:44:08 +0100

    tweaks

--- esp-hal/src/rng/mod.rs
+++ esp-hal/src/rng/mod.rs
@@ -14,68 +14,31 @@
 //! produce *true* random numbers. The hardware RNG produces true random numbers
 //! under any of the following conditions:
 //!
+//! <div class="warning">
+//!
 //! - RF subsystem is enabled (i.e. Wi-Fi or Bluetooth are enabled).
 //! - An ADC is used to generate entropy.
 //! - An internal entropy source has been enabled by calling `bootloader_random_enable()` and not
 //!   yet disabled by calling `bootloader_random_disable()`. As the default bootloader calls
 //!   `bootloader_random_disable()` automatically, this option requires a custom bootloader build.
+//! </div>
 //!
 //! When any of these conditions are true, samples of physical noise are
 //! continuously mixed into the internal hardware RNG state to provide entropy.
 //! If none of the above conditions are true, the output of the RNG should be
 //! considered pseudo-random only.
 //!
-//! > Note that, when the Wi-Fi module is enabled, the value read from the high-speed ADC can be
-//! > saturated in some extreme cases, which lowers the entropy. Thus, it is advisable to also
-//! > enable the SAR ADC as the noise source for the random number generator for such cases.
+//! <div class="warning">
+//!
+//! Note that, when the Wi-Fi module is enabled, the value read from the high-speed ADC can be
+//! saturated in some extreme cases, which lowers the entropy. Thus, it is advisable to also nable
+//! the SAR ADC as the noise source for the random number generator for such cases.
+//! </div>
 //!
 //! For more information, please refer to the
 //! # {documentation}
 //!
-//! ## Configuration
-//!
-//! To generate pseudo-random numbers, you can create [`Rng`] at any time.
-#![cfg_attr(
-    rng_trng_supported,
-    doc = r"To generate
-true random numbers, you need to create an instance of [`TrngSource`]. Once you've
-done that, you can create [`Trng`] instances at any time, as long as the [`TrngSource`]
-is alive."
-)]
-//! ## Compatibility with [`rand_core`]
-//!
-//! The RNG drivers implement the relevant
-//! traits from versions `0.6` and `0.9` of the [`rand_core`] crate.
-//!
-//! [`rand_core`]: https://crates.io/crates/rand_core
-//!
-//! ## Compatibility with [`getrandom`]
-//!
-//! The driver can be used to implement a custom backend for `getrandom`.
-//!
 //! ## Example
-//!
-//! The following example demonstrates how to set up a [custom backend] using `getrandom v0.3.3`:
-//!
-//! ```rust, ignore
-//! use getrandom::Error;
-//!
-//! #[no_mangle]
-//! unsafe extern "Rust" fn __getrandom_v03_custom(
-//!     dest: *mut u8,
-//!     len: usize,
-//! ) -> Result<(), Error> {
-//!     unsafe { esp_hal::rng:Rng::new().read_into_raw() };
-//!     Ok(())
-//! }
-//! ```
-//!
-//! [`getrandom`]: https://crates.io/crates/getrandom
-//! [custom backend]: https://github.com/rust-random/getrandom/tree/v0.3.3?tab=readme-ov-file#custom-backend
-//!
-//! ## Examples
-//!
-//! ### Basic RNG operation
 //!
 //! ```rust, no_run
 //! # {before_snippet}
@@ -93,6 +56,36 @@ is alive."
 //! loop {}
 //! # }
 //! ```
+//!
+//! ## Configuration
+//!
+//! To generate pseudo-random numbers, you can create [`Rng`] at any time.
+#![cfg_attr(
+    rng_trng_supported,
+    doc = r"To generate
+true random numbers, you need to create an instance of [`TrngSource`]. Once you've
+done that, you can create [`Trng`] instances at any time, as long as the [`TrngSource`]
+is alive."
+)]
+//! ## Compatibility with [`getrandom`]
+//!
+//! The driver can be used as a [custom backend] for `getrandom v0.3`:
+//!
+//! ```rust,ignore
+//! use getrandom::Error;
+//!
+//! #[no_mangle]
+//! unsafe extern "Rust" fn __getrandom_v03_custom(
+//!     dest: *mut u8,
+//!     len: usize,
+//! ) -> Result<(), Error> {
+//!     unsafe { esp_hal::rng::Rng::new().read_into_raw(dest, len) };
+//!     Ok(())
+//! }
+//! ```
+//!
+//! [`getrandom`]: https://crates.io/crates/getrandom
+//! [custom backend]: https://github.com/rust-random/getrandom/tree/v0.3.3?tab=readme-ov-file#custom-backend
 
 mod ll;
 
