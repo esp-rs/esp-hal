0a3e511cfe5dbe62addf91ae17430b52ee934b86
puyogo-suzuki <puyogo.suzuki@gmail.com>
2026-03-06 15:57:41 +0900

    feat(example): Restore lp_blinky Example for ESP32-S2, and S3

--- examples/peripheral/lp_core/lp_blinky/src/main.rs
+++ examples/peripheral/lp_core/lp_blinky/src/main.rs
@@ -12,16 +12,22 @@
 #![no_main]
 
 use esp_backtrace as _;
+#[cfg(feature = "esp32c6")]
 use esp_hal::{
     gpio::lp_io::LowPowerOutput,
-    load_lp_code,
     lp_core::{LpCore, LpCoreWakeupSource},
-    main,
 };
+#[cfg(any(feature = "esp32s2", feature = "esp32s3"))]
+use esp_hal::{
+    gpio::rtc_io::LowPowerOutput,
+    ulp_core::{UlpCore, UlpCoreWakeupSource},
+};
+use esp_hal::{load_lp_code, main};
 use esp_println::{print, println};
 
 esp_bootloader_esp_idf::esp_app_desc!();
 
+#[cfg(feature = "esp32c6")]
 #[main]
 fn main() -> ! {
     esp_println::logger::init_logger_from_env();
@@ -42,9 +48,42 @@ fn main() -> ! {
 
     // start LP core
     lp_core_code.run(&mut lp_core, LpCoreWakeupSource::HpCpu, lp_pin);
-    println!("lpcore run");
+    println!("lp core run");
 
-    let data = (0x5000_2000) as *mut u32;
+    let data = (0x5000_2000) as *const u32;
+    loop {
+        print!("Current {:x}           \u{000d}", unsafe {
+            data.read_volatile()
+        });
+    }
+}
+
+#[cfg(any(feature = "esp32s2", feature = "esp32s3"))]
+#[main]
+fn main() -> ! {
+    esp_println::logger::init_logger_from_env();
+    let peripherals = esp_hal::init(esp_hal::Config::default());
+
+    // configure GPIO 1 as LP output pin
+    let rtc_pin = LowPowerOutput::new(peripherals.GPIO1);
+
+    let mut ulp_core = UlpCore::new(peripherals.ULP_RISCV_CORE);
+    #[cfg(not(feature = "esp32s2"))]
+    {
+        ulp_core.stop(); // currently not implemented for ESP32-S2.
+        println!("ulp core stopped");
+    }
+
+    // load code to ULP coprocessor
+    let ulp_core_code = load_lp_code!(
+        "../../../../esp-lp-hal/target/riscv32imc-unknown-none-elf/release/examples/blinky"
+    );
+
+    // start ULP coprocessor
+    ulp_core_code.run(&mut ulp_core, UlpCoreWakeupSource::HpCpu, rtc_pin);
+    println!("ulp core run");
+
+    let data = (0x5000_0400) as *const u32;
     loop {
         print!("Current {:x}           \u{000d}", unsafe {
             data.read_volatile()
