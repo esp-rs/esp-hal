cc4e76dd1fa80f6c5c8dfc29d071bb5e8857a901
Kirill Mikhailov <62840029+playfulFence@users.noreply.github.com>
2026-03-19 20:01:31 +0100

    Enable a small hil-test for the CI (#5230)
    We need to test if probe-rs is alive on the runner

--- hil-test/src/bin/misc_non_drivers.rs
+++ hil-test/src/bin/misc_non_drivers.rs
@@ -19,7 +19,7 @@
 //! The goal of this test suite is to collect smaller, simpler test cases, to keep the overall
 //! number of test suites low(er).
 
-//% CHIPS: esp32 esp32c2 esp32c3 esp32c5 esp32c6 esp32h2 esp32s2 esp32s3
+//% CHIPS: esp32 esp32c2 esp32c3 esp32c5 esp32c6 esp32c61 esp32h2 esp32s2 esp32s3
 //% FEATURES: unstable
 
 #![no_std]
@@ -28,10 +28,12 @@
 #[path = "misc_non_drivers/clock_monitor.rs"]
 mod clock_monitor;
 
+#[cfg(not(esp32c61))]
 #[path = "misc_non_drivers/critical_section.rs"]
 mod critical_section;
 
 #[path = "misc_non_drivers/delay_async.rs"]
+#[cfg(not(esp32c61))] // will be removed after the clocktree is finished for the C61
 #[cfg(timergroup_driver_supported)]
 mod delay_async;
 
@@ -43,9 +45,11 @@ mod dma_macros;
 #[cfg(all(dma_driver_supported, dma_supports_mem2mem))]
 mod dma_mem2mem;
 
+#[cfg(not(esp32c61))]
 #[path = "misc_non_drivers/init.rs"]
 mod init;
 
+#[cfg(not(esp32c61))]
 #[path = "misc_non_drivers/simple.rs"]
 mod simple;
 
