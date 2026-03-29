74bfcad9b18a1034604c9b213c1ac814b65cecf0
Alex Dehart <105888845+Alex3404@users.noreply.github.com>
2026-03-28 21:27:34 -0600

    Capture update
    Cargo was updated to use the new updated pac that I needed to create. Cargo.toml can be reverted once the pac  is in the main branch.

--- esp-hal/src/mcpwm/capture.rs
+++ esp-hal/src/mcpwm/capture.rs
@@ -7,37 +7,29 @@
 //!
 //! Capture events can be triggered in 2 ways:
 //! During a falling and/or Rising edge of a preconfigured GPIO pin.
-//! Software triggered captures though the Capture::capture_time() function.
+//! Software triggered captures though the CaptureChannel::trigger() function.
 //!
 //! ## Configuration
 //! This module provides the flexiability of configuring any GPIO pin as an input
 //! for capturing the rising and/or falling edge of a signal. This module allows
-//! for the ability to trigger software captures, essentially capturing the current
-//! time in the capture clock.
-
-use core::marker::PhantomData;
-use core::panic;
+//! for the ability to trigger software captures.
+//! 
+//! Capture timer can be configured to sync with a PWM timer during specific events
+//! either when the PWM timer sync out event, or a sync in from an external GPIO pin.
+//! 
+use core::{marker::PhantomData, panic};
 
 use esp_sync::RawMutex;
 
 use super::PeripheralGuard;
-use crate::gpio::{InputSignal, interconnect::PeripheralInput};
-use crate::mcpwm::{PwmClockGuard, PwmPeripheral};
-
-#[derive(Copy, Clone, Debug, Default, PartialEq)]
-#[cfg_attr(feature = "defmt", derive(defmt::Format))]
-enum EdgeCaptureEvent {
-    FallingEdge,
-    RisingEdge,
-    EitherEdge,
-}
-
-#[derive(Copy, Clone, Debug, Default, PartialEq)]
-#[cfg_attr(feature = "defmt", derive(defmt::Format))]
-enum CapturedEdge {
-    FallingEdge,
-    RisingEdge,
-}
+pub use crate::pac::mcpwm0::{
+    cap_ch_cfg::CAP0_MODE as CaptureMode,
+    cap_status::CAP0_EDGE as CaptureEdge,
+};
+use crate::{
+    gpio::{InputSignal, interconnect::PeripheralInput},
+    mcpwm::{PwmClockGuard, PwmPeripheral},
+};
 
 static MUTEX: RawMutex = RawMutex::new();
 
@@ -49,6 +41,11 @@ pub struct CaptureTimer<'d, PWM> {
     _pwm_clock_guard: PwmClockGuard,
 }
 
+//! Capture timer inside MCPWM
+//! 
+//! ## Overview
+//! 
+//! This is a capture timer with the clock source of APB_CLK.
 impl<PWM: PwmPeripheral> CaptureTimer<'d, PWM> {
     pub(super) fn new(guard: PeripheralGuard) -> Self {
         Timer {
@@ -58,6 +55,7 @@ impl<PWM: PwmPeripheral> CaptureTimer<'d, PWM> {
         }
     }
 
+    /// Start the capture timer counting at APB_CLK
     pub fn start(&mut self) {
         // SAFETY:
         // We write to our MCPWM_CAP_TIMER_CFG_REG register
@@ -80,7 +78,10 @@ impl<PWM: PwmPeripheral> CaptureTimer<'d, PWM> {
 /// A MCPWM Capture Channel
 ///
 /// The MCPWM Capture Channel has the following functions:
-/// *
+/// * Enable/Disable capturing on this channel
+/// * Setting the capture input GPIO pin
+/// * Weather to trigger capture events on rising and/or falling edges
+/// * Read the last capture edge, and the last capture time 
 pub struct CaptureChannel<'d, const CHAN: usize, PWM> {
     phantom: PhantomData<&'d PWM>,
     _guard: PeripheralGuard,
@@ -96,26 +97,24 @@ impl<const CHAN: usize, PWM: PwmPeripheral> CaptureChannel<CHAN, PWM> {
         }
     }
 
-    /// Enables capturing on this channel
+    /// Enables this capture channel
     pub fn enable(&mut self) {
         // SAFETY:
         // We only write to our MCPWM_CAP_CHx_CFG_REG register
         let block = unsafe { &*PWM::block() };
-
-        block.cap_ch_cfg(CHAN).write(|w| w.en().set_bit());
+        block.cap_ch_cfg(CHAN).modify(|_, w| w.en().set_bit());
     }
 
-    /// Disable capturing on this channel
+    /// Disable this capture channel
     pub fn disable(&mut self) {
         // SAFETY:
         // We only write to our MCPWM_CAP_CHx_CFG_REG register
         let block = unsafe { &*PWM::block() };
-
-        block.cap_ch_cfg(CHAN).write(|w| w.en().clear_bit());
+        block.cap_ch_cfg(CHAN).modify(|_, w| w.en().clear_bit());
     }
 
     /// Set the capture signal (pin/high/low) for this channel
-    pub fn set_capture_signal(&mut self, source: impl PeripheralInput<'d>) {
+    pub fn set_signal_capture(&mut self, source: impl PeripheralInput<'d>) {
         let signal = PWM::capture_input_signal::<CHAN>();
 
         if signal as usize <= property!("gpio.input_signal_max") {
@@ -127,109 +126,79 @@ impl<const CHAN: usize, PWM: PwmPeripheral> CaptureChannel<CHAN, PWM> {
         }
     }
 
+    /// Sets the capture prescaler for the channel.
+    /// Although the function of the prescaler on capture channels
+    /// is better though of as a capture rate.
+    ///
+    /// The prescaler determines how often a capture event is generated
+    /// from incoming edges. Instead of capturing every valid edge,
+    /// the hardware will generate a capture event once every `capture_rate`
+    /// edges.
+    ///
+    /// The edges counted are those selected by the channel's edge configuration
+    /// (rising, falling, or both). For example:
+    ///
+    /// - If configured for rising edges only, captures occur every Nth rising edge
+    /// - If configured for both edges, captures occur every Nth edge (rising + falling combined)
+    ///
+    /// This is useful for reducing interrupt load or effectively averaging
+    /// measurements over multiple signal periods.
+    /// 
+    /// Passing in a value of capture_rate = 0 into this function will set the capture_rate value of 1
+    pub fn set_capture_rate(&mut self, capture_rate : u8) {
+        // SAFETY:
+        // We only write to our MCPWM_CAP_CHx_CFG_REG register
+        let block = unsafe { &*PWM::block() };
+        unsafe {
+            block.cap_ch_cfg(CHAN).modify(|_, w| w.prescale().bits(capture_rate));
+        }
+    }
+
     /// Sets the capture channel to listen to captures on specific edge events
     /// This channel can listen to Falling, and/or Rising edges on any of the GPIO pins.
-    pub fn listen(&mut self, edge_capture_event: EdgeCaptureEvent) {
+    pub fn listen(&mut self, capture_mode: CaptureMode) {
         // SAFETY:
         // We write to our MCPWM_CAP_CHx_CFG_REG register
-        // And we are modifying a shared peripheral interrupt enable register
+        // We are modifying a shared peripheral interrupt enable register
         let block = unsafe { &*PWM::block() };
-
-        // Maps the edge capture event to the proper bits in the capture
-        // configuration register.
-        //
-        // In the MCPWM_CAP_CHx_CFG_REG register within the
-        // the MCPWM_CAP0_MODE field of the register Bits[1..=2] discribe
-        // the falling and/or rising edge capture
-        //
-        // The 2 bits within the MCPWM_CAPx_MODE field are as of the following
-        // Bits[0] -> Selects weather or not Falling Edges should be captured
-        // Bits[1] -> Selects weather or not Rising Edges should be captured
-        //
-        // Has been checked for the following chips: ESP32s3
-        //
-        // Either of them can be on or off allowing for the 4 configurations seen below.
-
-        // Note: would be nice to be able to set this via an enum like the PCNT register block :)
-        match edge_capture_event {
-            EdgeCaptureEvent::FallingEdge => unsafe {
-                // Bit0 set -> Falling edges
-                block.cap_ch_cfg(CHAN).write(|w| w.mode().bits(0b01));
-            },
-            EdgeCaptureEvent::RisingEdge => unsafe {
-                // Bit1 set -> Rising edges
-                block.cap_ch_cfg(CHAN).write(|w| w.mode().bits(0b10));
-            },
-            EdgeCaptureEvent::EitherEdge => unsafe {
-                // Bit0 and Bit1 set -> Either edge
-                block.cap_ch_cfg(CHAN).write(|w| w.mode().bits(0b11));
-            },
-        };
+        block
+            .cap_ch_cfg(CHAN)
+            .write(|w| w.mode().variant(capture_mode));
 
         // Enable the capture interrupt
-        MUTEX.lock(|| {
-            // Note: would be nice to be able to get interrupts from channel number
-            match CHAN {
-                0 => block.int_ena().modify(|r, w| w.cap0().set_bit()),
-                1 => block.int_ena().modify(|r, w| w.cap1().set_bit()),
-                2 => block.int_ena().modify(|r, w| w.cap2().set_bit()),
-                _ => unreachable!(),
-            };
-        });
+        block.int_ena().modify(|_, w| w.cap(CHAN as u8).set_bit());
     }
 
     /// Stops listening to events on this channel
     pub fn unlisten(&mut self) {
         // SAFTEY:
-        // We are accessing a SHARED peripheral interrupt enable register
-        let _ = self._guard;
+        // We are writing to a SHARED peripheral interrupt enable register
+        // Tldr lowk don't know if I should use a mutex or not??
         let block = unsafe { &*PWM::block() };
 
         // We can unlisten from events simply by disabling the interrupt
-        MUTEX.lock(|| {
-            // Note: would be nice to be able to get interrupts from channel number
-            match CHAN {
-                0 => block.int_ena().modify(|r, w| w.cap0().clear_bit()),
-                1 => block.int_ena().modify(|r, w| w.cap1().clear_bit()),
-                2 => block.int_ena().modify(|r, w| w.cap2().clear_bit()),
-                _ => unreachable!(),
-            };
-        });
+        block.int_ena().modify(|_, w| w.cap(CHAN as u8).clear_bit());
     }
 
     pub fn is_interupt_set<const CHAN: usize>(&mut self) -> bool {
+        // SAFTEY:
+        // We only read from our MCPWM_INT_ST_REG register
         let block = unsafe { &*PWM::block() };
+        block.int_st().read().cap(CHAN as u8).bit()
+    }
 
-        // Note: would be nice to be able to get interrupts from channel number
-        match CHAN {
-            0 => block.int_raw().read().cap0().bit(),
-            1 => block.int_raw().read().cap1().bit(),
-            2 => block.int_raw().read().cap2().bit(),
-            _ => unreachable!(),
-        }
+    pub fn last_captured_time<const CHAN: usize>(&mut self) -> u32 {
+        // SAFTEY:
+        // We only read from our MCPWM_INT_ST_REG register
+        let block = unsafe { &*PWM::block() };
+        block.cap_ch(CHAN).read().value().bits()
     }
 
     /// Gets the last captured edge on this channel
-    pub fn get_captured_edge<const CHAN: usize>(&mut self) -> CapturedEdge {
+    pub fn get_captured_edge<const CHAN: usize>(&mut self) -> CaptureEdge {
         // SAFETY:
         // We only read from our MCPWM_CAP_STATUS_REG register
         let block = unsafe { &*PWM::block() };
-
-        // Read the last edge status from the either of the 3 channels
-        // the bit that we read will indicate if it was a faling edge.
-
-        // Note: would be nice to be able to get the capture status from channel number
-        let is_falling_edge = match CHAN {
-            0 => block.cap_status().read().cap0_edge().bit(),
-            1 => block.cap_status().read().cap1_edge().bit(),
-            2 => block.cap_status().read().cap2_edge().bit(),
-            _ => unreachable!(),
-        };
-
-        if is_falling_edge {
-            CapturedEdge::FallingEdge
-        } else {
-            CapturedEdge::RisingEdge
-        }
+        block.cap_status().read().cap_edge(CHAN as u8).variant()
     }
 }
