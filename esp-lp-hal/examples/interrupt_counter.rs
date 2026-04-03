2c31ed096c2a6dcff63c606a15c4e6927160d3ba
Leigh Oliver <leighcharlesoliver@gmail.com>
2026-04-03 17:56:24 +1100

    Update interrupt_counter.rs to align with ESP-HAL example

--- esp-lp-hal/examples/interrupt_counter.rs
+++ esp-lp-hal/examples/interrupt_counter.rs
@@ -1,21 +1,22 @@
 //! ULP interrupt-based counter example.
-//! Increments a 32 bit counter value at a known point in memory,
-//! whenever the ULP program is run. If GPIO0 is pressed, reset the counter.
-
-//% CHIPS: esp32s2, esp32s3
+//! Increments a 32 bit counter value at a known point in memory, whenever the ULP program is run.
+//! If GPIO0 is pressed, resets the counter.
 
 #![no_std]
 #![no_main]
+
 extern crate panic_halt;
 
+use core::cell::RefCell;
+
+use critical_section::Mutex;
 use esp_lp_hal::{
-    gpio::{Event, Input, Io, gpio_interrupt_clear, gpio_interrupt_status},
+    gpio::{Event, Input, Io},
     interrupt,
     pac::Peripherals,
     prelude::*,
 };
 
-// Shared memory address.
 const ADDRESS: u32 = 0x1000;
 
 fn increment_counter() {
@@ -32,22 +33,32 @@ fn reset_counter() {
     }
 }
 
+// NOTE: Normally this would contain Option<Input<0>>,
+//       but for some reason the ULP core is crashing when
+//       .unwrap() is used on anything...
+static BUTTON: Mutex<RefCell<Input<0>>> = Mutex::new(RefCell::new(Input::<0>::new()));
+
 #[entry]
-fn main(mut boot_button: Input<0>) {
+fn main(mut button: Input<0>) {
     let peripherals = Peripherals::take();
     let mut io = Io::new(peripherals.RTC_IO);
     io.set_interrupt_handler(gpio_interrupt_handler);
 
-    boot_button.listen(Event::FallingEdge);
-
     increment_counter();
+
+    critical_section::with(|cs| {
+        button.listen(Event::FallingEdge);
+        *BUTTON.borrow_ref_mut(cs) = button;
+    });
 }
 
 #[handler]
 fn gpio_interrupt_handler() {
-    let status = gpio_interrupt_status();
-    if status & (0b1) != 0 {
+    if critical_section::with(|cs| BUTTON.borrow_ref_mut(cs).is_interrupt_set()) {
+        // The button was the source of the interrupt
         reset_counter();
     }
-    gpio_interrupt_clear(status);
+
+    // Clear the interrupt
+    critical_section::with(|cs| BUTTON.borrow_ref_mut(cs).clear_interrupt());
 }
