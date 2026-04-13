5672c72110db83e33b7d852d1f4be875b03aedac
Leigh Oliver <leighcharlesoliver@gmail.com>
2026-04-14 09:35:37 +1000

    Update interrupt_counter.rs

--- esp-lp-hal/examples/interrupt_counter.rs
+++ esp-lp-hal/examples/interrupt_counter.rs
@@ -2,7 +2,7 @@
 //! Increments a 32 bit counter value at a known point in memory, whenever the ULP program is run.
 //! If GPIO0 is pressed, resets the counter.
 
-//% CHIPS: esp32s3,esp32s2
+//% CHIPS: esp32s3 esp32s2
 
 #![no_std]
 #![no_main]
