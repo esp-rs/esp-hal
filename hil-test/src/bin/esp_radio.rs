3fb896b22d9271e6866fce350b2af0315d14db84
Dániel Buga <bugadani@gmail.com>
2026-05-05 17:25:35 +0200

    Enable esp-rtos tests

--- hil-test/src/bin/esp_radio.rs
+++ hil-test/src/bin/esp_radio.rs
@@ -1,10 +1,11 @@
 //% CHIPS(no_wifi): esp32h2
 //% CHIPS(no_ble): esp32s2
+//% CHIPS(no_radio): esp32p4
 //% CHIPS(has_wifi_ble): esp32 esp32c2 esp32c3 esp32c6 esp32s3 esp32c5
 //% CHIPS(stable_wifi): esp32 esp32c2 esp32c3 esp32c5 esp32c6 esp32s2 esp32s3
 
 //% FEATURES: unstable esp-alloc embassy
-// //% FEATURES(no_radio): rtos-radio-driver TODO: restore for P4
+//% FEATURES(no_radio): rtos-radio-driver
 //% FEATURES(no_ble): esp-radio/wifi esp-radio esp-radio-unstable
 //% FEATURES(no_wifi): esp-radio/ble esp-radio esp-radio-unstable trouble-host
 //% FEATURES(has_wifi_ble): esp-radio/wifi esp-radio/ble esp-radio/coex esp-radio-unstable
@@ -26,7 +27,7 @@ extern crate alloc;
 
 fn init_heap() {
     cfg_if::cfg_if! {
-        if #[cfg(any(esp32, esp32s2, esp32s3, esp32c3, esp32c2, esp32c5, esp32c6, esp32c61))] {
+        if #[cfg(any(esp32, esp32s2, esp32s3, esp32c3, esp32c2, esp32c5, esp32c6, esp32c61, esp32p4))] {
             use esp_hal::ram;
             esp_alloc::heap_allocator!(#[ram(reclaimed)] size: 64 * 1024);
             esp_alloc::heap_allocator!(size: 48 * 1024);
