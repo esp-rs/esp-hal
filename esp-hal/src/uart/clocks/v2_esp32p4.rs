166335aa722d8add519d3cce271eb5b41b80b3e0
Dániel Buga <bugadani@gmail.com>
2026-07-07 15:47:31 +0200

    Actually implement UART clock config for P4

--- esp-hal/src/uart/clocks/v2_esp32p4.rs
+++ esp-hal/src/uart/clocks/v2_esp32p4.rs
@@ -1,25 +1,77 @@
-use crate::clock::ll::{
-    ClockTree,
-    UartBaudRateGeneratorConfig,
-    UartFunctionClockConfig,
-    UartInstance,
+use crate::{
+    clock::ll::{
+        ClockTree,
+        UartBaudRateGeneratorConfig,
+        UartFunctionClockConfig,
+        UartFunctionClockSclk,
+        UartInstance,
+    },
+    peripherals::HP_SYS_CLKRST,
 };
 
 // Per-instance clock impl for UART (called on UartInstance enum)
 
 impl UartInstance {
-    pub(crate) fn enable_function_clock_impl(self, _clocks: &mut ClockTree, _en: bool) {
-        // UART function clock enable is handled by peripheral clock gates in system.rs
+    pub(crate) fn enable_function_clock_impl(self, _clocks: &mut ClockTree, en: bool) {
+        let regs = HP_SYS_CLKRST::regs();
+        match self {
+            UartInstance::Uart0 => {
+                regs.peri_clk_ctrl110()
+                    .modify(|_, w| w.uart0_clk_en().bit(en));
+            }
+            UartInstance::Uart1 => {
+                regs.peri_clk_ctrl111()
+                    .modify(|_, w| w.uart1_clk_en().bit(en));
+            }
+            UartInstance::Uart2 => {
+                regs.peri_clk_ctrl112()
+                    .modify(|_, w| w.uart2_clk_en().bit(en));
+            }
+            UartInstance::Uart3 => {
+                regs.peri_clk_ctrl113()
+                    .modify(|_, w| w.uart3_clk_en().bit(en));
+            }
+            UartInstance::Uart4 => {
+                regs.peri_clk_ctrl114()
+                    .modify(|_, w| w.uart4_clk_en().bit(en));
+            }
+        }
     }
 
     pub(crate) fn configure_function_clock_impl(
         self,
         _clocks: &mut ClockTree,
         _old_config: Option<UartFunctionClockConfig>,
-        _new_config: UartFunctionClockConfig,
+        new_config: UartFunctionClockConfig,
     ) {
-        // TODO: Configure UART clock source selection
-        // HP_SYS_CLKRST PERI_CLK_CTRL110-114 for UART0-4
+        let sel = match new_config.sclk() {
+            UartFunctionClockSclk::Xtal => 0,
+            UartFunctionClockSclk::PllF80m => 2,
+            UartFunctionClockSclk::RcFast => 1,
+        };
+        let regs = HP_SYS_CLKRST::regs();
+        match self {
+            UartInstance::Uart0 => {
+                regs.peri_clk_ctrl110()
+                    .modify(|_, w| unsafe { w.uart0_clk_src_sel().bits(sel) });
+            }
+            UartInstance::Uart1 => {
+                regs.peri_clk_ctrl111()
+                    .modify(|_, w| unsafe { w.uart1_clk_src_sel().bits(sel) });
+            }
+            UartInstance::Uart2 => {
+                regs.peri_clk_ctrl112()
+                    .modify(|_, w| unsafe { w.uart2_clk_src_sel().bits(sel) });
+            }
+            UartInstance::Uart3 => {
+                regs.peri_clk_ctrl113()
+                    .modify(|_, w| unsafe { w.uart3_clk_src_sel().bits(sel) });
+            }
+            UartInstance::Uart4 => {
+                regs.peri_clk_ctrl114()
+                    .modify(|_, w| unsafe { w.uart4_clk_src_sel().bits(sel) });
+            }
+        }
     }
 
     pub(crate) fn enable_baud_rate_generator_impl(self, _clocks: &mut ClockTree, _en: bool) {
@@ -30,8 +82,18 @@ impl UartInstance {
         self,
         _clocks: &mut ClockTree,
         _old_config: Option<UartBaudRateGeneratorConfig>,
-        _new_config: UartBaudRateGeneratorConfig,
+        new_config: UartBaudRateGeneratorConfig,
     ) {
-        // Baud rate is configured directly in UART registers, not here
+        let regs = match self {
+            UartInstance::Uart0 => crate::peripherals::UART0::regs(),
+            UartInstance::Uart1 => crate::peripherals::UART1::regs(),
+            UartInstance::Uart2 => crate::peripherals::UART2::regs(),
+            UartInstance::Uart3 => crate::peripherals::UART3::regs(),
+            UartInstance::Uart4 => crate::peripherals::UART4::regs(),
+        };
+        regs.clkdiv().write(|w| unsafe {
+            w.clkdiv().bits(new_config.integral() as _);
+            w.clkdiv_frag().bits(new_config.fractional() as _)
+        });
     }
 }
