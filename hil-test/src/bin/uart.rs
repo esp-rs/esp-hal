149b4660bd278861adeac576332eaf4cc2731c50
Sergio Gasquez <sergio.gasquez@gmail.com>
2026-07-23 09:45:18 +0200

    feat: enable UART support for ESP32-S31

--- hil-test/src/bin/uart.rs
+++ hil-test/src/bin/uart.rs
@@ -30,9 +30,11 @@ mod tests {
 
     #[init]
     fn init() -> Context {
-        let peripherals = esp_hal::init(
-            esp_hal::Config::default().with_cpu_clock(esp_hal::clock::CpuClock::max()),
-        );
+        let config = cfg_select! {
+            esp32s31 => esp_hal::Config::default(),
+            _ => esp_hal::Config::default().with_cpu_clock(esp_hal::clock::CpuClock::max()),
+        };
+        let peripherals = esp_hal::init(config);
 
         let (rx, tx) = hil_test::common_test_pins!(peripherals);
         let rts = hil_test::unconnected_pin!(peripherals);
@@ -171,7 +173,7 @@ mod tests {
 
         let fastest_clock_source = cfg_select! {
             esp32c2 => ClockSource::PllF40m,
-            any(esp32c5, esp32c6, esp32c61, esp32p4) => ClockSource::PllF80m,
+            any(esp32c5, esp32c6, esp32c61, esp32p4, esp32s31) => ClockSource::PllF80m,
             esp32h2 => ClockSource::PllF48m,
             _ => ClockSource::Apb,
         };
