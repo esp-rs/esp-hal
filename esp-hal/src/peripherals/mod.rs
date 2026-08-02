203b6e5c3b444e8f2ffbf42a8e945bcea3693052
Juraj Sadel <juraj.sadel@espressif.com>
2026-08-02 11:54:20 +0200

    fix(spi): take register block pointer via ptr() instead of regs()

--- esp-hal/src/peripherals/mod.rs
+++ esp-hal/src/peripherals/mod.rs
@@ -158,7 +158,7 @@ macro_rules! create_peripheral {
             #[doc = r"Return a reference to the register block"]
             #[inline(always)]
             #[instability::unstable]
-            pub const fn regs<'a>() -> &'a <pac::$base as core::ops::Deref>::Target {
+            pub fn regs<'a>() -> &'a <pac::$base as core::ops::Deref>::Target {
                 unsafe { &*Self::PTR }
             }
 
