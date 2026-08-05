daf591c0edc38c9fe9ddfaf41400ab16002cfcac
Dániel Buga <bugadani@gmail.com>
2026-08-05 15:24:51 +0200

    Remove bogus USB wakeup sources

--- esp-metadata-generated/src/_generated_esp32c61.rs
+++ esp-metadata-generated/src/_generated_esp32c61.rs
@@ -819,10 +819,8 @@ macro_rules! for_each_wakeup_source {
         _for_each_inner_wakeup_source!((Uart1, 7)); _for_each_inner_wakeup_source!((Sdio,
         8)); _for_each_inner_wakeup_source!((Bt, 10));
         _for_each_inner_wakeup_source!((LpCore, 11));
-        _for_each_inner_wakeup_source!((Usb, 14));
         _for_each_inner_wakeup_source!((all(Ext1, 1), (Gpio, 2), (WifiBeacon, 3), (Timer,
-        4), (Wifi, 5), (Uart0, 6), (Uart1, 7), (Sdio, 8), (Bt, 10), (LpCore, 11), (Usb,
-        14)));
+        4), (Wifi, 5), (Uart0, 6), (Uart1, 7), (Sdio, 8), (Bt, 10), (LpCore, 11)));
     };
 }
 #[macro_export]
