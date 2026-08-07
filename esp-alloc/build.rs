8787791db8e58b2adbba3f3980972d59443fe901
Dániel Buga <bugadani@gmail.com>
2026-08-07 14:34:49 +0200

    Add DMA-compatible allocators

--- esp-alloc/build.rs
+++ esp-alloc/build.rs
@@ -1,9 +1,19 @@
+use std::error::Error;
+
 use esp_config::generate_config_from_yaml_definition;
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
+    // Ensure that exactly one chip has been specified:
+    let chip = esp_metadata_generated::Chip::from_cargo_feature()?;
+
+    // Define all necessary configuration symbols for the configured device:
+    chip.define_cfgs();
+
     // emit config
     println!("cargo:rerun-if-changed=./esp_config.yml");
     let cfg_yaml = std::fs::read_to_string("./esp_config.yml")
         .expect("Failed to read esp_config.yml for esp-alloc");
     generate_config_from_yaml_definition(&cfg_yaml, true, true, None).unwrap();
+
+    Ok(())
 }
