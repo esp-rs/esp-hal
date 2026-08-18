eeba2ad01cfeb22a9e221d99a4e6606210f2900e
Dániel Buga <bugadani@gmail.com>
2026-08-18 08:51:57 +0000

    Update fugit and use Monotonic Instant kind (#6141)

--- esp-hal/src/time.rs
+++ esp-hal/src/time.rs
@@ -7,7 +7,7 @@
 use core::fmt::{Debug, Display, Formatter, Result as FmtResult};
 
 type InnerRate = fugit::Rate<u32, 1, 1>;
-type InnerInstant = fugit::Instant<u64, 1, 1_000_000>;
+type InnerInstant = fugit::Instant<u64, 1, 1_000_000, fugit::kind::Monotonic>;
 type InnerDuration = fugit::Duration<u64, 1, 1_000_000>;
 
 /// Represents a rate or frequency of events.
@@ -328,8 +328,7 @@ impl core::ops::Sub for Instant {
 
     #[inline]
     fn sub(self, rhs: Self) -> Self::Output {
-        // Avoid "Sub failed! Other > self" panics
-        Duration::from_micros(self.0.ticks().wrapping_sub(rhs.0.ticks()))
+        Duration(self.0 - rhs.0)
     }
 }
 
@@ -409,7 +408,7 @@ impl Duration {
     /// ```
     #[inline]
     pub const fn from_micros(val: u64) -> Self {
-        Self(InnerDuration::micros(val))
+        Self(InnerDuration::from_micros(val))
     }
 
     #[procmacros::doc_replace]
@@ -425,7 +424,7 @@ impl Duration {
     /// ```
     #[inline]
     pub const fn from_millis(val: u64) -> Self {
-        Self(InnerDuration::millis(val))
+        Self(InnerDuration::from_millis(val))
     }
 
     #[procmacros::doc_replace]
@@ -441,7 +440,7 @@ impl Duration {
     /// ```
     #[inline]
     pub const fn from_secs(val: u64) -> Self {
-        Self(InnerDuration::secs(val))
+        Self(InnerDuration::from_secs(val))
     }
 
     #[procmacros::doc_replace]
@@ -457,7 +456,7 @@ impl Duration {
     /// ```
     #[inline]
     pub const fn from_minutes(val: u64) -> Self {
-        Self(InnerDuration::minutes(val))
+        Self(InnerDuration::from_minutes(val))
     }
 
     #[procmacros::doc_replace]
@@ -473,7 +472,7 @@ impl Duration {
     /// ```
     #[inline]
     pub const fn from_hours(val: u64) -> Self {
-        Self(InnerDuration::hours(val))
+        Self(InnerDuration::from_hours(val))
     }
 
     delegate::delegate! {
@@ -491,7 +490,6 @@ impl Duration {
             /// let micros = duration.as_micros();
             /// # {after_snippet}
             /// ```
-            #[call(to_micros)]
             pub const fn as_micros(&self) -> u64;
 
             #[procmacros::doc_replace]
@@ -506,7 +504,6 @@ impl Duration {
             /// let millis = duration.as_millis();
             /// # {after_snippet}
             /// ```
-            #[call(to_millis)]
             pub const fn as_millis(&self) -> u64;
 
             #[procmacros::doc_replace]
@@ -521,7 +518,6 @@ impl Duration {
             /// let secs = duration.as_secs();
             /// # {after_snippet}
             /// ```
-            #[call(to_secs)]
             pub const fn as_secs(&self) -> u64;
 
             #[procmacros::doc_replace]
@@ -536,7 +532,6 @@ impl Duration {
             /// let minutes = duration.as_minutes();
             /// # {after_snippet}
             /// ```
-            #[call(to_minutes)]
             pub const fn as_minutes(&self) -> u64;
 
             #[procmacros::doc_replace]
@@ -551,7 +546,6 @@ impl Duration {
             /// let hours = duration.as_hours();
             /// # {after_snippet}
             /// ```
-            #[call(to_hours)]
             pub const fn as_hours(&self) -> u64;
         }
     }
