038492f034b44fe3113ee31ce7c768391b422282
snek <the@snek.dev>
2026-09-08 14:18:38 +0200

    review

--- esp-radio/src/esp_now/mod.rs
+++ esp-radio/src/esp_now/mod.rs
@@ -421,15 +421,9 @@ impl EspNowManager<'_> {
 
     /// Get the version of ESP-NOW.
     ///
-    /// Currently, ESP-NOW supports two versions: v1.0 and v2.0. The maximum packet length supported
-    /// by v2.0 devices is 1470 (ESP_NOW_MAX_DATA_LEN_V2) bytes, while the maximum packet length
-    /// supported by v1.0 devices is 250 (ESP_NOW_MAX_DATA_LEN_V1) bytes. The v2.0 devices are
-    /// capable of receiving packets from both v2.0 and v1.0 devices. In contrast, v1.0 devices
-    /// can only receive packets from other v1.0 devices.
-    ///
-    /// However, v1.0 devices can receive v2.0 packets if the packet length is less than or equal to
-    /// 250. For packets exceeding this length, the v1.0 devices will either truncate the data to the
-    /// first 250 bytes or discard the packet entirely.
+    /// ESP-NOW supports two versions: v1.0 and v2.0. v1.0 and v2.0 are capable of talking to each
+    /// other, but v1.0 devices may truncate or discard v2.0 messages that exceed the v1.0 maximum
+    /// data length ([`ESP_NOW_MAX_DATA_LEN_V1`]).
     #[instability::unstable]
     pub fn version(&self) -> Result<u32, EspNowError> {
         let mut version = 0u32;
