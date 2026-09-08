ffb360a77c72a390afa0234cf47ef3912d7d916a
Tommaso Clini <tommasoclini@gmail.com>
2026-09-08 12:17:26 +0200

    implement Default for QueueStorage

--- esp-radio/src/esp_now/mod.rs
+++ esp-radio/src/esp_now/mod.rs
@@ -55,6 +55,12 @@ pub enum QueueStorage {
     Boxed(Box<[UnsafeCell<u8>]>),
 }
 
+impl Default for QueueStorage {
+    fn default() -> Self {
+        Self::boxed(16384)
+    }
+}
+
 impl QueueStorage {
     /// Boxed variant
     #[instability::unstable]
@@ -154,25 +160,25 @@ macro_rules! check_error_expect {
 #[instability::unstable]
 pub enum Error {
     /// ESP-NOW is not initialized.
-    NotInitialized    = 12389,
+    NotInitialized = 12389,
 
     /// Invalid argument.
-    InvalidArgument   = 12390,
+    InvalidArgument = 12390,
 
     /// Indicates that there was insufficient memory to complete the operation.
-    OutOfMemory       = 12391,
+    OutOfMemory = 12391,
 
     /// ESP-NOW peer list is full.
-    PeerListFull      = 12392,
+    PeerListFull = 12392,
 
     /// ESP-NOW peer is not found.
-    NotFound          = 12393,
+    NotFound = 12393,
 
     /// Internal error.
-    Internal          = 12394,
+    Internal = 12394,
 
     /// ESP-NOW peer already exists.
-    PeerExists        = 12395,
+    PeerExists = 12395,
 
     /// The Wi-Fi interface used for ESP-NOW doesn't match the expected one for the peer.
     InterfaceMismatch = 12396,
@@ -243,79 +249,79 @@ pub struct PeerCount {
 #[instability::unstable]
 pub enum WifiPhyRate {
     /// 1 Mbps with long preamble
-    Rate1mL      = wifi_phy_rate_t_WIFI_PHY_RATE_1M_L,
+    Rate1mL = wifi_phy_rate_t_WIFI_PHY_RATE_1M_L,
     /// 2 Mbps with long preamble
-    Rate2m       = wifi_phy_rate_t_WIFI_PHY_RATE_2M_L,
+    Rate2m = wifi_phy_rate_t_WIFI_PHY_RATE_2M_L,
     /// 5.5 Mbps with long preamble
-    Rate5mL      = wifi_phy_rate_t_WIFI_PHY_RATE_5M_L,
+    Rate5mL = wifi_phy_rate_t_WIFI_PHY_RATE_5M_L,
     /// 11 Mbps with long preamble
-    Rate11mL     = wifi_phy_rate_t_WIFI_PHY_RATE_11M_L,
+    Rate11mL = wifi_phy_rate_t_WIFI_PHY_RATE_11M_L,
     /// 2 Mbps with short preamble
-    Rate2mS      = wifi_phy_rate_t_WIFI_PHY_RATE_2M_S,
+    Rate2mS = wifi_phy_rate_t_WIFI_PHY_RATE_2M_S,
     /// 5.5 Mbps with short preamble
-    Rate5mS      = wifi_phy_rate_t_WIFI_PHY_RATE_5M_S,
+    Rate5mS = wifi_phy_rate_t_WIFI_PHY_RATE_5M_S,
     /// 11 Mbps with short preamble
-    Rate11mS     = wifi_phy_rate_t_WIFI_PHY_RATE_11M_S,
+    Rate11mS = wifi_phy_rate_t_WIFI_PHY_RATE_11M_S,
     /// 48 Mbps
-    Rate48m      = wifi_phy_rate_t_WIFI_PHY_RATE_48M,
+    Rate48m = wifi_phy_rate_t_WIFI_PHY_RATE_48M,
     /// 24 Mbps
-    Rate24m      = wifi_phy_rate_t_WIFI_PHY_RATE_24M,
+    Rate24m = wifi_phy_rate_t_WIFI_PHY_RATE_24M,
     /// 12 Mbps
-    Rate12m      = wifi_phy_rate_t_WIFI_PHY_RATE_12M,
+    Rate12m = wifi_phy_rate_t_WIFI_PHY_RATE_12M,
     /// 6 Mbps
-    Rate6m       = wifi_phy_rate_t_WIFI_PHY_RATE_6M,
+    Rate6m = wifi_phy_rate_t_WIFI_PHY_RATE_6M,
     /// 54 Mbps
-    Rate54m      = wifi_phy_rate_t_WIFI_PHY_RATE_54M,
+    Rate54m = wifi_phy_rate_t_WIFI_PHY_RATE_54M,
     /// 36 Mbps
-    Rate36m      = wifi_phy_rate_t_WIFI_PHY_RATE_36M,
+    Rate36m = wifi_phy_rate_t_WIFI_PHY_RATE_36M,
     /// 18 Mbps
-    Rate18m      = wifi_phy_rate_t_WIFI_PHY_RATE_18M,
+    Rate18m = wifi_phy_rate_t_WIFI_PHY_RATE_18M,
     /// 9 Mbps
-    Rate9m       = wifi_phy_rate_t_WIFI_PHY_RATE_9M,
+    Rate9m = wifi_phy_rate_t_WIFI_PHY_RATE_9M,
     /// MCS0 with long GI, 6.5 Mbps for 20MHz, 13.5 Mbps for 40MHz
-    RateMcs0Lgi  = wifi_phy_rate_t_WIFI_PHY_RATE_MCS0_LGI,
+    RateMcs0Lgi = wifi_phy_rate_t_WIFI_PHY_RATE_MCS0_LGI,
     /// MCS1 with long GI, 13 Mbps for 20MHz, 27 Mbps for 40MHz
-    RateMcs1Lgi  = wifi_phy_rate_t_WIFI_PHY_RATE_MCS1_LGI,
+    RateMcs1Lgi = wifi_phy_rate_t_WIFI_PHY_RATE_MCS1_LGI,
     /// MCS2 with long GI, 19.5 Mbps for 20MHz, 40.5 Mbps for 40MHz
-    RateMcs2Lgi  = wifi_phy_rate_t_WIFI_PHY_RATE_MCS2_LGI,
+    RateMcs2Lgi = wifi_phy_rate_t_WIFI_PHY_RATE_MCS2_LGI,
     /// MCS3 with long GI, 26 Mbps for 20MHz, 54 Mbps for 40MHz
-    RateMcs3Lgi  = wifi_phy_rate_t_WIFI_PHY_RATE_MCS3_LGI,
+    RateMcs3Lgi = wifi_phy_rate_t_WIFI_PHY_RATE_MCS3_LGI,
     /// MCS4 with long GI, 39 Mbps for 20MHz, 81 Mbps for 40MHz
-    RateMcs4Lgi  = wifi_phy_rate_t_WIFI_PHY_RATE_MCS4_LGI,
+    RateMcs4Lgi = wifi_phy_rate_t_WIFI_PHY_RATE_MCS4_LGI,
     /// MCS5 with long GI, 52 Mbps for 20MHz, 108 Mbps for 40MHz
-    RateMcs5Lgi  = wifi_phy_rate_t_WIFI_PHY_RATE_MCS5_LGI,
+    RateMcs5Lgi = wifi_phy_rate_t_WIFI_PHY_RATE_MCS5_LGI,
     /// MCS6 with long GI, 58.5 Mbps for 20MHz, 121.5 Mbps for 40MHz
-    RateMcs6Lgi  = wifi_phy_rate_t_WIFI_PHY_RATE_MCS6_LGI,
+    RateMcs6Lgi = wifi_phy_rate_t_WIFI_PHY_RATE_MCS6_LGI,
     /// MCS7 with long GI, 65 Mbps for 20MHz, 135 Mbps for 40MHz
-    RateMcs7Lgi  = wifi_phy_rate_t_WIFI_PHY_RATE_MCS7_LGI,
+    RateMcs7Lgi = wifi_phy_rate_t_WIFI_PHY_RATE_MCS7_LGI,
     /// MCS8 with long GI
     #[cfg(not(wifi_mac_version = "1"))]
-    RateMcs8Lgi  = wifi_phy_rate_t_WIFI_PHY_RATE_MCS8_LGI,
+    RateMcs8Lgi = wifi_phy_rate_t_WIFI_PHY_RATE_MCS8_LGI,
     /// MCS9 with long GI
     #[cfg(not(wifi_mac_version = "1"))]
-    RateMcs9Lgi  = wifi_phy_rate_t_WIFI_PHY_RATE_MCS9_LGI,
+    RateMcs9Lgi = wifi_phy_rate_t_WIFI_PHY_RATE_MCS9_LGI,
     /// MCS0 with short GI, 7.2 Mbps for 20MHz, 15 Mbps for 40MHz
-    RateMcs0Sgi  = wifi_phy_rate_t_WIFI_PHY_RATE_MCS0_SGI,
+    RateMcs0Sgi = wifi_phy_rate_t_WIFI_PHY_RATE_MCS0_SGI,
     /// MCS1 with short GI, 14.4 Mbps for 20MHz, 30 Mbps for 40MHz
-    RateMcs1Sgi  = wifi_phy_rate_t_WIFI_PHY_RATE_MCS1_SGI,
+    RateMcs1Sgi = wifi_phy_rate_t_WIFI_PHY_RATE_MCS1_SGI,
     /// MCS2 with short GI, 21.7 Mbps for 20MHz, 45 Mbps for 40MHz
-    RateMcs2Sgi  = wifi_phy_rate_t_WIFI_PHY_RATE_MCS2_SGI,
+    RateMcs2Sgi = wifi_phy_rate_t_WIFI_PHY_RATE_MCS2_SGI,
     /// MCS3 with short GI, 28.9 Mbps for 20MHz, 60 Mbps for 40MHz
-    RateMcs3Sgi  = wifi_phy_rate_t_WIFI_PHY_RATE_MCS3_SGI,
+    RateMcs3Sgi = wifi_phy_rate_t_WIFI_PHY_RATE_MCS3_SGI,
     /// MCS4 with short GI, 43.3 Mbps for 20MHz, 90 Mbps for 40MHz
-    RateMcs4Sgi  = wifi_phy_rate_t_WIFI_PHY_RATE_MCS4_SGI,
+    RateMcs4Sgi = wifi_phy_rate_t_WIFI_PHY_RATE_MCS4_SGI,
     /// MCS5 with short GI, 57.8 Mbps for 20MHz, 120 Mbps for 40MHz
-    RateMcs5Sgi  = wifi_phy_rate_t_WIFI_PHY_RATE_MCS5_SGI,
+    RateMcs5Sgi = wifi_phy_rate_t_WIFI_PHY_RATE_MCS5_SGI,
     /// MCS6 with short GI, 65 Mbps for 20MHz, 135 Mbps for 40MHz
-    RateMcs6Sgi  = wifi_phy_rate_t_WIFI_PHY_RATE_MCS6_SGI,
+    RateMcs6Sgi = wifi_phy_rate_t_WIFI_PHY_RATE_MCS6_SGI,
     /// MCS7 with short GI, 72.2 Mbps for 20MHz, 150 Mbps for 40MHz
-    RateMcs7Sgi  = wifi_phy_rate_t_WIFI_PHY_RATE_MCS7_SGI,
+    RateMcs7Sgi = wifi_phy_rate_t_WIFI_PHY_RATE_MCS7_SGI,
     /// MCS8 with short GI
     #[cfg(not(wifi_mac_version = "1"))]
-    RateMcs8Sgi  = wifi_phy_rate_t_WIFI_PHY_RATE_MCS8_SGI,
+    RateMcs8Sgi = wifi_phy_rate_t_WIFI_PHY_RATE_MCS8_SGI,
     /// MCS9 with short GI
     #[cfg(not(wifi_mac_version = "1"))]
-    RateMcs9Sgi  = wifi_phy_rate_t_WIFI_PHY_RATE_MCS9_SGI,
+    RateMcs9Sgi = wifi_phy_rate_t_WIFI_PHY_RATE_MCS9_SGI,
     /// 250 Kbps
     RateLora250k = wifi_phy_rate_t_WIFI_PHY_RATE_LORA_250K,
     /// 500 Kbps
@@ -439,19 +445,19 @@ impl EspNowWifiInterface {
 #[instability::unstable]
 pub enum PhyMode {
     /// PHY mode for Low Rate
-    Lr    = wifi_phy_mode_t_WIFI_PHY_MODE_LR,
+    Lr = wifi_phy_mode_t_WIFI_PHY_MODE_LR,
     /// PHY mode for 11b
-    _11b  = wifi_phy_mode_t_WIFI_PHY_MODE_11B,
+    _11b = wifi_phy_mode_t_WIFI_PHY_MODE_11B,
     /// PHY mode for 11g
-    _11g  = wifi_phy_mode_t_WIFI_PHY_MODE_11G,
+    _11g = wifi_phy_mode_t_WIFI_PHY_MODE_11G,
     /// PHY mode for 11a
-    _11a  = wifi_phy_mode_t_WIFI_PHY_MODE_11A,
+    _11a = wifi_phy_mode_t_WIFI_PHY_MODE_11A,
     /// PHY mode for Bandwidth HT20
-    Ht20  = wifi_phy_mode_t_WIFI_PHY_MODE_HT20,
+    Ht20 = wifi_phy_mode_t_WIFI_PHY_MODE_HT20,
     /// PHY mode for Bandwidth HT40
-    Ht40  = wifi_phy_mode_t_WIFI_PHY_MODE_HT40,
+    Ht40 = wifi_phy_mode_t_WIFI_PHY_MODE_HT40,
     /// PHY mode for Bandwidth HE20
-    He20  = wifi_phy_mode_t_WIFI_PHY_MODE_HE20,
+    He20 = wifi_phy_mode_t_WIFI_PHY_MODE_HE20,
     /// PHY mode for Bandwidth VHT20
     Vht20 = wifi_phy_mode_t_WIFI_PHY_MODE_VHT20,
 }
