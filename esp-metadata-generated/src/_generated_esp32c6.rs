722e4627ceef4dce41ae05204ede61d7ea1f0ba3
Kirill Mikhailov <konnor1980@yandex.ru>
2026-09-09 18:34:03 +0200

    Part 2: PMP granularity issue fix, GPIO, USJ

--- esp-metadata-generated/src/_generated_esp32c6.rs
+++ esp-metadata-generated/src/_generated_esp32c6.rs
@@ -541,6 +541,9 @@ macro_rules! property {
     ("soc.cpu_mcause_mask", str) => {
         stringify!(31)
     };
+    ("soc.cpu_pmp_granularity_128") => {
+        false
+    };
     ("clock_tree.soc_root_clk") => {
         [crate ::soc::clocks::SocRootClkConfig::Xtal, crate
         ::soc::clocks::SocRootClkConfig::RcFast, crate
