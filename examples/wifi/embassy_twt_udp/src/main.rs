833cd4aa6d275c4526f9726a3248e48d8c8266af
main() <main@ehvag.eu.org>
2026-09-12 02:48:20 +0200

    Update embassy_twt_udp

--- examples/wifi/embassy_twt_udp/src/main.rs
+++ examples/wifi/embassy_twt_udp/src/main.rs
@@ -22,7 +22,6 @@ use esp_alloc as _;
 use esp_backtrace as _;
 use esp_hal::{
     clock::CpuClock,
-    interrupt::software::SoftwareInterruptControl,
     ram,
     rng::Rng,
     time::Duration,
@@ -30,6 +29,7 @@ use esp_hal::{
 };
 use esp_println::println;
 use esp_radio::wifi::{
+    AuthenticationMethodConfig,
     Config,
     ControllerConfig,
     Interface,
@@ -66,13 +66,14 @@ async fn main(spawner: Spawner) -> ! {
     esp_alloc::heap_allocator!(size: 36 * 1024);
 
     let timg0 = TimerGroup::new(peripherals.TIMG0);
-    let sw_int = SoftwareInterruptControl::new(peripherals.SW_INTERRUPT);
-    esp_rtos::start(timg0.timer0, sw_int.software_interrupt0);
+    esp_rtos::start(timg0.timer0, peripherals.FROM_CPU_INTR0);
 
     let station_config = Config::Station(
         StationConfig::default()
-            .with_ssid(SSID)
-            .with_password(PASSWORD.into())
+            .with_ssid(SSID.try_into().unwrap())
+            .with_authentication(AuthenticationMethodConfig::Wpa2Personal(
+                PASSWORD.try_into().unwrap(),
+            ))
             .with_protocols(Protocols::default().with_2_4(Protocol::AX.into())),
     );
 
