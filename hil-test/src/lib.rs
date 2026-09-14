2099ed689e57eea37f2065d7c0ceb6d7ac6f2c91
Kirill Mikhailov <konnor1980@yandex.ru>
2026-09-14 14:40:00 +0200

    Part 4: stable I2C, I2S

--- hil-test/src/lib.rs
+++ hil-test/src/lib.rs
@@ -83,6 +83,7 @@ macro_rules! i2c_pins {
             any(esp32c6, esp32c61) => ($peripherals.GPIO6, $peripherals.GPIO7),
             esp32s31 => ($peripherals.GPIO7, $peripherals.GPIO6),
             esp32h2 => ($peripherals.GPIO12, $peripherals.GPIO22),
+            esp32h4 => ($peripherals.GPIO26, $peripherals.GPIO27),
             esp32c2 => ($peripherals.GPIO18, $peripherals.GPIO9),
             any(esp32c5, esp32p4) => ($peripherals.GPIO2, $peripherals.GPIO3),
             // esp32c3
