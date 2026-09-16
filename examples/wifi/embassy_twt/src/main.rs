49399facb921dbb708f8c9e85e5e610a940ab7b6
main() <main@ehvag.de>
2026-09-16 20:39:58 +0200

    update esp_rtos start

--- examples/wifi/embassy_twt/src/main.rs
+++ examples/wifi/embassy_twt/src/main.rs
@@ -82,7 +82,7 @@ async fn main(_spawner: embassy_executor::Spawner) -> ! {
     esp_alloc::heap_allocator!(size: 36 * 1024);
 
     let timg0 = TimerGroup::new(peripherals.TIMG0);
-    esp_rtos::start(timg0.timer0, peripherals.FROM_CPU_INTR0);
+    esp_rtos::start(timg0.timer0);
 
     let station_config = Config::Station(
         StationConfig::default()
